98630f56102ce90dd5ca1a38d427d99e2f6fbefe
supix <esposito.marce@gmail.com>
2017-01-30 12:44:17 +0100

    Iniziata la modellazione delle partenze
    Creata la classe ComposizionePartenza che modella l'invio di risorse di
    personale e strumentali per far fronte alla richiesta. Va approfondita
    la struttura della classe (per es. id squadra?).

---
+++ src/backend/SOVVF/Classi/Soccorso/Eventi/ComposizionePartenza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modello.Classi.Soccorso.Eventi
+{
+    /// <summary>
+    ///   Questa classe modella la composizione di una partenza. Si compongono le seguenti risorse:
+    ///   * Id Squadra
+    ///   * Elenco persone/ruoli
+    ///   * Elenco mezzi
+    ///   * Elenco attrezzature
+    ///
+    ///   ....
+    /// </summary>
+    /// <remarks>
+    ///   Questo evento implicitamente associa il personale al mezzo/attrezzatura. Probabilmente
+    ///   mezzi e attrezzature possnon essere ricondotti in un'unica categoria.
+    ///
+    ///   .....
+    /// </remarks>
+    public class ComposizionePartenza
+    {
+    }
+}
