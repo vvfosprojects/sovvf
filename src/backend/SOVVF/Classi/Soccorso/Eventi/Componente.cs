720ec052bd13147b4bc7d4137a6e60edb9984cc5
supix <esposito.marce@gmail.com>
2017-01-31 12:50:10 +0100

    Questo branch è committato per salvare l'ipotesi di avere il codice del capopartenza (unico) cablato nella classe ComposizionePartenza insieme alla lista dei mezzi (ciascuno con i suoi passeggeri) senza lista ruoli.

---
+++ src/backend/SOVVF/Classi/Soccorso/Eventi/Componente.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Modello.Classi.Soccorso.Eventi
+{
+    /// <summary>
+    ///   Modella il componente di una partenza, con i suoi ruoli ed il mezzo sul quale è collocato.
+    /// </summary>
+    public class Componente
+    {
+        public enum Ruolo
+        {
+            /// <summary>
+            ///   E' il ruolo del capoPartenza
+            /// </summary>
+            CapoPartenza,
+
+            /// <summary>
+            ///   E' il ruolo dell'autista
+            /// </summary>
+            Autista,
+
+            /// <summary>
+            ///   E' il ruolo base che non implica mansioni speciali
+            /// </summary>
+            Vigile
+        }
+
+        /// <summary>
+        ///   E' il codice fiscale del membro appartenente alla partenza
+        /// </summary>
+        public string CodiceFiscale { get; set; }
+
+        public string CodiceMezzo { get; set; }
+
+        /// <summary>
+        ///   E' la lista dei ruoli assegnati al soggetto contestualmente alla richiesta in corso
+        /// </summary>
+        public IList<Ruolo> Ruoli { get; set; }
+    }
+}
--- src/backend/SOVVF/Classi/Soccorso/Eventi/ComposizionePartenza.cs
+++ src/backend/SOVVF/Classi/Soccorso/Eventi/ComposizionePartenza.cs
@@ -8,20 +8,47 @@ namespace Modello.Classi.Soccorso.Eventi
 {
     /// <summary>
     ///   Questa classe modella la composizione di una partenza. Si compongono le seguenti risorse:
-    ///   * Id Squadra
-    ///   * Elenco persone/ruoli
+    ///   * Codice Squadra
+    ///   * Elenco persone relativi ruoli
     ///   * Elenco mezzi
     ///   * Elenco attrezzature
-    ///
-    ///   ....
     /// </summary>
     /// <remarks>
     ///   Questo evento implicitamente associa il personale al mezzo/attrezzatura. Probabilmente
-    ///   mezzi e attrezzature possnon essere ricondotti in un'unica categoria.
-    ///
-    ///   .....
+    ///   mezzi e attrezzature possono essere ricondotti ad un'unica categoria (risorsa strumentale).
     /// </remarks>
     public class ComposizionePartenza
     {
+        /// <summary>
+        ///   E' il codice della squadra coinvolta nella partenza, così come codificata nel modulo
+        ///   relativo alla composizione dei servizi.
+        /// </summary>
+        public string CodiceSquadra { get; set; }
+
+        /// <summary>
+        ///   E' il codice fiscale del capopartenza.
+        /// </summary>
+        /// <remarks>
+        ///   Questo codice fiscale deve essere ripetuto nell'attributo Mezzi per indicare la
+        ///   posizione del capopartenza all'interno di un mezzo. Non è un problema se questa codice
+        ///   fiscale è contenuto anche nall'attributo ComponentiSenzaMezzo.
+        /// </remarks>
+        public string CodiceFiscaleCapopartenza { get; set; }
+
+        /// <summary>
+        ///   E' la lista dei mezzi e dei relativi passeggeri a bordo
+        /// </summary>
+        public IList<PartenzaMezzo> Mezzi { get; set; }
+
+        /// <summary>
+        ///   E' la lista dei componenti che non sono passeggeri di un mezzo
+        /// </summary>
+        /// <remarks>
+        ///   Questa lista consente di non associare necessariamente un mezzo a delle persone (per
+        ///   es. in caso di vigilanza o verifica statica).
+        /// </remarks>
+        public IList<string> ComponentiSenzaMezzo { get; set; }
+
+        public IList<Componente> Componenti { get; set; }
     }
 }
