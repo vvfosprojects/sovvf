b6e54bc62b10cad6245552998f8da5c2665356bf
utente22 <utente22@OPERATORE22>
2017-02-02 13:36:59 +0100

    E' disponibile la classe UscitaPartenza

---
+++ src/backend/SOVVF/Classi/Soccorso/Eventi/UscitaPartenza.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modello.Classi.Soccorso.Eventi
+{
+    internal class UscitaPartenza : Evento
+    {
+    }
+}
