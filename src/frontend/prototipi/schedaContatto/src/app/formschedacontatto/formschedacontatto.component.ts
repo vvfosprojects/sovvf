12458b39e51a9d228f77aa0e3731e37fc78e5ff4
Fares Daniela <daniela.fares@dipvvf.it>
2017-03-31 11:03:47 +0200

    Creato progetto SchedaContatto
    Creato progetto SchedaContatto, creato il componente formSchedaContatto
    ed importato il template della pagina schedaContatto.

---
+++ src/frontend/prototipi/schedaContatto/src/app/formschedacontatto/formschedacontatto.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formschedacontatto',
+  templateUrl: './formschedacontatto.component.html',
+  styleUrls: ['./formschedacontatto.component.css']
+})
+export class FormschedacontattoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
