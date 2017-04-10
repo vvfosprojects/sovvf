11e9036aa1899e0908a29b45dd6945c0240fb4c6
Fares Daniela <daniela.fares@dipvvf.it>
2017-04-10 15:29:00 +0200

    Creato Componente MezzoInServizio
    Generato il Componente mezzoinservizio per visualizzare le informazioni
    relative al mezzo richiesto. Iniziata la creazione dell'oggetto grafico.

--- src/frontend/prototipi/mezzoInServizio/src/app/app.module.ts
+++ src/frontend/prototipi/mezzoInServizio/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MezzoinservizioComponent } from './mezzoinservizio/mezzoinservizio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MezzoinservizioComponent
   ],
   imports: [
     BrowserModule,
