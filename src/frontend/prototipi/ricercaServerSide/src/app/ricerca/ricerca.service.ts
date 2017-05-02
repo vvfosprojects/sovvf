a1a62a00788374706e1586125cf59e7fe50450e7
supix <esposito.marce@gmail.com>
2017-05-02 13:35:50 +0200

    Installazione di boostrap
    Installato il package npm bootstrap4 attraverso le istruzioni
    disponibili su:
    http://stackoverflow.com/questions/38534276/how-to-use-bootstrap-4-in-angular-2
    
    In pratica si lancia il comando:
    `npm i bootstrap@next --save`
    
    e poi si include in style.css il codice
    
    `@import "../node_modules/bootstrap/dist/css/bootstrap.min.css";`
    
    Resta da installare la parte dinamica di boostrap.

--- src/frontend/prototipi/ricercaServerSide/src/app/ricerca/ricerca.service.ts
+++ src/frontend/prototipi/ricercaServerSide/src/app/ricerca/ricerca.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import 'rxjs/add/of';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 import { RisultatoRicerca } from "./risultato-ricerca";
 
