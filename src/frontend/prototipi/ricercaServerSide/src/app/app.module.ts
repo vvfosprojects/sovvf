ca6145a205b682bb4dda36a7c27d511accf20859
supix <esposito.marce@gmail.com>
2017-05-02 13:41:10 +0200

    Installazione parte dinamica di Boostrap4
    E' installata la parte dinamica di boostrap4. E' stata seguita la
    documentazione disponibile a:
    https://ng-bootstrap.github.io/#/getting-started
    
    In pratica si lancia:
    `npm install --save @ng-bootstrap/ng-bootstrap`
    
    e poi si fanno le necessaria modifiche al module root (vedi docs).

--- src/frontend/prototipi/ricercaServerSide/src/app/app.module.ts
+++ src/frontend/prototipi/ricercaServerSide/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { RicercaService } from "./ricerca/ricerca.service";
@@ -11,6 +12,7 @@ import { RicercaService } from "./ricerca/ricerca.service";
     AppComponent
   ],
   imports: [
+    NgbModule.forRoot(),
     BrowserModule,
     FormsModule,
     HttpModule
