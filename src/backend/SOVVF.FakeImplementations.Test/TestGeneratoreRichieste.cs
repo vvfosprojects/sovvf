977313528c5cd29606e5a3b79a7a9b87c9d47d0d
supix <esposito.marce@gmail.com>
2017-05-08 13:49:15 +0200

    Generatore fake richieste
    L'evento telefonata viene aggiunto in testa agli eventi per ogni richiesta in
    corrispondenza della data segnalazione prevista per quella richiesta.
    Sarebbe utile popolare anche gli altri campi vouti dell'evento
    telefonata.

--- src/backend/SOVVF.FakeImplementations.Test/TestGeneratoreRichieste.cs
+++ src/backend/SOVVF.FakeImplementations.Test/TestGeneratoreRichieste.cs
@@ -22,6 +22,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Modello.Classi.Soccorso.Eventi;
+using Modello.Classi.Soccorso.Eventi.Segnalazioni;
 using NUnit.Framework;
 using SOVVF.FakeImplementations.Modello.GestioneSoccorso;
 using SOVVF.FakeImplementations.Modello.GestioneSoccorso.GenerazioneRichieste;
@@ -36,17 +38,19 @@ namespace SOVVF.FakeImplementations.Test
         {
             var gi = new GeneratoreRichieste(
                 "MI",
-                5,
-                DateTime.Now.AddMonths(-2),
+                3,
+                DateTime.Now.AddHours(-12),
                 DateTime.Now,
-                25,
+                100,
                 30 * 60,
                 15 * 60,
                 45 * 60,
                 15 * 60,
                 new float[] { .85F, .7F, .4F, .3F, .1F });
 
-            var richieste = gi.Genera();
+            var richieste = gi.Genera()
+                .OrderBy(r => (r.Eventi.First() as Evento).Istante)
+                .ToList();
         }
     }
 }
