51fb90100c3e2de3a4fa692657dc71d2339cb9cc
supix <esposito.marce@gmail.com>
2017-05-12 22:19:53 +0200

    Classe Utente
    E' disponibile nel modello di dominio la classe Utente che modella il
    generico utente del sistema, con una data di inizio e fine validità e un
    flag Attivo che ne consente la agile attivazione/disattivazione.
    
    I costruttori della classe garantiscono le invarianti sui parametri.
    
    Ci sono i casi di test per la classe.

---
+++ src/backend/Modello.Test/Classi/Autenticazione/TestUtente.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modello.Classi.Autenticazione;
+using NUnit.Framework;
+
+namespace Modello.Test.Classi.Autenticazione
+{
+    [TestFixture]
+    public class TestUtente
+    {
+        [Test]
+        public void Un_utente_con_username_e_correttamente_creato()
+        {
+            var utente = new Utente("username");
+
+            Assert.That(utente.Username, Is.EqualTo("username"));
+            Assert.That(utente.ValidoDa, Is.Null);
+            Assert.That(utente.ValidoFinoA, Is.Null);
+            Assert.That(utente.Attivo, Is.True);
+        }
+
+        [Test]
+        public void Un_utente_con_username_whitespace_non_puo_essere_creato()
+        {
+            Assert.That(() => new Utente(" "),
+                Throws.ArgumentException);
+        }
+
+        [Test]
+        public void Un_utente_con_username_e_data_fine_validita_e_correttamente_creato()
+        {
+            var fine = DateTime.Now;
+            var utente = new Utente("username", fine);
+
+            Assert.That(utente.Username, Is.EqualTo("username"));
+            Assert.That(utente.ValidoDa, Is.Null);
+            Assert.That(utente.ValidoFinoA, Is.EqualTo(fine));
+            Assert.That(utente.Attivo, Is.True);
+        }
+
+        [Test]
+        public void Un_utente_con_username_e_data_fine_validita_di_default_non_puo_essere_creato()
+        {
+            Assert.That(() => new Utente("username", new DateTime()),
+                Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void Un_utente_con_username_data_inizio_e_fine_validita_e_correttamente_creato()
+        {
+            var inizio = DateTime.Now;
+            var fine = DateTime.Now.AddDays(1);
+            var utente = new Utente("username", inizio, fine);
+
+            Assert.That(utente.Username, Is.EqualTo("username"));
+            Assert.That(utente.ValidoDa, Is.EqualTo(inizio));
+            Assert.That(utente.ValidoFinoA, Is.EqualTo(fine));
+            Assert.That(utente.Attivo, Is.True);
+        }
+
+        [Test]
+        public void Un_utente_con_username_data_inizio_validita_di_default_e_data_fine_validita_non_puo_essere_creato()
+        {
+            var fine = DateTime.Now.AddDays(1);
+            Assert.That(() => new Utente("username", new DateTime(), fine),
+                Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        public void Un_utente_con_username_data_inizio_validita_e_data_fine_validita_di_default_non_puo_essere_creato()
+        {
+            var inizio = DateTime.Now;
+            Assert.That(() => new Utente("username", inizio, new DateTime()),
+                Throws.TypeOf<ArgumentOutOfRangeException>());
+        }
+    }
+}
---
+++ src/backend/SOVVF/Classi/Autenticazione/Utente.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modello.Classi.Autenticazione
+{
+    /// <summary>
+    ///   Modella un utente del sistema.
+    /// </summary>
+    public class Utente
+    {
+        /// <summary>
+        ///   Costruttore della classe
+        /// </summary>
+        /// <param name="username">La username associata all'utente</param>
+        /// <remarks>L'utenza viene create per default con il flag attivo impostato a true</remarks>
+        public Utente(string username)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                throw new ArgumentException("Cannot be null or whitespace", nameof(username));
+            }
+
+            this.Username = username;
+            this.Attivo = true;
+        }
+
+        /// <summary>
+        ///   Costruttore della classe
+        /// </summary>
+        /// <param name="username">La username associata all'utente</param>
+        /// <param name="validoFinoA">La data di fine validità dell'utenza</param>
+        /// <remarks>La data di inizio validità è il big bang</remarks>
+        public Utente(string username, DateTime validoFinoA) : this(username)
+        {
+            if (validoFinoA == DateTime.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(validoFinoA));
+            }
+
+            this.ValidoFinoA = validoFinoA;
+        }
+
+        /// <summary>
+        ///   Costruttore della classe
+        /// </summary>
+        /// ///
+        /// <param name="username">La username associata all'utente</param>
+        /// <param name="validoDa">La data di inizio validità dell'utenza</param>
+        /// <param name="validoFinoA">La data di fine validità dell'utenza</param>
+        public Utente(string username, DateTime validoDa, DateTime validoFinoA) : this(username, validoFinoA)
+        {
+            if (validoDa == DateTime.MinValue)
+            {
+                throw new ArgumentOutOfRangeException(nameof(validoDa));
+            }
+
+            this.ValidoDa = validoDa;
+        }
+
+        public string Username { get; private set; }
+        public DateTime? ValidoDa { get; set; }
+        public DateTime? ValidoFinoA { get; set; }
+        public bool Attivo { get; set; }
+    }
+}
