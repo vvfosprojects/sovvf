774f8062be27e515fcb495fc704da1c6e5d388b6
supix <esposito.marce@gmail.com>
2017-05-12 22:28:57 +0200

    Licenza nell'header
    Tutti i files in cui mancava l'header di licenza ora ce l'hanno.

--- src/backend/SOVVF.FakeImplementations/Modello/GestioneSoccorso/GenerazioneRichieste/StatoMezzo/IStatoMezzo.cs
+++ src/backend/SOVVF.FakeImplementations/Modello/GestioneSoccorso/GenerazioneRichieste/StatoMezzo/IStatoMezzo.cs
@@ -17,7 +17,6 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // </copyright>
 //-----------------------------------------------------------------------
-
 namespace SOVVF.FakeImplementations.Modello.GestioneSoccorso.GenerazioneRichieste.StatoMezzo
 {
     /// <summary>
