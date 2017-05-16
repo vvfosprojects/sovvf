ba724af257e7145541a03c5599929ddaa133cf64
Luigi Frasca <luigi,frasca@vigilfuoco.it>
2017-05-16 11:25:08 +0200

    Aggiunta librerira PrimeNG
    Aggiunto PrimeNG, fornito il servizio di ricerca e aggiunta la chiamata nel componente.

--- src/frontend/prototipi/schedaTelefonata/src/app/app.module.ts
+++ src/frontend/prototipi/schedaTelefonata/src/app/app.module.ts
@@ -8,6 +8,9 @@ import { FormChiamataComponent } from './form-chiamata/form-chiamata.component';
 import { MultiselectDropdownModule  } from 'angular-2-dropdown-multiselect';
 
 import { AgmCoreModule } from "@agm/core";
+import {AutoCompleteModule} from 'primeng/primeng';
+
+import { RicercaTipologieService } from "./ricerca-tipologie/ricerca-tipologie.service";
 
 @NgModule({
   declarations: [
@@ -23,9 +26,10 @@ import { AgmCoreModule } from "@agm/core";
     FormsModule,
     HttpModule,
     ReactiveFormsModule,
-    MultiselectDropdownModule 
+    MultiselectDropdownModule,
+    AutoCompleteModule 
   ],
-  providers: [],
+  providers: [RicercaTipologieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/frontend/prototipi/schedaTelefonata/src/app/form-chiamata/form-chiamata.component.ts
+++ src/frontend/prototipi/schedaTelefonata/src/app/form-chiamata/form-chiamata.component.ts
@@ -7,9 +7,13 @@ import {
   FormControl,
   Validators
 } from '@angular/forms';
-
 import { AgmCoreModule, MapsAPILoader } from '@agm/core';
-import {} from '@types/googlemaps';
+import { } from '@types/googlemaps';
+import { AutoCompleteModule } from 'primeng/primeng';
+
+import { RicercaTipologieService } from ".././ricerca-tipologie/ricerca-tipologie.service";
+import { TipologiaIntervento } from ".././ricerca-tipologie/tipologia-intervento.model";
+
 
 @Component({
   selector: 'app-form-chiamata',
@@ -17,14 +21,17 @@ import {} from '@types/googlemaps';
   styleUrls: ['./form-chiamata.component.css']
 })
 export class FormChiamataComponent implements OnInit {
-   public latitude: number;
+  risultati: TipologiaIntervento[];
+
+  //------- Maps ------//
+  public latitude: number;
   public longitude: number;
   public searchControl: FormControl;
   public zoom: number;
 
   @ViewChild("search")
-    public searchElementRef: ElementRef;
-
+  public searchElementRef: ElementRef;
+  //------- End Maps ------//
 
   @Input() formChiamataModel: FormChiamataModel;
 
@@ -36,15 +43,15 @@ export class FormChiamataComponent implements OnInit {
    * opzioni per la multiselect del tag
    */
   myOptions: IMultiSelectOption[] = [
-        { id: 1, name: 'tag 1 lungo' },
-        { id: 2, name: 'tag 2 lungo' },
-        { id: 3, name: 'tag 3 lungo' },
-        { id: 4, name: 'tag 4' },
-        { id: 5, name: 'tag 5' },
-        { id: 6, name: 'tag 6' },
-    ];
+    { id: 1, name: 'tag 1 lungo' },
+    { id: 2, name: 'tag 2 lungo' },
+    { id: 3, name: 'tag 3 lungo' },
+    { id: 4, name: 'tag 4' },
+    { id: 5, name: 'tag 5' },
+    { id: 6, name: 'tag 6' },
+  ];
 
-    myTexts: IMultiSelectTexts = {
+  myTexts: IMultiSelectTexts = {
     checkAll: 'Seleziona tutti',
     uncheckAll: 'Deseleziona tutti',
     checked: 'Selezionato',
@@ -52,9 +59,9 @@ export class FormChiamataComponent implements OnInit {
     searchPlaceholder: 'Cerca...',
     defaultTitle: 'Seleziona tag',
     allSelected: 'Tutti selezionati',
-};
+  };
 
-mySettings: IMultiSelectSettings = {
+  mySettings: IMultiSelectSettings = {
     pullRight: true,
     enableSearch: true,
     checkedStyle: 'fontawesome',
@@ -65,14 +72,14 @@ mySettings: IMultiSelectSettings = {
     autoUnselect: false,
     showCheckAll: true,
     showUncheckAll: true,
-   // fixedTitle: false,
+    // fixedTitle: false,
     dynamicTitleMaxItems: 2,
     maxHeight: '300px',
-};
-
+  };
+  //------- End multiselect ------//
 
   constructor( @Inject(FormBuilder) private fb: FormBuilder, private fb2: FormBuilder, private mapsAPILoader: MapsAPILoader,
-    private ngZone: NgZone) {
+    private ngZone: NgZone, private _ricercaTipologieService: RicercaTipologieService, ) {
     this.formChiamataModel = new FormChiamataModel();
     this.formChiamataModel.numero_chiamata = "123.4567.890";
     this.formChiamataModel.operatore = "Mario Rossi";
@@ -109,10 +116,10 @@ mySettings: IMultiSelectSettings = {
      * multiselect per il campo tag
      */
     this.myForm.controls['optionsModel'].valueChanges
-            .subscribe((selectedOptions) => {
-                // changes
-                console.log("valore multiselect cambiato ! "+this.myForm.controls['optionsModel'].value);
-            });
+      .subscribe((selectedOptions) => {
+        // changes
+        console.log("valore multiselect cambiato ! " + this.myForm.controls['optionsModel'].value);
+      });
 
     /**
      * il validatore multi campo agisce a livello di gruppo.
@@ -122,26 +129,26 @@ mySettings: IMultiSelectSettings = {
       'cognome': [this.formChiamataModel.cognome],
       'ragione_sociale': [this.formChiamataModel.ragione_sociale],
       //'telefono': [this.formChiamataModel.telefono]
-    }, { validator: this.validaRichiedente.bind(this) }); 
+    }, { validator: this.validaRichiedente.bind(this) });
 
 
     //  console.log("fine init nome vale "+this.formChiamataModel.nome);
     console.log('myForm status ' + this.myForm.status);
     console.log('formRagSoc status ' + this.formRagSoc.status);
 
-    //----------------------maps--------------------------------//
+    //-------------settaggi iniziali di maps onInit----------------------------//
 
-     //set google maps defaults
+    //set google maps defaults
     this.zoom = 18;
     this.latitude = 39.8282;
     this.longitude = -98.5795;
-    
+
     //create search FormControl
     this.searchControl = new FormControl();
-    
+
     //set current position
     this.setCurrentPosition();
-    
+
     //load Places Autocomplete
     this.mapsAPILoader.load().then(() => {
       let autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
@@ -151,12 +158,12 @@ mySettings: IMultiSelectSettings = {
         this.ngZone.run(() => {
           //get the place result
           let place: google.maps.places.PlaceResult = autocomplete.getPlace();
-  
+
           //verify result
           if (place.geometry === undefined || place.geometry === null) {
             return;
           }
-          
+
           //set latitude, longitude and zoom
           this.latitude = place.geometry.location.lat();
           this.longitude = place.geometry.location.lng();
@@ -164,9 +171,22 @@ mySettings: IMultiSelectSettings = {
         });
       });
     });
+     //-------------end settaggi iniziali di maps onInit----------------------------//
+  }
+  /**
+   * Valorizza l'array tipologie per il componente PrimeNG (Autocomplete) filtrato per chiave di ricerca.
+   * @param event 
+   */
+  searchTipologia(event) {
+
+    this.risultati = this._ricercaTipologieService.search(event.query);
+
   }
 
-   private setCurrentPosition() {
+  /**
+   * Setta la posizione geografica corrente e il livello di zoom iniziale nella mappa.
+   */
+  private setCurrentPosition() {
     if ("geolocation" in navigator) {
       navigator.geolocation.getCurrentPosition((position) => {
         this.latitude = position.coords.latitude;
@@ -174,7 +194,7 @@ mySettings: IMultiSelectSettings = {
         this.zoom = 18;
       });
     }
-  }  
+  }
 
   get formValido(): boolean {
     return this.formRagSoc.valid;
@@ -182,77 +202,77 @@ mySettings: IMultiSelectSettings = {
 
   validaRichiedente(group: FormGroup): { [s: string]: boolean } {
     // var val_telefono = group.controls['telefono'].value;
-     var val_nome = group.controls['nome'].value;
-     var val_cognome = group.controls['cognome'].value;
-     var val_ragione_sociale = group.controls['ragione_sociale'].value;
-   /*
-     console.log("telfono "+val_telefono);
-     console.log("nome "+val_nome);
-     console.log("cognome "+val_cognome);
-     console.log("val_ragione_sociale "+val_ragione_sociale);
-
-     if (val_telefono || val_nome) 
-          console.log("eccomi !");
-*/
-/*
-     var valido = (val_nome !== "pippo");
-     console.log("valida richiedetente è NON valido ?"+valido);
-*/
-    if (val_nome || val_cognome || val_ragione_sociale ) 
+    var val_nome = group.controls['nome'].value;
+    var val_cognome = group.controls['cognome'].value;
+    var val_ragione_sociale = group.controls['ragione_sociale'].value;
+    /*
+      console.log("telfono "+val_telefono);
+      console.log("nome "+val_nome);
+      console.log("cognome "+val_cognome);
+      console.log("val_ragione_sociale "+val_ragione_sociale);
+ 
+      if (val_telefono || val_nome) 
+           console.log("eccomi !");
+ */
+    /*
+         var valido = (val_nome !== "pippo");
+         console.log("valida richiedetente è NON valido ?"+valido);
+    */
+    if (val_nome || val_cognome || val_ragione_sociale)
       return { campoVuoto: true };
   }
 
-/*
-validaCognome(control: FormGroup): { [s: string]: boolean } {
-  //    console.log(control.status);
-  if (control.value != "AAAA") {
-    return { nonAAAA: true };
+  /*
+  validaCognome(control: FormGroup): { [s: string]: boolean } {
+    //    console.log(control.status);
+    if (control.value != "AAAA") {
+      return { nonAAAA: true };
+    }
   }
-}
-*/
+  */
 
-onClickCC(){
-  this.formChiamataModel.ragione_sociale = "Carabinieri";
-  console.log("onClickCC !!");
-  this.formRagSoc.controls['ragione_sociale'].setValue("Carabinieri");
-}
-onClickPS(){
-  this.formChiamataModel.ragione_sociale = "Polizia di Stato";
-  this.formRagSoc.controls['ragione_sociale'].setValue("Polizia di Stato");
-  console.log("onClickPS !!");
+  onClickCC() {
+    this.formChiamataModel.ragione_sociale = "Carabinieri";
+    console.log("onClickCC !!");
+    this.formRagSoc.controls['ragione_sociale'].setValue("Carabinieri");
+  }
+  onClickPS() {
+    this.formChiamataModel.ragione_sociale = "Polizia di Stato";
+    this.formRagSoc.controls['ragione_sociale'].setValue("Polizia di Stato");
+    console.log("onClickPS !!");
 
-}
-onClickSSU(){
-  this.formChiamataModel.ragione_sociale = "Servizio Sanitario Urgenze";
-  this.formRagSoc.controls['ragione_sociale'].setValue("Servizio Sanitario Urgenze");
-  console.log("onClickSSU !!");
-}
-onClickPM(){
-  this.formChiamataModel.ragione_sociale = "Polizia Municipale";
-  this.formRagSoc.controls['ragione_sociale'].setValue("Polizia Municipale");
-  console.log("onClickPM !!");
-}
+  }
+  onClickSSU() {
+    this.formChiamataModel.ragione_sociale = "Servizio Sanitario Urgenze";
+    this.formRagSoc.controls['ragione_sociale'].setValue("Servizio Sanitario Urgenze");
+    console.log("onClickSSU !!");
+  }
+  onClickPM() {
+    this.formChiamataModel.ragione_sociale = "Polizia Municipale";
+    this.formRagSoc.controls['ragione_sociale'].setValue("Polizia Municipale");
+    console.log("onClickPM !!");
+  }
 
-onClickAnnulla() {
-       this.myForm.reset();
-       this.formRagSoc.reset();
-   }
+  onClickAnnulla() {
+    this.myForm.reset();
+    this.formRagSoc.reset();
+  }
 
-onSubmit(value: any): void {
-  console.log("nome ", value.nome);
-  console.log("cognome ", value.cognome);
-  console.log("ragione_sociale ", value.ragione_sociale);
-  console.log("tipo ", this.myForm.controls.tipo_interv.value);
-  console.log("indirizzo value ", this.myForm.controls.indirizzo.value);
-  console.log("telefono ", this.myForm.controls.telefono.value);
-  console.log("zona_emergenza ", this.myForm.controls.zona_emergenza.value);
-  console.log("ciao! "+this.myForm.controls['optionsModel'].value);
-  console.log("motivazione ", this.myForm.controls.motivazione.value);
-  console.log("note_indirizzo ", this.myForm.controls.note_indirizzo.value);
-  console.log("note_pubbliche ", this.myForm.controls.note_pubbliche.value);
-  console.log("note_private ", this.myForm.controls.note_private.value);
-  
-  // let formChiamataModel = new FormChiamataModel();
-  
- }
+  onSubmit(value: any): void {
+    console.log("nome ", value.nome);
+    console.log("cognome ", value.cognome);
+    console.log("ragione_sociale ", value.ragione_sociale);
+    console.log("tipo ", this.myForm.controls.tipo_interv.value);
+    console.log("indirizzo value ", this.myForm.controls.indirizzo.value);
+    console.log("telefono ", this.myForm.controls.telefono.value);
+    console.log("zona_emergenza ", this.myForm.controls.zona_emergenza.value);
+    console.log("ciao! " + this.myForm.controls['optionsModel'].value);
+    console.log("motivazione ", this.myForm.controls.motivazione.value);
+    console.log("note_indirizzo ", this.myForm.controls.note_indirizzo.value);
+    console.log("note_pubbliche ", this.myForm.controls.note_pubbliche.value);
+    console.log("note_private ", this.myForm.controls.note_private.value);
+
+    // let formChiamataModel = new FormChiamataModel();
+
+  }
 }
