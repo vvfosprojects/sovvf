182cbb853c6d5d0b3b386264c207b5581cfd5fe0
lucky1969 <lucia.reale@vigilfuoco.it>
2017-05-26 10:49:38 +0200

    aggiunta mappa gogle nel dashboard

--- src/frontend/prototipi/cruscottoIntegrato/src/app/pages/dashboard/dashboard.routing.ts
+++ src/frontend/prototipi/cruscottoIntegrato/src/app/pages/dashboard/dashboard.routing.ts
@@ -3,6 +3,14 @@ import { Routes, RouterModule }  from '@angular/router';
 import { Dashboard } from './dashboard.component';
 import { ModuleWithProviders } from '@angular/core';
 
+///INIZIO
+import { Maps } from '../maps/maps.component';
+import { BubbleMaps } from '../maps/components/bubbleMaps/bubbleMaps.component';
+import { GoogleMaps } from '../maps/components/googleMaps/googleMaps.component';
+import { LeafletMaps } from '../maps/components/leafletMaps/leafletMaps.component';
+import { LineMaps } from '../maps/components/lineMaps/lineMaps.component';
+///FINE
+
 // noinspection TypeScriptValidateTypes
 export const routes: Routes = [
   {
@@ -10,6 +18,12 @@ export const routes: Routes = [
     component: Dashboard,
     children: [
       //{ path: 'treeview', component: TreeViewComponent }
+///INIZIO      
+      { path: 'bubblemaps', component: BubbleMaps },
+      { path: 'googlemaps', component: GoogleMaps },
+      { path: 'leafletmaps', component: LeafletMaps },
+      { path: 'linemaps', component: LineMaps }
+///FINE      
     ]
   }
 ];
--- src/frontend/prototipi/cruscottoIntegrato/src/app/pages/maps/components/googleMaps/googleMaps.component.ts
+++ src/frontend/prototipi/cruscottoIntegrato/src/app/pages/maps/components/googleMaps/googleMaps.component.ts
@@ -14,13 +14,39 @@ export class GoogleMaps {
   ngAfterViewInit() {
     let el = this._elementRef.nativeElement.querySelector('.google-maps');
 
+    //L.Icon.Default.imagePath = 'assets/img/theme/vendor/leaflet';
+
     // TODO: do not load this each time as we already have the library after first attempt
     GoogleMapsLoader.load((google) => {
-      new google.maps.Map(el, {
-        center: new google.maps.LatLng(44.5403, -78.5463),
-        zoom: 8,
+        new google.maps.Map(el, {
+        //center: new google.maps.LatLng(44.5403, -78.5463),
+        center: new google.maps.LatLng(41.9060, 12.5122),        
+        zoom: 12,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       });
     });
+
+    //let myLatlng = new google.maps.LatLng(44.5403,-78.5463);
+    //let map = new google.maps.Map(el, {
+    //    center: new google.maps.LatLng(44.5403, -78.5463),
+    //    zoom: 8,
+    //    mapTypeId: google.maps.MapTypeId.ROADMAP
+    //  });
+    //let marker = new google.maps.Marker({ 
+    //    draggable: true,
+    //    animation: google.maps.Animation.DROP,
+    //      position: myLatlng,
+    //      map: map,//set map created here
+    //        title:"Hello World!"
+    //      });          
+    //var map = L.map(el).setView([41.9060, 12.5122], 13);
+    //L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    //  attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+    //}).addTo(map);
+
+    //L.marker([41.9065464, 12.544122]).addTo(map)
+    //  .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    //  .openPopup();
+    ;
   }
 }
