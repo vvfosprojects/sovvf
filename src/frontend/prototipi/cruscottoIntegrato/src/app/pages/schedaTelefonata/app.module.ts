142f3a503d700807a5a2f6d51008e3636973dcc5
Luigi Frasca <luigi.frasca@ap.dipvvf.it>
2017-05-31 17:25:18 +0200

    UI Scheda telefonata
    utilizzato il componente p-chips per la gestione dei tag. (sviluppo in corso).

--- src/frontend/prototipi/cruscottoIntegrato/src/app/pages/schedaTelefonata/app.module.ts
+++ src/frontend/prototipi/cruscottoIntegrato/src/app/pages/schedaTelefonata/app.module.ts
@@ -8,7 +8,7 @@ import { FormChiamataComponent } from './form-chiamata/form-chiamata.component';
 import { MultiselectDropdownModule  } from 'angular-2-dropdown-multiselect';
 
 import { AgmCoreModule } from "@agm/core";
-import {AutoCompleteModule, GrowlModule, TooltipModule} from 'primeng/primeng';
+import {AutoCompleteModule, GrowlModule, TooltipModule, ChipsModule} from 'primeng/primeng';
 
 import { RicercaTipologieService } from "./ricerca-tipologie/ricerca-tipologie.service";
 import { RicercaService } from "./ricerca/ricerca.service";
@@ -30,7 +30,8 @@ import { RicercaService } from "./ricerca/ricerca.service";
     MultiselectDropdownModule,
     AutoCompleteModule,
     GrowlModule,
-    TooltipModule
+    TooltipModule,
+    ChipsModule
   ],
   providers: [RicercaService, RicercaTipologieService],
   bootstrap: [AppComponent]
