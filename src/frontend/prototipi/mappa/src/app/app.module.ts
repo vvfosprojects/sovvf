95cf5f768aed9145e5860a5b5f3dfa5a528eeed9
lucky1969 <lucia.reale@vigilfuoco.it>
2017-06-23 15:06:53 +0200

    Aggiunto il prototipo del componente mappa

---
+++ src/frontend/prototipi/mappa/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { MappaService } from "./google/mappa.service";
+import { PuntiMappaGoogle } from "./google/mappa.model";
+import { MappaComponent } from './google/mappa.component'
+
+@NgModule({
+  declarations: [
+    AppComponent
+
+    ,MappaComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [MappaService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/frontend/prototipi/mappa/src/app/google/mappa.model.ts
@@ -0,0 +1,14 @@
+
+export class PuntiMappaGoogle {
+
+     constructor(
+        public codice: string,
+        public tipologia: string,
+        public indirizzo: string,
+        public descrizione: string,
+        public longitudine: number,
+        public latitudine: number,
+        public marker: string
+     ) { }
+
+}
