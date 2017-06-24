47d6213353cecbc698309c2aa56422298c478479
supix <esposito.marce@gmail.com>
2017-06-24 07:59:12 +0200

    CodeMaid eseguito

--- src/backend/SOVVF/Servizi/Infrastruttura/Organigramma/IGetUnitaOperativeVisibiliPerSoccorso.cs
+++ src/backend/SOVVF/Servizi/Infrastruttura/Organigramma/IGetUnitaOperativeVisibiliPerSoccorso.cs
@@ -17,11 +17,7 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 // </copyright>
 //-----------------------------------------------------------------------
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Modello.Servizi.Infrastruttura.Organigramma
 {
