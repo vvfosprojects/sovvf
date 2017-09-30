47dc79ccba4c543aa9392b83516f1f9a0efcb6ca
supix <esposito.marce@gmail.com>
2017-09-30 16:27:26 +0200

    Organizzazione files progetto
    Il file contenente la classe per gli integration test è rinominato senza
    IntegrationTest, implicito nel nome del progetto a cui il file
    appartiene.
    
    Eseguito stylecop sul progetto IntegrationTest.

---
+++ src/backend/Modello.IntegrationTest/Classi/Soccorso/SituazioneMezziRichiesta.cs
@@ -0,0 +1,64 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="SituazioneMezziRichiesta.cs" company="CNVVF">
+// Copyright (C) 2017 - CNVVF
+//
+// This file is part of SOVVF.
+// SOVVF is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// SOVVF is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modello.Classi.Soccorso;
+using Modello.Classi.Soccorso.Eventi.Partenze;
+using Modello.Classi.Soccorso.Mezzi.StatiMezzo;
+using NUnit.Framework;
+
+namespace Modello.IntegrationTest.Classi.Soccorso
+{
+    [TestFixture]
+    public class SituazioneMezziRichiesta
+    {
+        ////[Test]
+        ////public void UnaRichiestaSenzaEventiRestituisceUnaSituazioneVuota()
+        ////{
+        ////    var richiesta = new RichiestaAssistenza();
+
+        //// var situazioneMezzi = richiesta.SituazioneMezzi;
+
+        ////    CollectionAssert.IsEmpty(situazioneMezzi);
+        ////}
+
+        ////[Test]
+        ////public void RichiestaConComposizioneContenenteUnSoloMezzoAssegnato()
+        ////{
+        ////    var richiesta = new RichiestaAssistenza() { Codice = "R1" };
+        ////    new ComposizionePartenze(richiesta, DateTime.Now, "fonte", false)
+        ////    {
+        ////        Componenti = new HashSet<ComponentePartenza>(new[] { new ComponentePartenza("XXX", "M1") })
+        ////    };
+        ////    var situazioneMezzi = richiesta.SituazioneMezzi;
+
+        ////    Assert.Multiple(() =>
+        ////    {
+        ////        Assert.That(situazioneMezzi, Has.Count.EqualTo(1));
+        ////        Assert.That(situazioneMezzi.Single.Codice, Is.EqualTo("M1"));
+        ////        Assert.That(situazioneMezzi.Single.Stato, Is.InstanceOf<Assegnato>());
+        ////        Assert.That(situazioneMezzi.Single.CodiceRichiestaAssistenza, Is.EqualTo("R1"));
+        ////    });
+        ////}
+    }
+}
--- src/backend/Modello.IntegrationTest/Properties/AssemblyInfo.cs
+++ src/backend/Modello.IntegrationTest/Properties/AssemblyInfo.cs
@@ -1,10 +1,28 @@
-﻿using System.Reflection;
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="CNVVF">
+// Copyright (C) 2017 - CNVVF
+//
+// This file is part of SOVVF.
+// SOVVF is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// SOVVF is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+//-----------------------------------------------------------------------
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
 [assembly: AssemblyTitle("Modello.IntegrationTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,9 +32,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
+// Setting ComVisible to false makes the types in this assembly not visible to COM components. If you
+// need to access a type in this assembly from COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
@@ -24,13 +41,9 @@ using System.Runtime.InteropServices;
 
 // Version information for an assembly consists of the following four values:
 //
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
+// Major Version Minor Version Build Number Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
