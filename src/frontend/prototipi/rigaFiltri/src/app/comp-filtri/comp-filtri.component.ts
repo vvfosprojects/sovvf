83700668053ade8b63564a215ec216b23127fca6
Fares Daniela <daniela.fares@dipvvf.it>
2017-10-13 13:24:17 +0200

    Sono presenti modifiche componente rigaFiltri
    Sono presenti le seguenti modifiche al componente rigaFiltri:
    - Se il filtro Interventi viene disattivato, i filtri Presidiati/Non
    presidiati non scompaiono ma vengono disabilitati
    - Scatta l'evento sul filtro Nuova Chiamata
    - Scatta l'evento su cancellazione (con X) del keyword ricercatestuale
    (corretto)

--- src/frontend/prototipi/rigaFiltri/src/app/comp-filtri/comp-filtri.component.ts
+++ src/frontend/prototipi/rigaFiltri/src/app/comp-filtri/comp-filtri.component.ts
@@ -26,39 +26,42 @@ export class CompFiltriComponent implements OnInit {
   
    private selFiltri(elemento_filtri : string) 
    {
-   
-    if (elemento_filtri === "chiamate") 
-       this.filtri.chiamate = !this.filtri.chiamate;     
-    else if (elemento_filtri === "interventi") 
-      {
-       this.filtri.interventi = !this.filtri.interventi;
-       if (this.filtri.interventi == false) 
-          {
-           this.filtri.presidiati = true; 
-           this.filtri.nonPresidiati = true;  
+    if (elemento_filtri === "nuova_chiamata") 
+      console.log("Evento Nuova Chiamata");
+    else  {
+      
+          if (elemento_filtri === "chiamate") 
+            this.filtri.chiamate = !this.filtri.chiamate;     
+          else if (elemento_filtri === "interventi") 
+         {
+            this.filtri.interventi = !this.filtri.interventi;
+            if (this.filtri.interventi == false) 
+            {
+            this.filtri.presidiati = true; 
+            this.filtri.nonPresidiati = true;  
+            }
           }
-      }
-    else if (elemento_filtri === "aperti")
-      this.filtri.aperti = !this.filtri.aperti;
-    else if (elemento_filtri === "chiusi")
-      this.filtri.chiusi = !this.filtri.chiusi;
-    else if (elemento_filtri === "interni")
-      this.filtri.interni = !this.filtri.interni;
-    else if (elemento_filtri === "esterni")
-      this.filtri.esterni = !this.filtri.esterni;
-    else if (elemento_filtri === "presidiati")
-      this.filtri.presidiati = !this.filtri.presidiati;
-    else if (elemento_filtri === "nonPresidiati")
-      this.filtri.nonPresidiati = !this.filtri.nonPresidiati;
-     
-    
-   console.log("CHIAMATE:"+ ' ' + this.filtri.chiamate + "  INTERVENTI:"+ ' '+ this.filtri.interventi + "  APERTI:"+ ' '+ this.filtri.aperti + "  CHIUSI: "+ ' '+ this.filtri.chiusi + "  INTERNI:" + ' ' + this.filtri.interni + "  ESTERNI:"+ ' '+ this.filtri.esterni + "  PRESIDIATI:"+ ' '+ this.filtri.presidiati + "  NON PRESIDIATI:"+ ' '+ this.filtri.nonPresidiati + "  CHIAVE DI RICERCA:" + ' ' + this.filtri.chiaveDiRicerca);
-   
-  }
+          else if (elemento_filtri === "aperti")
+            this.filtri.aperti = !this.filtri.aperti;
+          else if (elemento_filtri === "chiusi")
+            this.filtri.chiusi = !this.filtri.chiusi;
+          else if (elemento_filtri === "interni")
+            this.filtri.interni = !this.filtri.interni;
+          else if (elemento_filtri === "esterni")
+            this.filtri.esterni = !this.filtri.esterni;
+          else if (elemento_filtri === "presidiati")
+            this.filtri.presidiati = !this.filtri.presidiati;
+          else if (elemento_filtri === "nonPresidiati")
+            this.filtri.nonPresidiati = !this.filtri.nonPresidiati;
+           
+          console.log("CHIAMATE:"+ ' ' + this.filtri.chiamate + "  INTERVENTI:"+ ' '+ this.filtri.interventi + "  APERTI:"+ ' '+ this.filtri.aperti + "  CHIUSI: "+ ' '+ this.filtri.chiusi + "  INTERNI:" + ' ' + this.filtri.interni + "  ESTERNI:"+ ' '+ this.filtri.esterni + "  PRESIDIATI:"+ ' '+ this.filtri.presidiati + "  NON PRESIDIATI:"+ ' '+ this.filtri.nonPresidiati + "  CHIAVE DI RICERCA:" + ' ' + this.filtri.chiaveDiRicerca);
+         }
+    }
 
   private clearSearchText()
   {
   this.filtri.chiaveDiRicerca = '';
+  console.log("CHIAMATE:"+ ' ' + this.filtri.chiamate + "  INTERVENTI:"+ ' '+ this.filtri.interventi + "  APERTI:"+ ' '+ this.filtri.aperti + "  CHIUSI: "+ ' '+ this.filtri.chiusi + "  INTERNI:" + ' ' + this.filtri.interni + "  ESTERNI:"+ ' '+ this.filtri.esterni + "  PRESIDIATI:"+ ' '+ this.filtri.presidiati + "  NON PRESIDIATI:"+ ' '+ this.filtri.nonPresidiati + "  CHIAVE DI RICERCA:" + ' ' + this.filtri.chiaveDiRicerca);
   }
 }
 
