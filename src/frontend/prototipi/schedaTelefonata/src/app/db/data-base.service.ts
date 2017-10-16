a503d6bf63dd998fead5c0634bb96979c6e67384
Luigi Frasca <luigi,frasca@vigilfuoco.it>
2017-10-16 13:54:17 +0200

    schedatelefonata(prototipo): servizi DB
    aggiunti metodi del servizio db e nel componente che consentono il get ed il post delle informazioni.

--- src/frontend/prototipi/schedaTelefonata/src/app/db/data-base.service.ts
+++ src/frontend/prototipi/schedaTelefonata/src/app/db/data-base.service.ts
@@ -2,12 +2,55 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from "@angular/http";
 import 'rxjs/Rx';
 import { Observable } from "rxjs";
+import { FormChiamataModel } from "app/form-chiamata/form-chiamata.model";
 
 @Injectable()
 export class DataBaseService {
 
   cercaChiamataURL = "http://www.xxxx.com"; //TODO
+  salvaChiamataURL = "http://www.xxxx.com"; //TODO
+
+  chiamataRecuperata: FormChiamataModel;
 
   constructor(private _http: Http) { }
 
+  /**
+   * Questo metodo chiama il webservice che cerca nel db la chiamata in base
+   * all'id fornito e ritorna l'oggetto chiamata.
+   * @param idChiamata 
+   */
+  getChiamata(idChiamata) {
+    return this._http.get(this.cercaChiamataURL).map(
+      response => {
+        {
+          return this.chiamataRecuperata = JSON.parse(JSON.stringify(response.json))
+        };
+      })
+      .catch(error => Observable.throw(error.json()));
+  }
+
+  /**
+   * Questo metodo effettua la chiamata al webservice che 
+   * si occuperà dell'inserimento nel DB.
+   * 
+   * @param nuovaChiamata 
+   */
+  aggiungiChiamata(nuovaChiamata: FormChiamataModel): Observable<FormChiamataModel> {
+    return this._http.post(this.salvaChiamataURL, nuovaChiamata)
+      .map(this.datiEstratti)
+      .catch(this.gestioneErroriObservable);
+  }
+
+  private datiEstratti(res: Response) {
+    let body = res.json();
+    return body.data || {};
+  }
+  private gestioneErroriObservable(error: Response | any) {
+    console.error(error.message || error);
+    return Observable.throw(error.message || error);
+  }
+
 }
+
+
+
