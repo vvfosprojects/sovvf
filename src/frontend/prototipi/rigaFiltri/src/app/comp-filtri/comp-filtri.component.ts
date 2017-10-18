f49816f5aac71d2256bdfc6299656af846311c1f
Fares Daniela <daniela.fares@dipvvf.it>
2017-10-18 11:47:55 +0200

    Eliminato l'hover e configurato il servizio
    Eliminato l'hover che cambia il colore dei pulsanti al passaggio del
    mouse. Configurato il servizio che prende i valori di default dei filtri
    dal servizio fake e visualizza a consolle gli stati dei filtri dopo la
    cattura dell'evento.

--- src/frontend/prototipi/rigaFiltri/src/app/comp-filtri/comp-filtri.component.ts
+++ src/frontend/prototipi/rigaFiltri/src/app/comp-filtri/comp-filtri.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { CompFiltriService } from "./comp-filtri.service";
 import { FiltriRichieste } from "./filtri-richieste.model";
 
 @Component({
@@ -8,54 +9,45 @@ import { FiltriRichieste } from "./filtri-richieste.model";
 })
 
 export class CompFiltriComponent implements OnInit { 
-  
-  private filtri : FiltriRichieste; 
+  private filtri: FiltriRichieste;
+  //private filtriTutti: FiltriRichieste[];
+  private errorMessage: string; 
  
+  @Input() filtro: FiltriRichieste;
+
   @Output() nuovoIntervento = new EventEmitter();
   /*@Output() nuovoFiltro: EventEmitter<FiltriRichieste> = new EventEmitter();*/
 
-  constructor() {
-    this.filtri = new FiltriRichieste(true, true, true, false, true, false, true, true,'');
+  constructor(private filtriRichiesteService: CompFiltriService) {
+    /*this.filtri = new FiltriRichieste(true, true, true, false, true, false, true, true,'');*/
     }
 
   ngOnInit() {
+    this.getFiltri();
    }
    /**
    * Questo metodo viene invocato sul click di una casella di filtraggio disponibile.
    */
   
-   private selFiltri(elemento_filtri : string) 
-   {
-    if (elemento_filtri === "nuova_chiamata") 
-      console.log("Evento Nuova Chiamata");
-    else  {
-      
-          if (elemento_filtri === "chiamate") 
-            this.filtri.chiamate = !this.filtri.chiamate;     
-          else if (elemento_filtri === "interventi") 
-         {
-            this.filtri.interventi = !this.filtri.interventi;
-            if (this.filtri.interventi == false) 
-            {
-            this.filtri.presidiati = true; 
-            this.filtri.nonPresidiati = true;  
-            }
-          }
-          else if (elemento_filtri === "aperti")
-            this.filtri.aperti = !this.filtri.aperti;
-          else if (elemento_filtri === "chiusi")
-            this.filtri.chiusi = !this.filtri.chiusi;
-          else if (elemento_filtri === "interni")
-            this.filtri.interni = !this.filtri.interni;
-          else if (elemento_filtri === "esterni")
-            this.filtri.esterni = !this.filtri.esterni;
-          else if (elemento_filtri === "presidiati")
-            this.filtri.presidiati = !this.filtri.presidiati;
-          else if (elemento_filtri === "nonPresidiati")
-            this.filtri.nonPresidiati = !this.filtri.nonPresidiati;
-           
-          console.log("CHIAMATE:"+ ' ' + this.filtri.chiamate + "  INTERVENTI:"+ ' '+ this.filtri.interventi + "  APERTI:"+ ' '+ this.filtri.aperti + "  CHIUSI: "+ ' '+ this.filtri.chiusi + "  INTERNI:" + ' ' + this.filtri.interni + "  ESTERNI:"+ ' '+ this.filtri.esterni + "  PRESIDIATI:"+ ' '+ this.filtri.presidiati + "  NON PRESIDIATI:"+ ' '+ this.filtri.nonPresidiati + "  CHIAVE DI RICERCA:" + ' ' + this.filtri.chiaveDiRicerca);
-         }
+   /**
+   * Questo metodo esegue la subscription all'observable del servizio di accesso
+   * allo stato dei filtri. In occasione di una nuova pubblicazione, viene assegnato il
+   * vettore this.tuttiIFiltri.
+   */
+  private getFiltri() {
+    this.filtriRichiesteService.getFiltri()
+      .subscribe(
+      filtri_richieste => {
+        this.filtri= filtri_richieste;
+       },
+      error => this.errorMessage = <any>error
+      )
+  }
+
+
+  private selFiltri(elemento_filtri : string) 
+    {
+    this.filtriRichiesteService.getFiltriSelezionati(elemento_filtri, this.filtri)
     }
 
   private clearSearchText()
--- src/frontend/prototipi/rigaFiltri/src/app/comp-filtri/comp-filtri.service.ts
+++ src/frontend/prototipi/rigaFiltri/src/app/comp-filtri/comp-filtri.service.ts
@@ -1,8 +1,57 @@
 import { Injectable } from '@angular/core';
+import { Observable } from "rxjs/Observable";
+import 'rxjs/add/observable/of';
+
+import { FiltriRichieste } from "./filtri-richieste.model";
+
 
 @Injectable()
 export class CompFiltriService {
+  private filtri_richieste : FiltriRichieste = 
+    new FiltriRichieste(
+      true, true, true, false, true, false, true, true,'');
+  
+   constructor() { }
 
-  constructor() { }
+  public getFiltri(): Observable<FiltriRichieste> {
+
+    return Observable.of(this.filtri_richieste);
+  }
+  public getFiltriSelezionati(elemento_filtri : string, valore_filtro : FiltriRichieste)   
+  {
+
+  if (elemento_filtri === "nuova_chiamata") 
+    console.log("Evento Nuova Chiamata");
+  else  {
+    
+        if (elemento_filtri === "chiamate") 
+           valore_filtro.chiamate = !valore_filtro.chiamate;     
+        else if (elemento_filtri === "interventi") 
+       {
+        valore_filtro.interventi = !valore_filtro.interventi;
+          if (valore_filtro.interventi == false) 
+          {
+            valore_filtro.presidiati = true; 
+            valore_filtro.nonPresidiati = true;  
+          }
+        }
+        else if (elemento_filtri === "aperti")
+          valore_filtro.aperti = !valore_filtro.aperti;
+        else if (elemento_filtri === "chiusi")
+          valore_filtro.chiusi = !valore_filtro.chiusi;
+        else if (elemento_filtri === "interni")
+          valore_filtro.interni = !valore_filtro.interni;
+        else if (elemento_filtri === "esterni")
+          valore_filtro.esterni = !valore_filtro.esterni;
+        else if (elemento_filtri === "presidiati")
+          valore_filtro.presidiati = !valore_filtro.presidiati;
+        else if (elemento_filtri === "nonPresidiati")
+          valore_filtro.nonPresidiati = !valore_filtro.nonPresidiati;
+         
+        console.log("CHIAMATE:"+ ' ' + valore_filtro.chiamate + "  INTERVENTI:"+ ' '+ valore_filtro.interventi + "  APERTI:"+ ' '+ valore_filtro.aperti + "  CHIUSI: "+ ' '+ valore_filtro.chiusi + "  INTERNI:" + ' ' + valore_filtro.interni + "  ESTERNI:"+ ' '+ valore_filtro.esterni + "  PRESIDIATI:"+ ' '+ valore_filtro.presidiati + "  NON PRESIDIATI:"+ ' '+ valore_filtro.nonPresidiati + "  CHIAVE DI RICERCA:" + ' ' + valore_filtro.chiaveDiRicerca);
+       }
+      }
 
 }
+
+
