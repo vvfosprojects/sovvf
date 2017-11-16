2fe4276a46c06deef499beefbf5ce3a0e83c4e90
felix448 <felice.deflorio@email.it>
2017-11-16 18:40:21 +0100

    Elenco eventi richiesta
    Associazione dell'icona da visualizzare con il nome della classe evento,
    utilizzando una mappa.

--- src/frontend/prototipi/eventiRichiestaDiAssistenza/src/app/evento-richiesta/evento-richiesta.component.ts
+++ src/frontend/prototipi/eventiRichiestaDiAssistenza/src/app/evento-richiesta/evento-richiesta.component.ts
@@ -9,16 +9,48 @@ import { EventoRichiesta } from './evento-richiesta.model';
 export class EventoRichiestaComponent implements OnInit {
 
   private espanso: boolean = false;
+  private iconaClasseEventoCorrente;
+  iconeClassiEvento: any ;
+  myMap: any ;
   
   @Input() eventoRichiesta: EventoRichiesta;
   @Output() mostraDettaglio: EventEmitter<EventoRichiesta> = new EventEmitter();
   
   @Input() istanteEventoPrecedente: Date;
   @Input() istantePrimoEvento: Date;
-  
+  @Input() nomeClasseEvento;
+
+
   constructor() {  }
 
   ngOnInit() {
+        
+    this.iconeClassiEvento = [
+      ["telefonata","fa-phone-square"]
+      ,["assegnazione_partenza","fa-truck"]
+      ,["uscita_partenza","fa-rocket"]
+      ,["arrivo_partenza_sul_posto","fa-check-square-o"]
+      ,["fine_intervento_della_partenza","fa-handshake-o"]
+      ,["rientro_in_sede_della_partenza","fa-fast-backward"]
+      ,["chiusura_richiesta","fa-close"]
+    ];
+
+    this.myMap = new Map(this.iconeClassiEvento);    
+
+    this.iconaClasseEventoCorrente = this.myMap.get(this.nomeClasseEvento);
+
+    //this.iconaClasseEventoCorrente = myMap.get("telefonata");    
+    //this.iconaClasseEventoCorrente = "fa-phone-square";
+    /*
+    console.log("Felix start"); 
+    console.log(this.nomeClasseEvento); 
+    console.log(Array.from(this.myMap));
+    console.log(this.myMap.get(this.nomeClasseEvento));
+    console.log(this.myMap.get("telefonata"));
+    console.log(this.iconaClasseEventoCorrente); 
+    */
+ 
+ 
   }
 
     /**
