6fcd5e9058cfb455b9bb8634d357db8ef6325e6d
luigi.frasca@vigilfuoco.it <luigi.frasca@laz.dipvvf.it>
2018-03-15 09:37:32 +0100

    Redix implementazione
    Impostato lo stato iniziale di userloginfail.

--- src/frontend/prototipi/cruscottoIntegratoSB/src/app/reducers/userLoginFail.ts
+++ src/frontend/prototipi/cruscottoIntegratoSB/src/app/reducers/userLoginFail.ts
@@ -2,7 +2,7 @@ import { ActionReducer, Action } from '@ngrx/store';
 import * as amount from '../actions/user';
 //import { User } from '../login/_models/user';
 
-export function reducer(state: any, action: amount.UserloginFail) {
+export function reducer(state: any = "Error: Username o password errate", action: amount.UserloginFail) {
     switch (action.type) {
         case amount.USERLOGINFAIL:
             return action.payload;
