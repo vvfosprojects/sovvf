32ff9e6d59c38d39b408f498d992df69777cb7c4
Luigi Frasca <luigi,frasca@vigilfuoco.it>
2018-05-28 15:02:49 +0200

    chat signalr:
    aggiunta hubconnection di signalr per collegarsi al server signalr
    e iscriversi al servizio di chat.
    L'attributo orario del model chat_msg adesso è di tipo Date.

--- src/frontend/prototipi/cruscottoIntegratoSB/src/app/layout/dashboard/components/chat/chat.component.ts
+++ src/frontend/prototipi/cruscottoIntegratoSB/src/app/layout/dashboard/components/chat/chat.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ChatRowComponent } from "../chat-row/chat-row.component";
 import { chat_msg } from "../model/chat_msg";
 import { TimeAgoPipe } from "ngx-moment";
+import { HubConnection } from "@aspnet/signalr";
 import 'moment/locale/de';
 
 
@@ -13,18 +14,36 @@ import 'moment/locale/de';
 export class ChatComponent implements OnInit {
     
     myDate: Date;
+    private _hubConnection: HubConnection;
     
     constructor() {
         this.myDate = new Date();
     }
 
     righe_chat:chat_msg[] = [
-        {messaggio: "ciao1", username: "pippo", orario:"10 minuti fa"},
-        {messaggio: "ciao2", username: "pluto", orario:"20 minuti fa"},
-        {messaggio: "ciao3", username: "paperino", orario:"30 minuti fa"}
+        {messaggio: "ciao1", username: "pippo", orario: new Date()},
+        {messaggio: "ciao2", username: "pluto", orario: new Date()},
+        {messaggio: "ciao3", username: "paperino", orario: new Date()}
     ];
 
     ngOnInit() {
+        this._hubConnection = new HubConnection('http://localhost:5000/chat');
+        
+
+        this._hubConnection
+          .start()
+          .then(() => console.log('Connection started!'))
+          .catch(err => console.log('Error while establishing connection :('));
+    
+          this._hubConnection.on('sendToAll', (nick: string, receivedMessage: string) => {
+            console.log("messaggio ricevuto");
+            const text = `${nick}: ${receivedMessage}`;
+            this.righe_chat.push( {
+                messaggio: receivedMessage,
+                username: nick,
+                orario: new Date()
+            });
+          });      
        
     }
 
