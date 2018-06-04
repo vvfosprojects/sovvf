391e5269da9006dea4474749f029647b5f208dd9
Luigi Frasca <luigi,frasca@vigilfuoco.it>
2018-06-04 14:56:52 +0200

    signalr chat: aggiunto piccolo server
    per la gestione dell'invio messaggi ai client
    registrati.

---
+++ src/frontend/prototipi/cruscottoIntegratoSB/AngularAspNetCoreSignalR/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AngularAspNetCoreSignalR
+{
+    public class ChatHub : Hub
+    {
+        public void SendToAll(string name, string message)
+        {
+            Clients.All.SendAsync("sendToAll", name, message);
+        }
+    }
+}
