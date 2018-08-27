113fb0a1de1d2a80e9fcea9aaaccc32b45db9328
Davide Cappa <cappadavide@gmail.com>
2018-08-27 11:07:40 +0200

    Fix creazione service fake maps

--- src/frontend/prototipi/rigaElencoRichiesteDiAssistenzaV6/src/app/maps-service/maps-service.service.fake.ts
+++ src/frontend/prototipi/rigaElencoRichiesteDiAssistenzaV6/src/app/maps-service/maps-service.service.fake.ts
@@ -7,12 +7,13 @@ import {DescrizioneLocalita} from '../model/descrizione-localita.model';
 })
 export class MapsServiceFake {
 
+    data: RichiestaMarker[];
+
     constructor() {
     }
 
     public getData() {
-        return;
-        const data: RichiestaMarker[] = [
+        return this.data = [
             new RichiestaMarker(1,
                 new DescrizioneLocalita(
                     'Via Cavour, 5',
