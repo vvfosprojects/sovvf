3b831db6cf5c9ec1baf317d82a3221363424c039
Davide Cappa <cappadavide@gmail.com>
2018-08-29 12:38:50 +0200

    Aggiunto index di shared e importazione

--- src/frontend/prototipi/rigaElencoRichiesteDiAssistenzaV6/src/app/app.module.ts
+++ src/frontend/prototipi/rigaElencoRichiesteDiAssistenzaV6/src/app/app.module.ts
@@ -4,22 +4,15 @@ import {PipeModule} from './shared/pipes/pipe.module';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
-
+import * as Shared from './shared/';
 import {AppComponent} from './app.component';
 import {ListaRichiesteComponent} from './lista-richieste/lista-richieste.component';
 import {SintesiRichiesteService} from './lista-richieste/sintesi-richieste-service/sintesi-richieste.service';
 import {SintesiRichiesteServiceFake} from './lista-richieste/sintesi-richieste-service/sintesi-richieste.service.fake';
 import {SintesiRichiestaComponent} from './lista-richieste/sintesi-richiesta/sintesi-richiesta.component';
-
-import {DebounceClickDirective} from './shared/directive/debounce-click';
-import {DebounceKeyUpDirective} from './shared/directive/debounce-keyup';
-
-import {ComponenteComponent} from './shared/components/componente/componente.component';
-import {MezzoComponent} from './shared/components/mezzo/mezzo.component';
 import {FiltriComponent} from './lista-richieste/filtri/filtri.component';
 import {FiltroComponent} from './lista-richieste/filtri/filtro/filtro.component';
 import {RicercaRichiesteComponent} from './lista-richieste/ricerca-richieste/ricerca-richieste.component';
-import {CompetenzaComponent} from './shared/components/competenza/competenza.component';
 
 import {AgmCoreModule} from '@agm/core';
 import {MapsComponent} from './maps/maps.component';
@@ -31,16 +24,20 @@ import {AnimationPipe} from './maps/maps-service/animation.pipe';
 @NgModule({
     declarations: [
         AppComponent,
-        SintesiRichiestaComponent,
         ListaRichiesteComponent,
-        ComponenteComponent,
-        MezzoComponent,
+        SintesiRichiestaComponent,
         FiltriComponent,
         FiltroComponent,
         RicercaRichiesteComponent,
-        CompetenzaComponent,
-        DebounceClickDirective,
-        DebounceKeyUpDirective,
+        // start import of Shared Declarations
+        [
+            Shared.DebounceClickDirective,
+            Shared.DebounceKeyUpDirective,
+            Shared.CompetenzaComponent,
+            Shared.ComponenteComponent,
+            Shared.MezzoComponent
+        ],
+        // end import of Shared Declarations
         MapsComponent,
         AnimationPipe,
     ],
@@ -55,7 +52,7 @@ import {AnimationPipe} from './maps/maps-service/animation.pipe';
         FormsModule
     ],
     providers: [
-        {provide: SintesiRichiesteService, useClass: SintesiRichiesteServiceFake},
+        {provide: SintesiRichiesteService, useClass: SintesiRichiesteService},
         {provide: MapsService, useClass: MapsService},
     ],
     bootstrap: [AppComponent]
