8b7e8788354f1a8fd07ccaed6c8c0b58ebae2107
Davide Cappa <cappadavide@gmail.com>
2018-08-29 17:40:45 +0200

    test launcher con componente maps + componente elencorichieste

--- src/frontend/prototipi/launcherComponent/src/app/app.module.ts
+++ src/frontend/prototipi/launcherComponent/src/app/app.module.ts
@@ -5,6 +5,12 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
 import * as Shared from './shared/';
+import {AgmCoreModule} from '@agm/core';
+import {MapsComponent} from './maps/maps.component';
+import {MapsService} from './maps/maps-service/maps-service.service';
+import {MapsServiceFake} from './maps/maps-service/maps-service.service.fake';
+import {googleApiKey} from './maps/apikey';
+import {AnimationPipe} from './maps/maps-service/animation.pipe';
 import {AppComponent} from './app.component';
 import {ListaRichiesteComponent} from './lista-richieste/lista-richieste.component';
 import {SintesiRichiesteService} from './lista-richieste/sintesi-richieste-service/sintesi-richieste.service';
@@ -30,18 +36,24 @@ import {RicercaRichiesteComponent} from './lista-richieste/ricerca-richieste/ric
             Shared.CompetenzaComponent,
             Shared.ComponenteComponent,
             Shared.MezzoComponent
-        ]
+        ],
         // end import of Shared Declarations
+        MapsComponent,
+        AnimationPipe
     ],
     imports: [
         BrowserModule,
         HttpClientModule,
         NgbModule,
         PipeModule.forRoot(),
+        AgmCoreModule.forRoot({
+            apiKey: googleApiKey.apiKey
+        }),
         FormsModule
     ],
     providers: [
-        {provide: SintesiRichiesteService, useClass: SintesiRichiesteService}
+        {provide: SintesiRichiesteService, useClass: SintesiRichiesteService},
+        {provide: MapsService, useClass: MapsService}
     ],
     bootstrap: [AppComponent]
 })
