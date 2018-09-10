d4781270d062a456c67732fd4f3f9fd84c942e4e
Davide <cappadavide@gmail.com>
2018-09-10 09:10:20 +0200

    migliorato servizio meteo e piccoli fix su box info aggregate

--- src/frontend/prototipi/boxesInformazioniAggregateV6/src/app/boxes/info-aggregate/box-meteo/box-meteo.component.ts
+++ src/frontend/prototipi/boxesInformazioniAggregateV6/src/app/boxes/info-aggregate/box-meteo/box-meteo.component.ts
@@ -11,40 +11,16 @@ export class BoxMeteoComponent implements OnInit {
 
     datimeteo: Meteo;
     /* Dati coordinate fake in attesa di quelle passate dal servizio località utente*/
-    coordinate = {
-        lat: '41.53',
-        lon: '12.30'
-    };
-    // coordinate = {
-    //     lat: '45.28',
-    //     lon: '9.10'
-    // };
+    coordinate = [43.47, 11.14];
 
     constructor(private meteoService: MeteoService) {
     }
 
     ngOnInit() {
-        this.meteoService.getMeteoData(this.coordinate.lat, this.coordinate.lon)
+        this.meteoService.getMeteoData(this.coordinate)
             .subscribe(data => {
-                this.datimeteo = new Meteo(
-                    data.weather[0].description,
-                    data.weather[0].icon,
-                    data.main.humidity,
-                    Math.floor(data.main['temp'] * 10) / 10,
-                    Math.floor(data.wind.speed * 3.6), {
-                        gradi: Math.floor(data.wind['deg']),
-                        cardinali: this.degToCompass(Math.floor(data.wind['deg']))
-                    }
-                );
+                this.datimeteo = data;
             });
     }
 
-    degToCompass(num) {
-        const val = Math.floor((num / 22.5) + 0.5);
-        const arr = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW'];
-        // console.log(num);
-        // console.log(val);
-        // console.log(arr[(val % 16)]);
-        return arr[(val % 16)];
-    }
 }
