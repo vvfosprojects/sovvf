2e47f83d908f4dfbf70770b62ab4c72fe7517c6f
Michele Dragonetti <39093578+notmikenot@users.noreply.github.com>
2018-09-14 18:17:06 +0200

    Navbar: completata closed #1

--- src/frontend/prototipi/launcherComponent/src/app/navbar/navbar.component.ts
+++ src/frontend/prototipi/launcherComponent/src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-navbar',
@@ -6,11 +8,19 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  @Input() user: any;
   @Output() openedSidebar: EventEmitter<any> = new EventEmitter();
+  moment = new Observable<any>((observer) => {
+    setInterval( () => observer.next(moment().format('DD/MM/YYYY, HH:MM:ss')));
+  });
+  time;
 
   constructor() { }
 
   ngOnInit() {
+    this.moment.subscribe(res => {
+      this.time = res;
+    });
   }
 
   openSidebar() {
