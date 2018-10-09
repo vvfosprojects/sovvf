ab954274c50c06f7000bf348db8b9845a1191ba7
Davide <cappadavide@gmail.com>
2018-10-09 09:20:37 +0200

    Componente Maps: aggiornato package.json

--- src/frontend/prototipi/mapsV6/src/environments/environment.ts
+++ src/frontend/prototipi/mapsV6/src/environments/environment.ts
@@ -23,7 +23,7 @@ export const environment = {
                 mezzi: ''
             },
             agm: {
-                key: 'AIzaSyAqr7bgViJbF0ckCnNzg9f64P1drVXVqa8'
+                key: 'AIzaSyDYIMWNyVt1X_30PybcDMTZkFkcSsUytDk'
             }
         },
         rigaElencoRichieste: {
