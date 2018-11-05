543c372989c06e78a6b2842abfe968a3e93da239
Davide <cappadavide@gmail.com>
2018-11-05 15:32:23 +0100

    Componente Boxes: modifica boxes module

--- src/frontend/prototipi/boxesInformazioniAggregateV6/src/app/app.module.ts
+++ src/frontend/prototipi/boxesInformazioniAggregateV6/src/app/app.module.ts
@@ -12,15 +12,7 @@ import {BoxesModule} from './boxes/boxes.module';
  * Shared Module
  */
 import {SharedModule} from './shared/shared.module';
-/**
- * solo per il componente
- */
-import {InfoAggregateService} from './core/service/boxes-service/info-aggregate.service';
-import {InfoAggregateServiceFake} from './core/service/boxes-service/info-aggregate.service.fake';
-import {DispatcherInfoAggregateServiceFake} from './core/dispatcher/dispatcher-boxes.service.fake';
-import {DispatcherInfoAggregateService} from './core/dispatcher/dispatcher-boxes.service';
-import {BoxManagerService} from './core/manager/boxes-manager/box-manager-service.service';
-import {BoxManagerServiceFake} from './core/manager/boxes-manager/box-manager-service.service.fake';
+
 
 @NgModule({
     declarations: [
@@ -34,11 +26,6 @@ import {BoxManagerServiceFake} from './core/manager/boxes-manager/box-manager-se
         BoxesModule,
         SharedModule,
     ],
-    providers: [
-        {provide: BoxManagerService, useClass: BoxManagerServiceFake},
-        {provide: DispatcherInfoAggregateService, useClass: DispatcherInfoAggregateServiceFake},
-        {provide: InfoAggregateService, useClass: InfoAggregateServiceFake}
-        ],
     bootstrap: [AppComponent]
 })
 export class AppModule {
