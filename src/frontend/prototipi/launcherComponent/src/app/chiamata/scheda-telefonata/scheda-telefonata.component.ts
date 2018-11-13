cc193a45b18098bc140c3d4dd69e88c1aa87f6f3
Davide <cappadavide@gmail.com>
2018-11-13 15:47:29 +0100

    Componente Chiamata&Maps: implementato chiamata Marker fix

--- src/frontend/prototipi/launcherComponent/src/app/chiamata/scheda-telefonata/scheda-telefonata.component.ts
+++ src/frontend/prototipi/launcherComponent/src/app/chiamata/scheda-telefonata/scheda-telefonata.component.ts
@@ -5,59 +5,85 @@ import { FilterbarService } from '../../filterbar/filterbar-service/filterbar-se
 import { ClipboardService } from 'ngx-clipboard';
 import { Localita } from 'src/app/shared/model/localita.model';
 import { Coordinate } from 'src/app/shared/model/coordinate.model';
+import * as MapManager from '../../core/manager/maps-manager';
+import { ChiamataMarker } from '../../maps/maps-model/chiamata-marker.model';
+import { MarkerService } from '../../maps/service/marker-service/marker-service.service';
+import { CenterService } from '../../maps/service/center-service/center-service.service';
+import { CentroMappa } from '../../maps/maps-model/centro-mappa.model';
 
 @Component({
-  selector: 'app-scheda-telefonata',
-  templateUrl: './scheda-telefonata.component.html',
-  styleUrls: ['./scheda-telefonata.component.css'],
-  animations: [
-    trigger('hideShowAnimator', [
-      state('true', style({ opacity: 1 })),
-      state('false', style({ opacity: 0 })),
-      transition('0 => 1', animate('.1s')),
-      transition('1 => 0', animate('.1s'))
-    ])
-  ]
+    selector: 'app-scheda-telefonata',
+    templateUrl: './scheda-telefonata.component.html',
+    styleUrls: ['./scheda-telefonata.component.css'],
+    animations: [
+        trigger('hideShowAnimator', [
+            state('true', style({opacity: 1})),
+            state('false', style({opacity: 0})),
+            transition('0 => 1', animate('.1s')),
+            transition('1 => 0', animate('.1s'))
+        ])
+    ]
 })
 export class SchedaTelefonataComponent implements OnInit {
-  coords: Localita;
-  tipologie: any;
-  hideShowAnimator = false;
+    coords: Localita;
+    tipologie: any;
+    hideShowAnimator = false;
 
-  constructor(private tipologieS: TipologieService,
-    private viewService: FilterbarService,
-    private _clipboardService: ClipboardService) { }
+    centroMappa: CentroMappa;
 
-  ngOnInit() {
-    this.tipologieS.getTipologie().subscribe(t => {
-      this.tipologie = t;
-      setTimeout(() => {
-        this.hideShowAnimator = true;
-      }, 1);
-    });
+    constructor(private tipologieS: TipologieService,
+                private viewService: FilterbarService,
+                private _clipboardService: ClipboardService,
+                private chiamataManager: MapManager.ChiamataMarkerManagerService,
+                private markerService: MarkerService,
+                private centerService: CenterService) {
+    }
 
-  }
+    ngOnInit() {
+        this.tipologieS.getTipologie().subscribe(t => {
+            this.tipologie = t;
+            setTimeout(() => {
+                this.hideShowAnimator = true;
+            }, 1);
+        });
 
-  handleAddressChange(result) {
-    this.coords = new Localita(new Coordinate(result.geometry.location.lat(), result.geometry.location.lng()));
-    // console.log(this.coords);
-  }
+        this.centroMappa = this.centerService.centroMappaIniziale;
+        this.centerService.getCentro().subscribe(r => {
+            if (this.coords) {
+                const xyChiamata = [Math.floor(this.coords.coordinate.latitudine * 1000) / 1000, Math.floor(this.coords.coordinate.longitudine * 1000) / 1000];
+                const xyCentro = [Math.floor(r.coordinate.latitudine * 1000) / 1000, Math.floor(r.coordinate.longitudine * 1000) / 1000];
+                if (xyChiamata[0] !== xyCentro[0] && xyChiamata[1] !== xyCentro[1]) {
+                    this.centroMappa = r;
+                }
+            }
+        });
+    }
 
-  copy(lat: number, lng: number) {
-    const copiedText = lat.toString() + ', ' + lng.toString();
-    this._clipboardService.copyFromContent(copiedText);
-    // console.log(copiedText);
-  }
+    handleAddressChange(result) {
+        this.coords = new Localita(new Coordinate(result.geometry.location.lat(), result.geometry.location.lng()));
+        const chiamataCorrente = new ChiamataMarker('RM-004', this.coords);
+        this.chiamataManager.chiamataMarker[0] = chiamataCorrente;
+        this.markerService.chiamata(chiamataCorrente, 'centra');
+    }
 
-  annullaChiamata() {
-    this.hideShowAnimator = false;
-    setTimeout(() => {
-      this.viewService.sendView({
-        richieste: true,
-        mappa: true,
-        split: true,
-        chiamata: false,
-      });
-    }, 100);
-  }
+    copy(lat: number, lng: number) {
+        const copiedText = lat.toString() + ', ' + lng.toString();
+        this._clipboardService.copyFromContent(copiedText);
+        // console.log(copiedText);
+    }
+
+    annullaChiamata() {
+        this.hideShowAnimator = false;
+        setTimeout(() => {
+            this.viewService.sendView({
+                richieste: true,
+                mappa: true,
+                split: true,
+                chiamata: false,
+            });
+        }, 100);
+        const chiamataVuota = new ChiamataMarker('RM-004', new Localita(new Coordinate(null, null)));
+        this.chiamataManager.chiamataMarker[0] = chiamataVuota;
+        this.markerService.chiamata(null, '', this.centroMappa);
+    }
 }
