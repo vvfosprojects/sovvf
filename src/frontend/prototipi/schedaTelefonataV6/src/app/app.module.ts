167cf6b019fc1a86fb4a670aaaf875ccbf1698b8
Michele Dragonetti <micheledragonetti99@gmail.com>
2018-11-13 11:33:42 +0100

    schedaTelefonata: Aggiunto fade-in / fade-out al componente

--- src/frontend/prototipi/schedaTelefonataV6/src/app/app.module.ts
+++ src/frontend/prototipi/schedaTelefonataV6/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { ChiamataModule } from './chiamata/chiamata.module';
 import { SharedModule } from './shared/shared.module';
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     ChiamataModule,
     SharedModule,
     HttpClientModule
