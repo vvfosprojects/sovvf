923429e8fdb18c778ae0024da5d10766da8a4406
mike <micheledragonetti99@gmail.com>
2018-11-22 11:31:29 +0100

    composizionePartenza: Creato modulo 'ComposizionePartenzaModule'

--- src/frontend/prototipiV6/composizionePartenzaV6/src/app/app.module.ts
+++ src/frontend/prototipiV6/composizionePartenzaV6/src/app/app.module.ts
@@ -1,60 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { ComposizionePartenzaComponent } from './composizione-partenza/composizione-partenza.component';
-import { FasterComponent } from './composizione-partenza/composizione-veloce/faster.component';
-import { SlowerComponent } from './composizione-partenza/composizione-avanzata/slower.component';
-import { SintesiRichiestaComponent } from './richieste/lista-richieste/sintesi-richiesta/sintesi-richiesta.component';
-// Module
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { SharedModule } from './shared/shared.module';
-import { PipeModule } from './shared/pipes/pipe.module';
-import { TimeagoModule, TimeagoIntl, TimeagoFormatter, TimeagoCustomFormatter } from 'ngx-timeago';
 
-// Service
-import { DispatcherService } from './core/dispatcher/dispatcher-lista-richieste.service';
-import { DispatcherFakeService } from './core/dispatcher/dispatcher-lista-richieste-fake.service';
-import { ListaRichiesteManagerService } from './core/manager/lista-richieste-manager/lista-richieste-manager.service';
-import { ListaRichiesteManagerServiceFake } from './core/manager/lista-richieste-manager/lista-richieste-manager.service.fake';
-import { SintesiRichiesteService } from './core/service/lista-richieste-service/lista-richieste.service';
-import { SintesiRichiesteServiceFake } from './core/service/lista-richieste-service/lista-richieste.service.fake';
-import { DispatcherCompPartenzaService } from './core/dispatcher/dispatcher-comp-partenza/dispatcher-comp-partenza.service';
-import { DispatcherCompPartenzaFakeService } from './core/dispatcher/dispatcher-comp-partenza/dispatcher-comp-partenza.fake.service';
-import { CompPartenzaManagerService } from './core/manager/comp-partenza-manager/comp-partenza-manager.service';
-import { CompPartenzaManagerServiceFake } from './core/manager/comp-partenza-manager/comp-partenza-manager.service.fake';
-import { CompPartenzaService } from './core/service/comp-partenza-service/comp-partenza.service';
-import { CompPartenzaServiceFake } from './core/service/comp-partenza-service/comp-partenza.service.fake';
-import { NavComponent } from './composizione-partenza/composizione-veloce/nav/nav.component';
+// Modules
+import { ComposizionePartenzaModule } from './composizione-partenza/composizione-partenza.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ComposizionePartenzaComponent,
-    FasterComponent,
-    SlowerComponent,
-    SintesiRichiestaComponent,
-    NavComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    NgbModule,
-    NgbModule.forRoot(),
-    PipeModule.forRoot(),
-    SharedModule.forRoot(),
-    TimeagoModule.forRoot({
-        intl: TimeagoIntl,
-        formatter: { provide: TimeagoFormatter, useClass: TimeagoCustomFormatter }
-    })
-  ],
-  providers: [
-    { provide: DispatcherService, useClass: DispatcherFakeService },
-    { provide: ListaRichiesteManagerService, useClass: ListaRichiesteManagerServiceFake },
-    { provide: SintesiRichiesteService, useClass: SintesiRichiesteServiceFake },
-    
-    { provide: DispatcherCompPartenzaService, useClass: DispatcherCompPartenzaFakeService },
-    { provide: CompPartenzaManagerService, useClass: CompPartenzaManagerServiceFake },
-    { provide: CompPartenzaService, useClass: CompPartenzaServiceFake }
+    ComposizionePartenzaModule
   ],
   bootstrap: [AppComponent]
 })
