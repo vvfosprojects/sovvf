0f400363aae0adde5e1fe2e8d1f6fd3e19b9dffe
Davide Cappa <cappadavide@gmail.com>
2018-11-26 15:49:48 +0100

    Componente Maps: aggiornato con meteo marker chiamata ref. #37

--- src/frontend/prototipiV6/mapsV6/src/app/maps/maps-ui/info-window/info-window.component.ts
+++ src/frontend/prototipiV6/mapsV6/src/app/maps/maps-ui/info-window/info-window.component.ts
@@ -1,78 +1,80 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Meteo} from '../../../shared/model/meteo.model';
-import {RichiestaMarker} from '../../maps-model/richiesta-marker.model';
-import {SedeMarker} from '../../maps-model/sede-marker.model';
-import {MezzoMarker} from '../../maps-model/mezzo-marker.model';
-import {CambioSedeModalComponent} from './cambio-sede-modal/cambio-sede-modal.component';
-import {NgbModal, NgbPopoverConfig, NgbTooltipConfig} from '@ng-bootstrap/ng-bootstrap';
-import {ColoriStatoMezzo} from '../../../shared/helper/_colori';
+import { Component, Input, OnInit } from '@angular/core';
+import { Meteo } from '../../../shared/model/meteo.model';
+import { RichiestaMarker } from '../../maps-model/richiesta-marker.model';
+import { SedeMarker } from '../../maps-model/sede-marker.model';
+import { MezzoMarker } from '../../maps-model/mezzo-marker.model';
+import { CambioSedeModalComponent } from './cambio-sede-modal/cambio-sede-modal.component';
+import { NgbModal, NgbPopoverConfig, NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
+import { ColoriStatoMezzo } from '../../../shared/helper/_colori';
+import { ChiamataMarker } from '../../maps-model/chiamata-marker.model';
 
 @Component({
-    selector: 'app-info-window',
-    templateUrl: './info-window.component.html',
-    styleUrls: ['./info-window.component.css']
+  selector: 'app-info-window',
+  templateUrl: './info-window.component.html',
+  styleUrls: ['./info-window.component.css']
 })
 export class InfoWindowComponent implements OnInit {
 
-    @Input() datiMeteo: Meteo;
-    @Input() richiestaMarker: RichiestaMarker;
-    @Input() sedeMarker: SedeMarker;
-    @Input() mezzoMarker: MezzoMarker;
-    @Input() tipoSedeIcona: string;
-    stato = new ColoriStatoMezzo();
+  @Input() datiMeteo: Meteo;
+  @Input() richiestaMarker: RichiestaMarker;
+  @Input() sedeMarker: SedeMarker;
+  @Input() chiamata: ChiamataMarker;
+  @Input() mezzoMarker: MezzoMarker;
+  @Input() tipoSedeIcona: string;
+  stato = new ColoriStatoMezzo();
 
-    constructor(private _modalService: NgbModal, popoverConfig: NgbPopoverConfig, tooltipConfig: NgbTooltipConfig) {
-        popoverConfig.container = 'body';
-        popoverConfig.placement = 'bottom';
-        popoverConfig.autoClose = 'inside';
-        tooltipConfig.container = 'body';
-        tooltipConfig.placement = 'bottom';
+  constructor(private _modalService: NgbModal, popoverConfig: NgbPopoverConfig, tooltipConfig: NgbTooltipConfig) {
+    popoverConfig.container = 'body';
+    popoverConfig.placement = 'bottom';
+    popoverConfig.autoClose = 'inside';
+    tooltipConfig.container = 'body';
+    tooltipConfig.placement = 'bottom';
+  }
+
+  openModal() {
+    this._modalService.open(CambioSedeModalComponent);
+  }
+
+  ngOnInit() {
+  }
+
+  coloraIcona(nome): any {
+    const colori = [
+      {
+        icon: 'fa fa-fire',
+        color: 'text-danger'
+      },
+      {
+        icon: 'fa fa-exclamation-triangle',
+        color: 'text-warning'
+      },
+      {
+        icon: 'fa fa-medkit',
+        color: 'text-primary'
+      }
+    ];
+
+    const colore = colori.find(x => x.icon === nome);
+    if (nome === undefined || nome === '') {
+      return 'fa fa-exclamation-triangle text-warning';
+    } else if (colore !== undefined) {
+      return nome + ' ' + colore.color;
+    } else {
+      return nome + ' guida';
     }
+  }
 
-    openModal() {
-        this._modalService.open(CambioSedeModalComponent);
-    }
+  vettorePallini(richiesta) {
+    return new Array(richiesta.priorita);
+  }
 
-    ngOnInit() {
-    }
+  vettoreBuchini(richiesta) {
+    const MAX_PRIORITA = 5;
+    return new Array(MAX_PRIORITA - richiesta.priorita);
+  }
 
-    coloraIcona(nome): any {
-        const colori = [
-            {
-                icon: 'fa fa-fire',
-                color: 'text-danger'
-            },
-            {
-                icon: 'fa fa-exclamation-triangle',
-                color: 'text-warning'
-            },
-            {
-                icon: 'fa fa-medkit',
-                color: 'text-primary'
-            }
-        ];
-
-        const colore = colori.find(x => x.icon === nome);
-        if (nome === undefined || nome === '') {
-            return 'fa fa-exclamation-triangle text-warning';
-        } else if (colore !== undefined) {
-            return nome + ' ' + colore.color;
-        } else {
-            return nome + ' guida';
-        }
-    }
-
-    vettorePallini(richiesta) {
-        return new Array(richiesta.priorita);
-    }
-
-    vettoreBuchini(richiesta) {
-        const MAX_PRIORITA = 5;
-        return new Array(MAX_PRIORITA - richiesta.priorita);
-    }
-
-    dettagliMezzo(stato, tipostato, classe) {
-        return this.stato.getColor(stato, tipostato, classe);
-    }
+  dettagliMezzo(stato, tipostato, classe) {
+    return this.stato.getColor(stato, tipostato, classe);
+  }
 
 }
