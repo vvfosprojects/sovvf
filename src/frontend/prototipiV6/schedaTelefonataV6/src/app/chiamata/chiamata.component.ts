41a73f36a1bc1d75e32bbd32bfb6b8b97a1ceeb0
Davide Cappa <cappadavide@gmail.com>
2019-01-04 17:42:46 +0100

    Aggiornamento componenti rif. ea2190e a3c03d2 60da30d

--- src/frontend/prototipiV6/schedaTelefonataV6/src/app/chiamata/chiamata.component.ts
+++ src/frontend/prototipiV6/schedaTelefonataV6/src/app/chiamata/chiamata.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-chiamata',
-  templateUrl: './chiamata.component.html',
-  styleUrls: ['./chiamata.component.css']
+    selector: 'app-chiamata',
+    templateUrl: './chiamata.component.html',
+    styleUrls: ['./chiamata.component.css']
 })
-export class ChiamataComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class ChiamataComponent {
 
 }
