5a533bd03b5f9ec51507d0569b64f7cdd42adf01
Davide Cappa <cappadavide@gmail.com>
2019-01-09 20:53:47 +0100

    ViewService gestisce lo stato dei componenti attivi closed #58

--- src/frontend/launcherComponent/src/app/filterbar/view-service/view-service.service.ts
+++ src/frontend/launcherComponent/src/app/filterbar/view-service/view-service.service.ts
@@ -1,44 +1,141 @@
 import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { ViewInterface } from '../view-mode/view.interface';
+import { ViewInterface, ViewInterfaceButton, ViewInterfaceComposizione, ViewInterfaceLayout, ViewInterfaceMaps } from '../view-mode/view.interface';
 
 @Injectable({
     providedIn: 'root'
 })
 export class ViewService {
 
+    /**
+     * stato iniziale
+     */
     viewState: ViewInterface = {
-        richieste: true,
-        mappa: true,
-        comp_partenza: false,
-        split: true,
-        chiamata: false,
-        columns: ['col-6', 'col-6']
+        components: {
+            richieste: true,
+            mappa: true,
+            chiamata: false,
+            comp_partenza: false,
+        },
+        layout: {
+            split: true,
+            columns: ['col-6', 'col-6'],
+            button: {
+                chiamata: 'btn-outline-success',
+                buttonView: ['btn-outline-secondary', 'btn-secondary', 'btn-outline-secondary']
+            },
+            composizione: {
+                column: 'col-5',
+                disable: false,
+                modalita: 'slower'
+            }
+        },
+        maps: {
+            active: 'richieste'
+        },
     };
-
-    colorButton = ['btn-outline-secondary', 'btn-secondary', 'btn-outline-secondary'];
 
     private subject = new Subject<ViewInterface>();
 
+    static columns(view: ViewInterface): ViewInterfaceLayout {
+        const result: ViewInterfaceLayout = {
+            split: true,
+            columns: ['col-6', 'col-6']
+        };
+
+        if (view.layout.split) {
+            result.columns = ['col-6', 'col-6'];
+            result.split = true;
+        } else {
+            result.split = false;
+            if (view.components.mappa) {
+                result.columns = ['', 'col-12'];
+            } else {
+                result.columns = ['col-12', ''];
+            }
+        }
+
+        return result;
+    }
+
+    static viewMaps(view: ViewInterface): ViewInterfaceMaps {
+        const result: ViewInterfaceMaps = {
+            active: 'richieste'
+        };
+
+        if (view.components.richieste) {
+            result.active = 'richieste';
+        } else if (view.components.chiamata) {
+            result.active = 'chiamata';
+        } else if (view.components.comp_partenza) {
+            result.active = 'composizione';
+        }
+
+        return result;
+    }
+
+    static colorButton(view: ViewInterface): ViewInterfaceButton {
+        const result: ViewInterfaceButton = {
+            chiamata: 'btn-outline-success',
+            buttonView: ['btn-outline-secondary', 'btn-secondary', 'btn-outline-secondary']
+        };
+
+        if (!view.layout.split) {
+            result.buttonView[0] = view.components.richieste ? 'btn-secondary' : 'btn-outline-secondary';
+            result.buttonView[1] = 'btn-outline-secondary';
+            result.buttonView[2] = view.components.mappa ? 'btn-secondary' : 'btn-outline-secondary';
+        }
+
+        result.chiamata = view.components.chiamata ? 'btn-danger' : 'btn-outline-success';
+
+        return result;
+    }
+
+    static composizioneState(view: ViewInterface): ViewInterfaceComposizione {
+        const result: ViewInterfaceComposizione = {
+            column: 'col-5',
+            disable: false,
+            modalita: 'slower'
+        };
+
+        result.disable = !!view.components.comp_partenza;
+        result.column = view.components.comp_partenza ? 'col-6' : 'col-5';
+
+        return result;
+    }
+
+    action(view: ViewInterface): void {
+        this.viewState.layout = ViewService.columns(view);
+        this.viewState.maps = ViewService.viewMaps(view);
+        this.viewState.layout.button = ViewService.colorButton(view);
+        this.viewState.layout.composizione = ViewService.composizioneState(view);
+    }
+
     sendView(view: ViewInterface) {
         this.viewState = view;
-        this._columns(view);
-        this.subject.next(view);
+        this.action(view);
+        this.subject.next(this.viewState);
     }
 
     getView(): Observable<ViewInterface> {
         return this.subject.asObservable();
     }
 
-    _columns(view) {
-        if (view.split) {
-            view.columns = ['col-6', 'col-6'];
-        } else {
-            if (view.mappa) {
-                view.columns = ['', 'col-12'];
-            } else {
-                view.columns = ['col-12', ''];
+    switchView(event, chiamata?) {
+
+        const view: ViewInterface = {
+            components: {
+                richieste: event === 'normale' || event === 'soloRichieste' || chiamata === false,
+                mappa: event !== 'soloRichieste',
+                comp_partenza: event === 'comp_partenza',
+                chiamata: event === 'chiamata' && chiamata === undefined
+            },
+            layout: {
+                split: !(event === 'soloRichieste' || event === 'soloMappa')
             }
-        }
+        };
+
+        this.sendView(view);
     }
+
 }
