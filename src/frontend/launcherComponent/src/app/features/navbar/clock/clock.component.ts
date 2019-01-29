ddd9968f12e1b9948fca4599bf34d3f28f927880
Davide Cappa <cappadavide@gmail.com>
2019-01-29 17:38:35 +0100

    App clock è stateless

--- src/frontend/launcherComponent/src/app/features/navbar/clock/clock.component.ts
+++ src/frontend/launcherComponent/src/app/features/navbar/clock/clock.component.ts
@@ -1,25 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { ClockService } from '../navbar-service/clock-service/clock.service';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-clock',
-  templateUrl: './clock.component.html',
-  styleUrls: ['./clock.component.css']
+    selector: 'app-clock',
+    templateUrl: './clock.component.html',
+    styleUrls: ['./clock.component.css']
 })
-export class ClockComponent implements OnInit, OnDestroy {
+export class ClockComponent {
 
-    private _clockSubscription: Subscription;
-    time: Date;
-
-    constructor(private clockService: ClockService) { }
-
-    ngOnInit(): void {
-        this._clockSubscription = this.clockService.getClock().subscribe(time => this.time = time);
-    }
-
-    ngOnDestroy(): void {
-        this._clockSubscription.unsubscribe();
-    }
+    @Input() time: Date;
 
 }
--- src/frontend/launcherComponent/src/app/features/navbar/navbar.component.ts
+++ src/frontend/launcherComponent/src/app/features/navbar/navbar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output, isDevMode, OnDestroy } from '@
 import { Subscription } from 'rxjs';
 import { TurnoService } from './navbar-service/turno-service/turno.service';
 import { Turno } from './turno/turno.model';
+import { ClockService } from './navbar-service/clock-service/clock.service';
 
 @Component({
     selector: 'app-navbar',
@@ -12,10 +13,13 @@ export class NavbarComponent implements OnInit, OnDestroy {
 
     @Output() openedSidebar = new EventEmitter<any>();
     turno: Turno;
+    time: Date;
     subscription = new Subscription();
 
-    constructor(private _turno: TurnoService) {
+    constructor(private _turno: TurnoService,
+                private _clock: ClockService) {
         this.subscription.add(this._turno.getTurni().subscribe(res => this.turno = res));
+        this.subscription.add(this._clock.getClock().subscribe(time => this.time = time));
     }
 
     ngOnInit() {
