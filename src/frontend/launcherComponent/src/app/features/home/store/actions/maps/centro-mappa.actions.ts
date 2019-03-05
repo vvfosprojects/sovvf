967bf8b6fa3ec3fe81903042cb7fb538e4f1c50c
Davide Cappa <cappadavide@gmail.com>
2019-03-05 09:40:13 +0100

    Aggiunte Action e Selector allo Stato di CentroMappa

--- src/frontend/launcherComponent/src/app/features/home/store/actions/maps/centro-mappa.actions.ts
+++ src/frontend/launcherComponent/src/app/features/home/store/actions/maps/centro-mappa.actions.ts
@@ -1,4 +1,5 @@
 import { CentroMappa } from '../../../maps/maps-model/centro-mappa.model';
+import { Coordinate } from '../../../../../shared/model/coordinate.model';
 
 export class GetCentroMappa {
     static readonly type = '[Centro Mappa] Get Centro Mappa';
@@ -8,5 +9,19 @@ export class SetCentroMappa {
     static readonly type = '[Centro Mappa] Set Centro Mappa';
 
     constructor(public centroMappa: CentroMappa) {
+    }
+}
+
+export class SetZoomCentroMappa {
+    static readonly type = '[Centro Mappa] Set Zoom Centro Mappa';
+
+    constructor(public zoom: number) {
+    }
+}
+
+export class SetCoordCentroMappa {
+    static readonly type = '[Centro Mappa] Set Coordinate Centro Mappa';
+
+    constructor(public coordinate: Coordinate) {
     }
 }
--- src/frontend/launcherComponent/src/app/features/home/store/states/maps/centro-mappa.state.ts
+++ src/frontend/launcherComponent/src/app/features/home/store/states/maps/centro-mappa.state.ts
@@ -1,7 +1,8 @@
 import { Action, NgxsOnInit, Selector, State, StateContext } from '@ngxs/store';
 import { CentroMappa } from '../../../maps/maps-model/centro-mappa.model';
 import { CentroMappaService } from '../../../../../core/service/maps-service';
-import { GetCentroMappa, SetCentroMappa } from '../../actions/maps/centro-mappa.actions';
+import { GetCentroMappa, SetCentroMappa, SetCoordCentroMappa, SetZoomCentroMappa } from '../../actions/maps/centro-mappa.actions';
+import { Coordinate } from '../../../../../shared/model/coordinate.model';
 
 export interface CentroMappaStateModel {
     centroMappa: CentroMappa;
@@ -19,8 +20,18 @@ export const CentroMappaStateDefaults: CentroMappaStateModel = {
 export class CentroMappaState implements NgxsOnInit {
 
     @Selector()
-    static centroMappa(state: CentroMappaStateModel) {
+    static centroMappa(state: CentroMappaStateModel): CentroMappa {
         return state.centroMappa;
+    }
+
+    @Selector()
+    static zoomCentroMappa(state: CentroMappaStateModel): number {
+        return state.centroMappa.zoom;
+    }
+
+    @Selector()
+    static coordCentroMappa(state: CentroMappaStateModel): Coordinate {
+        return state.centroMappa.coordinate;
     }
 
     constructor(private _centro: CentroMappaService) {
@@ -46,4 +57,29 @@ export class CentroMappaState implements NgxsOnInit {
             centroMappa: action.centroMappa
         });
     }
+
+    @Action(SetZoomCentroMappa)
+    setZoomCentroMappa({ getState, patchState }: StateContext<CentroMappaStateModel>, action: SetZoomCentroMappa) {
+        const state = getState();
+        patchState({
+            ...state,
+            centroMappa: {
+                coordinate: state.centroMappa.coordinate,
+                zoom: action.zoom
+            }
+        });
+    }
+
+    @Action(SetCoordCentroMappa)
+    setCoordCentroMappa({ getState, patchState }: StateContext<CentroMappaStateModel>, action: SetCoordCentroMappa) {
+        const state = getState();
+        patchState({
+            ...state,
+            centroMappa: {
+                coordinate: action.coordinate,
+                zoom: state.centroMappa.zoom
+            }
+        });
+    }
+
 }
