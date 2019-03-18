d65365d351ed298a111fcfa4351ae0d329f40037
notmikenot <micheledragonetti99@gmail.com>
2019-03-18 09:04:16 +0100

    Fix posizionamento dropdown dei filtri

--- src/frontend/launcherComponent/src/app/features/home/filterbar/filtri-richieste/filtri-richieste.component.ts
+++ src/frontend/launcherComponent/src/app/features/home/filterbar/filtri-richieste/filtri-richieste.component.ts
@@ -1,62 +1,64 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { VoceFiltro } from './voce-filtro.model';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-filtri-richieste',
-  templateUrl: './filtri-richieste.component.html',
-  styleUrls: ['./filtri-richieste.component.css']
+    selector: 'app-filtri-richieste',
+    templateUrl: './filtri-richieste.component.html',
+    styleUrls: ['./filtri-richieste.component.css']
 })
 export class FiltriRichiesteComponent implements OnInit {
-  @Input() filtri: VoceFiltro[];
-  @Input() filtriSelezionati: VoceFiltro[];
+    @Input() filtri: VoceFiltro[];
+    @Input() filtriSelezionati: VoceFiltro[];
 
-  // Events
-  @Output() filtroSelezionato: EventEmitter<VoceFiltro> = new EventEmitter();
-  @Output() filtroDeselezionato: EventEmitter<VoceFiltro> = new EventEmitter();
-  @Output() filtriReset: EventEmitter<any> = new EventEmitter();
+    // Events
+    @Output() filtroSelezionato: EventEmitter<VoceFiltro> = new EventEmitter();
+    @Output() filtroDeselezionato: EventEmitter<VoceFiltro> = new EventEmitter();
+    @Output() filtriReset: EventEmitter<any> = new EventEmitter();
 
-  categorie: Array<String> = [];
-  categoriaSelezionata = 'Presidiato';
-  filtersSearch = { descrizione: '' };
+    categorie: Array<String> = [];
+    categoriaSelezionata = 'Presidiato';
+    filtersSearch = {descrizione: ''};
 
-  constructor(private modalService: NgbModal) {
-  }
+    constructor(private modalService: NgbModal,
+                dropdownOpts: NgbDropdownConfig) {
+        dropdownOpts.placement = 'bottom-left';
+    }
 
-  ngOnInit() {
-    this.setCategorie(this.filtri);
-  }
+    ngOnInit() {
+        this.setCategorie(this.filtri);
+    }
 
-  setCategorie(filtri: VoceFiltro[]) {
-    let count = -1;
-    filtri.forEach(filtro => {
-      if (this.categorie) {
-        count = this.categorie.indexOf(filtro.categoria);
-      }
-      if (count <= -1) {
-        this.categorie.push(filtro.categoria);
-      }
-    });
-  }
+    setCategorie(filtri: VoceFiltro[]) {
+        let count = -1;
+        filtri.forEach(filtro => {
+            if (this.categorie) {
+                count = this.categorie.indexOf(filtro.categoria);
+            }
+            if (count <= -1) {
+                this.categorie.push(filtro.categoria);
+            }
+        });
+    }
 
-  onSelezioneCategoria(categoria: any) {
-    this.categoriaSelezionata = categoria;
-  }
+    onSelezioneCategoria(categoria: any) {
+        this.categoriaSelezionata = categoria;
+    }
 
-  openFiltersModal(content: any) {
-    this.filtersSearch = { descrizione: '' };
-    this.modalService.open(content, { windowClass: 'xlModal', backdropClass: 'light-blue-backdrop', centered: true });
-  }
+    openFiltersModal(content: any) {
+        this.filtersSearch = {descrizione: ''};
+        this.modalService.open(content, {windowClass: 'xlModal', backdropClass: 'light-blue-backdrop', centered: true});
+    }
 
-  onSelezioneFiltro(filtro: VoceFiltro) {
-    this.filtroSelezionato.emit(filtro);
-  }
+    onSelezioneFiltro(filtro: VoceFiltro) {
+        this.filtroSelezionato.emit(filtro);
+    }
 
-  onDeselezioneFiltro(filtro: VoceFiltro) {
-    this.filtroDeselezionato.emit(filtro);
-  }
+    onDeselezioneFiltro(filtro: VoceFiltro) {
+        this.filtroDeselezionato.emit(filtro);
+    }
 
-  eliminaFiltriAttivi() {
-    this.filtriReset.emit();
-  }
+    eliminaFiltriAttivi() {
+        this.filtriReset.emit();
+    }
 }
