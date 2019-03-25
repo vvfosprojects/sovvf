9915f3bdd65c0e369503b0d7b79a5e80aead6928
Davide Cappa <cappadavide@gmail.com>
2019-03-25 07:35:29 +0100

    Aggiunto controllo per effettuare una nuova richiesta al servizio meteo. Impostato a 5 minuti dall'ultimo click del marker.

--- src/frontend/launcherComponent/src/app/features/home/store/actions/maps/marker-info-window.actions.ts
+++ src/frontend/launcherComponent/src/app/features/home/store/actions/maps/marker-info-window.actions.ts
@@ -2,6 +2,15 @@ import { Coordinate } from '../../../../../shared/model/coordinate.model';
 import { MarkerDatiMeteo } from '../../../maps/maps-model/marker-dati-meteo.interface';
 
 export class GetMarkerDatiMeteo {
+    static readonly type = '[Marker Info Window] Get Meteo Marker';
+    date: Date;
+
+    constructor(public id: string, public coordinate: Coordinate) {
+        this.date = new Date;
+    }
+}
+
+export class GetMarkerDatiMeteoFromApi {
     static readonly type = '[Marker Info Window] Get Meteo Marker from API';
     date: Date;
 
@@ -10,8 +19,15 @@ export class GetMarkerDatiMeteo {
     }
 }
 
-export class SetMarkerDatiMeteo {
-    static readonly type = '[Marker Info Window] Set Marker Dati Meteo';
+export class AddMarkerDatiMeteo {
+    static readonly type = '[Marker Info Window] Add Marker Dati Meteo';
+
+    constructor(public payload: MarkerDatiMeteo) {
+    }
+}
+
+export class UpdateMarkerDatiMeteo {
+    static readonly type = '[Marker Info Window] Update Marker Dati Meteo';
 
     constructor(public payload: MarkerDatiMeteo) {
     }
--- src/frontend/launcherComponent/src/app/features/home/store/states/maps/marker-info-window.state.ts
+++ src/frontend/launcherComponent/src/app/features/home/store/states/maps/marker-info-window.state.ts
@@ -3,7 +3,7 @@ import { patch, append, updateItem } from '@ngxs/store/operators';
 import { MarkerDatiMeteo } from '../../../maps/maps-model/marker-dati-meteo.interface';
 import { MeteoService } from '../../../../../shared/meteo/meteo-service.service';
 import { Meteo } from '../../../../../shared/model/meteo.model';
-import { GetMarkerDatiMeteo, SetMarkerDatiMeteo } from '../../actions/maps/marker-info-window.actions';
+import { GetMarkerDatiMeteo, AddMarkerDatiMeteo, UpdateMarkerDatiMeteo, GetMarkerDatiMeteoFromApi } from '../../actions/maps/marker-info-window.actions';
 
 export interface MarkerInfoWindowStateModel {
     markerDatiMeteo: MarkerDatiMeteo[];
@@ -20,6 +20,12 @@ export const markerInfoWindowStateDefaults: MarkerInfoWindowStateModel = {
 })
 export class MarkerInfoWindowState {
 
+    /**
+     * minuti che devono trascorrere prima di effettuare una nuova richiesta api
+     * per un marker che ha già ricevuto precedentemente i dati del meteo
+     */
+    readonly meteoUpdateTime = 5;
+
     constructor(private meteo: MeteoService) {
     }
 
@@ -29,34 +35,49 @@ export class MarkerInfoWindowState {
     }
 
     @Action(GetMarkerDatiMeteo)
-    getMarkerDatiMeteo({ dispatch }: StateContext<MarkerInfoWindowStateModel>, action: GetMarkerDatiMeteo) {
+    getMarkerDatiMeteo({ getState, dispatch }: StateContext<MarkerInfoWindowStateModel>, action: GetMarkerDatiMeteo) {
+        const state = getState();
+        const index = state.markerDatiMeteo.findIndex(dati => dati.id === action.id);
+        const checkUpdate = !!(index >= 0 && new Date().getTime() - state.markerDatiMeteo[index].date.getTime() > this.meteoUpdateTime * 60000);
+        if (index < 0 || checkUpdate) {
+            dispatch(new GetMarkerDatiMeteoFromApi(action.id, action.coordinate));
+        }
+    }
+
+    @Action(GetMarkerDatiMeteoFromApi)
+    getMarkerDatiMeteoFromApi({ getState, dispatch }: StateContext<MarkerInfoWindowStateModel>, action: GetMarkerDatiMeteoFromApi) {
+        const state = getState();
+        const index = state.markerDatiMeteo.findIndex(dati => dati.id === action.id);
         this.meteo.getMeteoData(action.coordinate).subscribe((response: Meteo) => {
             const markerDatiMeteo: MarkerDatiMeteo = {
                 id: action.id,
                 datiMeteo: response,
                 date: action.date
             };
-            dispatch(new SetMarkerDatiMeteo(markerDatiMeteo));
+            if (index < 0) {
+                dispatch(new AddMarkerDatiMeteo(markerDatiMeteo));
+            } else {
+                dispatch(new UpdateMarkerDatiMeteo(markerDatiMeteo));
+            }
         });
     }
 
-    @Action(SetMarkerDatiMeteo)
-    setMarkerDatiMeteo({ getState, setState }: StateContext<MarkerInfoWindowStateModel>, { payload }: SetMarkerDatiMeteo) {
-        const state = getState();
-        const index = state.markerDatiMeteo.findIndex(dati => dati.id === payload.id);
-        if (index < 0) {
-            setState(
-                patch({
-                    markerDatiMeteo: append([payload])
-                })
-            );
-        } else {
-            setState(
-                patch({
-                    markerDatiMeteo: updateItem<MarkerDatiMeteo>(dati => dati.id === payload.id, payload)
-                })
-            );
-        }
+    @Action(AddMarkerDatiMeteo)
+    addMarkerDatiMeteo({ setState }: StateContext<MarkerInfoWindowStateModel>, { payload }: AddMarkerDatiMeteo) {
+        setState(
+            patch({
+                markerDatiMeteo: append([payload])
+            })
+        );
+    }
+
+    @Action(UpdateMarkerDatiMeteo)
+    updateMarkerDatiMeteo({ setState }: StateContext<MarkerInfoWindowStateModel>, { payload }: UpdateMarkerDatiMeteo) {
+        setState(
+            patch({
+                markerDatiMeteo: updateItem<MarkerDatiMeteo>(dati => dati.id === payload.id, payload)
+            })
+        );
     }
 
 }
