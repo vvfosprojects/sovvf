47b10cd9e72431afeed1b5c54feb920f55a13b75
mike <micheledragonetti99@gmail.com>
2019-05-10 12:48:41 +0200

    ComposizionePartenza: Creati oggetti per le select dei filtri

--- src/frontend/launcherComponent/src/app/features/home/composizione-partenza/shared/filterbar/composizione-filterbar.component.ts
+++ src/frontend/launcherComponent/src/app/features/home/composizione-partenza/shared/filterbar/composizione-filterbar.component.ts
@@ -1,41 +1,93 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-composizione-filterbar',
-  templateUrl: './composizione-filterbar.component.html',
-  styleUrls: ['./composizione-filterbar.component.css']
+    selector: 'app-composizione-filterbar',
+    templateUrl: './composizione-filterbar.component.html',
+    styleUrls: ['./composizione-filterbar.component.css']
 })
 export class ComposizioneFilterbarComponent implements OnInit {
-  generiMezzi = ['APS', 'ABP', 'AG', 'AS'];
-  distaccamenti = ['Roma', 'Frosinone', 'Latina', 'Rieti'];
-  stati = ['In Sede', 'In Rientro', 'In Viaggio', 'Sul Posto'];
+    generiMezzi = [
+        {
+            id: '1',
+            descrizione: 'APS'
+        },
+        {
+            id: '2',
+            descrizione: 'ABP'
+        },
+        {
+            id: '3',
+            descrizione: 'AG'
+        },
+        {
+            id: '4',
+            descrizione: 'AS'
+        }
+    ];
+    stati = [
+        {
+            id: '1',
+            descrizione: 'In Sede'
+        },
+        {
+            id: '2',
+            descrizione: 'In Rientro'
+        },
+        {
+            id: '3',
+            descrizione: 'In Viaggio'
+        },
+        {
+            id: '4',
+            descrizione: 'Sul Posto'
+        }
+    ];
+    distaccamenti = [
+        {
+            id: '1',
+            descrizione: 'Roma'
+        },
+        {
+            id: '2',
+            descrizione: 'Frosinone'
+        },
+        {
+            id: '3',
+            descrizione: 'Latina'
+        },
+        {
+            id: '4',
+            descrizione: 'Rieti'
+        }
+    ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-  iconaStatiClass(stato: string) {
-    let returnClass = '';
-
-    switch (stato) {
-      case 'In Sede':
-        returnClass = 'text-secondary';
-        break;
-      case 'In Viaggio':
-        returnClass = 'text-info';
-        break;
-      case 'In Rientro':
-        returnClass = 'text-primary';
-        break;
-      case 'Sul Posto':
-        returnClass = 'text-success';
-        break;
-
-      default:
-        break;
+    constructor() {
     }
 
-    return returnClass;
-  }
+    ngOnInit() {
+    }
+
+    iconaStatiClass(stato: string) {
+        let returnClass = '';
+
+        switch (stato) {
+            case 'In Sede':
+                returnClass = 'text-secondary';
+                break;
+            case 'In Viaggio':
+                returnClass = 'text-info';
+                break;
+            case 'In Rientro':
+                returnClass = 'text-primary';
+                break;
+            case 'Sul Posto':
+                returnClass = 'text-success';
+                break;
+
+            default:
+                break;
+        }
+
+        return returnClass;
+    }
 }
