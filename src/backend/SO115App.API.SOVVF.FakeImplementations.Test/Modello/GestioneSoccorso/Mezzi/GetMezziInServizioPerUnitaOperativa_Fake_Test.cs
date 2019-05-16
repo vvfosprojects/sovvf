2ad6e34b97bd258f6e46e3a6ed2803ec9148c1d9
Marcello Esposito <esposito.marce@gmail.com>
2019-05-16 14:20:18 +0200

    Mezzi in servizio per UO
    E' implementata una versione fake del servizio di restituzione mezzi per
    unità operativa (interface IGetMezziInServizioPerUnitaOperativa). La
    classe restituisce un'APS e, con certe probabilità, un'APS a tre posti.
    Se si tratta di un'unità operativa Comando VVF, restituisce anche
    autoscala ed autobotte e, con una certa probabilità, una seconda APS.
    
    E' disponibile una serie di unit tests per verificare il funzionamento
    della classe fake.
    
    Per la realizzazione degli unit tests è stato utilizzato un mock del
    servizio IEspandiNodoSuOrganigramma. Per questo è stata inclusa la
    libreria Moq.

---
+++ src/backend/SO115App.API.SOVVF.FakeImplementations.Test/Modello/GestioneSoccorso/Mezzi/GetMezziInServizioPerUnitaOperativa_Fake_Test.cs
@@ -0,0 +1,78 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GetMezziInServizioPerUnitaOperativa_Fake_Test.cs" company="CNVVF">
+// Copyright (C) 2017 - CNVVF
+//
+// This file is part of SOVVF.
+// SOVVF is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// SOVVF is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+//-----------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Moq;
+using NUnit.Framework;
+using SO115App.API.Models.Classi.Organigramma;
+using SO115App.API.Models.Servizi.Infrastruttura.Organigramma;
+using SO115App.API.SOVVF.FakeImplementations.Modello.GestioneSoccorso.Mezzi;
+
+namespace SO115App.API.SOVVF.FakeImplementations.Test.Modello.GestioneSoccorso.Mezzi
+{
+    public class GetMezziInServizioPerUnitaOperativa_Fake_Test
+    {
+        private IEspandiPinNodoSuOrganigramma espandiPinNodoSuOrganigramma_mock;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var mock = new Mock<IEspandiPinNodoSuOrganigramma>();
+            mock.Setup(p => p.Espandi(It.IsAny<IEnumerable<PinNodo>>()))
+                .Returns((IEnumerable<PinNodo> nodi) => { return nodi.Select(n => n.Codice); });
+            espandiPinNodoSuOrganigramma_mock = mock.Object;
+        }
+
+        [Test]
+        public void TraIMezziCheNonSonoDelComandCiEUnaEdUnaSolaAps()
+        {
+            var generatore = new GetMezziInServizioPerUnitaOperativa_Fake(this.espandiPinNodoSuOrganigramma_mock);
+
+            const string codice = "RM.1001";
+            var mezzi = generatore.Get(new[] { new PinNodo(codice, false) });
+
+            var numeroAPS = mezzi.Count(m => m.Genere == "APS");
+
+            Assert.That(numeroAPS, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void TraIMezziDelComandoCiEAlmenoUnaApsUnaEdUnaSolaAbEUnaEdUnaSolaAs()
+        {
+            var generatore = new GetMezziInServizioPerUnitaOperativa_Fake(this.espandiPinNodoSuOrganigramma_mock);
+
+            const string codice = "RM.1000";
+            var mezzi = generatore.Get(new[] { new PinNodo(codice, false) });
+
+            var numeroAPS = mezzi.Count(m => m.Genere == "APS");
+            var numeroAB = mezzi.Count(m => m.Genere == "AB");
+            var numeroAS = mezzi.Count(m => m.Genere == "AS");
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(numeroAPS, Is.GreaterThanOrEqualTo(1));
+                Assert.That(numeroAB, Is.EqualTo(1));
+                Assert.That(numeroAS, Is.EqualTo(1));
+            });
+        }
+    }
+}
