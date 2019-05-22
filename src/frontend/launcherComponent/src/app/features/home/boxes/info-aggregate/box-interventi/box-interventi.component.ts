14f820ec835bbde99d9ef4182facb5d47b9cdae8
Davide Cappa <cappadavide@gmail.com>
2019-05-22 17:52:05 +0200

    Gli indicatori degli interventi lampeggiano ad ogni cambiamento di stato

--- src/frontend/launcherComponent/src/app/features/home/boxes/info-aggregate/box-interventi/box-interventi.component.ts
+++ src/frontend/launcherComponent/src/app/features/home/boxes/info-aggregate/box-interventi/box-interventi.component.ts
@@ -1,16 +1,50 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { BoxInterventi } from '../../boxes-model/box-interventi.model';
 import { BoxClickInterface } from '../../box-interface/box-click-interface';
+import { objectDiff } from '../../../../../shared/helper/function';
+import { setArrow, setBlinking } from '../../../../../shared/helper/function-css';
 
 @Component({
     selector: 'app-box-interventi',
     templateUrl: './box-interventi.component.html',
     styleUrls: ['./box-interventi.component.css']
 })
-export class BoxInterventiComponent {
+export class BoxInterventiComponent implements OnChanges {
 
+    interventiDiff: any;
     @Input() interventi: BoxInterventi;
     @Input() boxClick: BoxClickInterface;
     @Output() clickRichieste = new EventEmitter<string>();
 
+    ngOnChanges(changes: SimpleChanges): void {
+        const interventi = changes['interventi'];
+        if (interventi && interventi.currentValue && interventi.previousValue) {
+            this.interventiDiff = objectDiff({
+                chiamate: interventi.currentValue.chiamate,
+                assegnati: interventi.currentValue.assegnati,
+                presidiati: interventi.currentValue.presidiati,
+                sospesi: interventi.currentValue.sospesi
+            }, {
+                chiamate: interventi.previousValue.chiamate,
+                assegnati: interventi.previousValue.assegnati,
+                presidiati: interventi.previousValue.presidiati,
+                sospesi: interventi.previousValue.sospesi
+            });
+            setTimeout(() => {
+                this.interventiDiff = null;
+            }, 5000);
+        }
+    }
+
+    checkDiff(key: string) {
+        if (this.interventiDiff) {
+            return setBlinking(this.interventiDiff[key]);
+        }
+    }
+
+    realDiff(key: string) {
+        if (this.interventiDiff) {
+            return setArrow(this.interventiDiff[key]);
+        }
+    }
 }
