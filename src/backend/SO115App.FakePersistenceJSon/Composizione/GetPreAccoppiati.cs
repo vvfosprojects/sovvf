dcd297ee3dbd10cf4884ad388c03e57e5420002f
marcosapienza85 <marco_sapienza@hotmail.com>
2019-05-27 11:19:28 +0200

    Gestione Composizione Partenza su progetto FakePersistenceJson

---
+++ src/backend/SO115App.FakePersistenceJSon/Composizione/GetPreAccoppiati.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using SO115App.API.Models.Classi.Composizione;
+using SO115App.Models.Servizi.Infrastruttura.GetPreAccoppiati;
+using SO115App.API.Models.Servizi.CQRS.Queries.GestioneSoccorso.Composizione.PreAccoppiati;
+
+namespace SO115App.FakePersistenceJSon.Composizione
+{
+    public class GetPreAccoppiati : IGetPreAccoppiati
+    {
+        public List<PreAccoppiati> Get(PreAccoppiatiQuery query)
+        {
+            List<PreAccoppiati> preAccoppiati = new List<PreAccoppiati>();
+            //TODO PARTE CHIAMATA DB
+
+            //TODO DA MODIFICARE CON LA CONNESSIONE AL DB PER IL REPERIMENTO DEI DATI DEFINITIVI
+            //DATI FAKE - ORA LI LEGGO DA FILE
+            string filepath = "Fake/PreAccoppiatiComposizione.json";
+            string json;
+            using (StreamReader r = new StreamReader(filepath))
+            {
+                json = r.ReadToEnd();
+            }
+
+            preAccoppiati = JsonConvert.DeserializeObject<List<PreAccoppiati>>(json);
+            List<ComposizioneSquadre> composizioneSquadre = new List<ComposizioneSquadre>();
+            var squadra = new ComposizioneSquadre();
+            var codiceDistaccamento = "";
+            if ((query.Filtro.CodiceDistaccamento != null && query.Filtro.CodiceDistaccamento.Length > 0 && !string.IsNullOrEmpty(query.Filtro.CodiceDistaccamento[0])) || (query.Filtro.CodiceMezzo != null && query.Filtro.CodiceMezzo.Length > 0 && !string.IsNullOrEmpty(query.Filtro.CodiceMezzo[0]))
+                || (query.Filtro.CodiceSquadra != null && query.Filtro.CodiceSquadra.Length > 0) && !string.IsNullOrEmpty(query.Filtro.CodiceSquadra[0]) || (query.Filtro.CodiceStatoMezzo != null && query.Filtro.CodiceStatoMezzo.Length > 0 && string.IsNullOrEmpty(query.Filtro.CodiceStatoMezzo[0])) || (query.Filtro.CodiceTipoMezzo != null && query.Filtro.CodiceTipoMezzo.Length > 0 && string.IsNullOrEmpty(query.Filtro.CodiceTipoMezzo[0])))
+            {
+                if (query.Filtro.CodiceSquadra != null && query.Filtro.CodiceSquadra.Length > 0 && !string.IsNullOrEmpty(query.Filtro.CodiceSquadra[0]))
+                {
+                    string path = "Fake/SquadreComposizione.json";
+                    string jsonSquadre;
+                    using (StreamReader r = new StreamReader(path))
+                    {
+                        jsonSquadre = r.ReadToEnd();
+                    }
+
+                    composizioneSquadre = JsonConvert.DeserializeObject<List<ComposizioneSquadre>>(jsonSquadre);
+                    squadra = composizioneSquadre.Where(x => query.Filtro.CodiceSquadra.Any(x.Squadra.Id.Equals)).FirstOrDefault();
+                    if (squadra != null)
+                    {
+                        codiceDistaccamento = squadra.Squadra.Distaccamento.Codice;
+                        preAccoppiati = preAccoppiati.Where(x => (x.MezzoComposizione.Mezzo.Distaccamento.Codice == codiceDistaccamento)).ToList();
+                    }
+                }
+                if (query.Filtro.CodiceDistaccamento != null && query.Filtro.CodiceDistaccamento.Length > 0 && !string.IsNullOrEmpty(query.Filtro.CodiceDistaccamento[0]))
+                    preAccoppiati = preAccoppiati.Where(x => query.Filtro.CodiceDistaccamento.Any(x.MezzoComposizione.Mezzo.Distaccamento.Codice.Equals)).ToList();
+                if (query.Filtro.CodiceMezzo != null && query.Filtro.CodiceMezzo.Length > 0 && !string.IsNullOrEmpty(query.Filtro.CodiceMezzo[0]))
+                    preAccoppiati = preAccoppiati.Where(x => query.Filtro.CodiceMezzo.Any(x.MezzoComposizione.Mezzo.Codice.Equals)).ToList();
+                if (query.Filtro.CodiceStatoMezzo != null && query.Filtro.CodiceStatoMezzo.Length > 0 && !string.IsNullOrEmpty(query.Filtro.CodiceStatoMezzo[0]))
+                    preAccoppiati = preAccoppiati.Where(x => query.Filtro.CodiceStatoMezzo.Any(x.MezzoComposizione.Mezzo.Stato.Equals)).ToList();
+                if (query.Filtro.CodiceTipoMezzo != null && query.Filtro.CodiceTipoMezzo.Length > 0 && !string.IsNullOrEmpty(query.Filtro.CodiceTipoMezzo[0]))
+                    preAccoppiati = preAccoppiati.Where(x => query.Filtro.CodiceTipoMezzo.Any(x.MezzoComposizione.Mezzo.Genere.Equals)).ToList();
+                return preAccoppiati;
+            }
+            else
+            {
+                return preAccoppiati;
+            }
+        }
+    }
+}
---
+++ src/backend/SO115App.Models/Servizi/Infrastruttura/Composizione/IGetPreAccoppiati.cs
@@ -0,0 +1,13 @@
+﻿using SO115App.API.Models.Classi.Composizione;
+using SO115App.API.Models.Servizi.CQRS.Queries.GestioneSoccorso.Composizione.PreAccoppiati;
+using System.Collections.Generic;
+
+
+namespace SO115App.Models.Servizi.Infrastruttura.GetPreAccoppiati
+{
+    public interface IGetPreAccoppiati
+    {
+        List<PreAccoppiati> Get(PreAccoppiatiQuery query);
+    }
+}
+
