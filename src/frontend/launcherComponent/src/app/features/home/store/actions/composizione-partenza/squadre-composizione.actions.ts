f9a71d05f8f4b4d927d0bb89ebc131e1fcf46480
mike <micheledragonetti99@gmail.com>
2019-05-28 16:34:25 +0200

    Create nuove "Action" per MezzoComposizione e SquadraComposizione

--- src/frontend/launcherComponent/src/app/features/home/store/actions/composizione-partenza/squadre-composizione.actions.ts
+++ src/frontend/launcherComponent/src/app/features/home/store/actions/composizione-partenza/squadre-composizione.actions.ts
@@ -1,8 +1,43 @@
 import { SquadraComposizione } from '../../../composizione-partenza/interface/squadra-composizione-interface';
 
 export class SetListaSquadreComposizione {
-    static readonly type = '[ComposizioneAvanzata] Set Lista Squadre Composizione';
+    static readonly type = '[SquadreComposizione] Set Lista Squadre Composizione';
 
-    constructor(public squadreComposizione: SquadraComposizione[]) {
+    constructor(public squadre: SquadraComposizione[]) {
+    }
+}
+
+export class AddSquadraComposizione {
+    static readonly type = '[SquadreComposizione] Add Squadra Composizione';
+
+    constructor(public squadra: SquadraComposizione) {
+    }
+}
+
+export class RemoveSquadraComposizione {
+    static readonly type = '[SquadreComposizione] Remove Squadra Composizione';
+
+    constructor(public idSquadra: string) {
+    }
+}
+
+export class UpdateSquadraComposizione {
+    static readonly type = '[SquadreComposizione] Update Squadra Composizione';
+
+    constructor(public squadra: SquadraComposizione) {
+    }
+}
+
+export class SelectSquadraComposizione {
+    static readonly type = '[SquadreComposizione] Select Squadra Composizione';
+
+    constructor(public idSquadra: string) {
+    }
+}
+
+export class UnselectSquadraComposizione {
+    static readonly type = '[SquadreComposizione] Unselect Squadra Composizione';
+
+    constructor(public idSquadra: string) {
     }
 }
