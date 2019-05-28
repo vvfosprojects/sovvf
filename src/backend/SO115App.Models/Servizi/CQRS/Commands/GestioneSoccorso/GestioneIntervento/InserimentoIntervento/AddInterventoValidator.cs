2746a583e278210a487942664b9fae45b902c990
Marcello Esposito <esposito.marce@gmail.com>
2019-05-28 10:47:23 +0200

    Handler InserisciTelefonata non più presente
    E' stato eliminato perché l'handler di inserimento telefonata è stato
    riscritto da zero.

--- src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneSoccorso/GestioneIntervento/InserimentoIntervento/AddInterventoValidator.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneSoccorso/GestioneIntervento/InserimentoIntervento/AddInterventoValidator.cs
@@ -8,21 +8,19 @@ namespace DomainModel.CQRS.Commands.AddIntervento
     {
         public IEnumerable<ValidationResult> Validate(AddInterventoCommand command)
         {
-            /// <summary>
-            /// Controlli sui codici
-            /// Se lo stato è 0 - Chiamata non devono essere presenti codici, verranno messi dal BE
-            /// Se lo stato è 1 - Sospesa
-            /// Se lo stato è 2 - Presidiato
-            /// Se lo stato è 3 - Assegnata
-            /// Se lo stato è 4 - Chiusa
-            /// </summary>
+            /*
+            Controlli sui codici
+            Se lo stato è 0 - Chiamata non devono essere presenti codici, verranno messi dal BE
+            Se lo stato è 1 - Sospesa
+            Se lo stato è 2 - Presidiato
+            Se lo stato è 3 - Assegnata
+            Se lo stato è 4 - Chiusa
+            */
 
-            /// <summary>
-            /// Controlli sul richiedente
-            /// </summary>
+            // Controlli sul richiedente
             if (command.Chiamata.Richiedente.Nome.Length > 0)
             {
-                if(string.IsNullOrWhiteSpace(command.Chiamata.Richiedente.Cognome))
+                if (string.IsNullOrWhiteSpace(command.Chiamata.Richiedente.Cognome))
                 {
                     yield return new ValidationResult("E' presente il nome del richiedente ma non il suo cognome");
                 }
