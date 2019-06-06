d8b1bdd4acff4cdf09c1857d29dcc585f16e240a
supix <esposito.marce@gmail.com>
2019-06-06 20:34:29 +0200

    Layer di persistenza in memoria
    E' disponibile un layer di persistenza implementato in memoria. Il layer
    è utilizzato a scopo sviluppo, test e debug, non richiedendo la presenza di
    un RDBMS in fase di sviluppo dei layer di business logic e GUI.
    
    Va aggiunta la configurazione in dependency injection per pluggare
    questo strato in maniera dinamica ed agevole.

---
+++ src/backend/SO115App.FakePersistence.InMemory/Richieste.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using SO115App.API.Models.Classi.Soccorso;
+
+namespace SO115App.FakePersistence.InMemory
+{
+    public class Richieste
+    {
+        private Dictionary<string, RichiestaAssistenza> richieste = new Dictionary<string, RichiestaAssistenza>();
+
+        public Richieste(IEnumerable<RichiestaAssistenza> richieste)
+        {
+            foreach (var richiesta in richieste)
+                this.richieste.Add(richiesta.Id, richiesta);
+        }
+
+        public RichiestaAssistenza GetById(string id)
+        {
+            return this.richieste[id];
+        }
+    }
+}
