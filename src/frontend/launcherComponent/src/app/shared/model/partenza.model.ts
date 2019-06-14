3eed0d8e9793a0200833f6cbce65598a9ad31101
Davide Cappa <cappadavide@gmail.com>
2019-06-14 14:26:09 +0200

    Creato model TurnoPartenza e aggiunto turno a Partenza

--- src/frontend/launcherComponent/src/app/shared/model/partenza.model.ts
+++ src/frontend/launcherComponent/src/app/shared/model/partenza.model.ts
@@ -1,9 +1,11 @@
 import { Mezzo } from './mezzo.model';
 import { Squadra } from './squadra.model';
+import { TurnoPartenza } from './turno-partenza';
 
 export class Partenza {
     constructor(
         public squadre: Squadra[],
-        public mezzo?: Mezzo
+        public mezzo?: Mezzo,
+        public turno?: TurnoPartenza
     ) { }
 }
