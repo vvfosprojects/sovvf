fcf2e28921f79891083888c19d647dede2915ffc
Riccardo Trionfera <riccardo.trionfera@sistinf.it>
2019-06-26 17:14:29 +0200

    Add - Attività Utente per la gestrione degli stati "InLavorazione" e "Prenotato"

---
+++ src/backend/SO115App.Models/Classi/Soccorso/AttivitaUtente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO115App.Models.Classi.Soccorso
+{
+    public class AttivitaUtente
+    {
+        public string Nominativo { get; set; }
+
+        public DateTime DataInizioAttivita { get; set; }
+    }
+}
