a8dde509f69903dd346e8f4130adfcd8f9b5bfbb
Riccardo Trionfera <riccardo.trionfera@sistinf.it>
2019-07-03 15:38:12 +0200

    Add - Aggiunti dei Mapper customizzati.

---
+++ src/backend/SO115App.Mapper/MappingProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using SO115App.API.Models.Classi.Soccorso;
+using SO115App.API.Models.Servizi.CQRS.Command.GestioneSoccorso.Shared;
+using SO115App.API.Models.Servizi.CQRS.Queries.GestioneSoccorso.Shared.SintesiRichiestaAssistenza;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO115App.CustomMapper.Profili
+{
+    public class MappingProfiles : Profile
+    {
+        public MappingProfiles()
+        {
+            CreateMap<RichiestaAssistenza, SintesiRichiesta>().ReverseMap();
+            CreateMap<SintesiRichiesta, Intervento>().ReverseMap();
+        }
+    }
+}
