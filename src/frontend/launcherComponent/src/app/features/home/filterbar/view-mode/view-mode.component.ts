12ede2a85c0818cc22cedb9d2424fc8011286736
Davide Cappa <cappadavide@gmail.com>
2019-07-04 11:24:33 +0200

    Aggiunti tooltip ai tasti di scelta visualizzazione layout

--- src/frontend/launcherComponent/src/app/features/home/filterbar/view-mode/view-mode.component.ts
+++ src/frontend/launcherComponent/src/app/features/home/filterbar/view-mode/view-mode.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AppFeatures } from '../../../../shared/enum/app-features.enum';
+import { NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-view-mode',
@@ -11,6 +12,11 @@ export class ViewModeComponent {
     @Input() colorButtonView = ['btn-outline-secondary', 'btn-secondary', 'btn-outline-secondary'];
     @Output() buttonView = new EventEmitter<AppFeatures>();
     AppFeature = AppFeatures;
+
+
+    constructor(config: NgbTooltipConfig) {
+        config.container = 'body';
+    }
 
     buttonViewMode($event) {
         this.buttonView.emit($event);
