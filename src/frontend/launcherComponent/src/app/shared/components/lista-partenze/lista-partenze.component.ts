ab4a44f279a9009629434425271e426104d46a42
mike <micheledragonetti99@gmail.com>
2019-08-26 12:23:12 +0200

    Fix: contatore delle partenze in sintesi-richiesta

--- src/frontend/launcherComponent/src/app/shared/components/lista-partenze/lista-partenze.component.ts
+++ src/frontend/launcherComponent/src/app/shared/components/lista-partenze/lista-partenze.component.ts
@@ -3,11 +3,8 @@ import { ListaSquadre } from '../../interface/lista-squadre';
 import { ListaSquadrePartenzaComponent } from '../..';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { Partenza } from '../../model/partenza.model';
-import { Mezzo } from '../../model/mezzo.model';
 import { MezzoActionInterface } from '../../interface/mezzo-action.interface';
-import { StatoMezzo } from '../../enum/stato-mezzo.enum';
 import { StatoRichiesta } from '../../enum/stato-richiesta.enum';
-import { StatoMezzoActions } from '../../enum/stato-mezzo-actions.enum';
 
 @Component({
     selector: 'app-lista-partenze',
@@ -33,34 +30,19 @@ export class ListaPartenzeComponent {
             () => console.log('Lista Squadre Partenza Chiusa'));
     }
 
-    onActionMezzo(mezzoAction: MezzoActionInterface) {
-        /* if (this.partenze && this.partenze.length > 0) {
-            let mezziSulPostoCount = 0;
-            let mezziInViaggioCount = 0;
-            this.partenze.forEach((p: Partenza) => {
-                if (p.mezzo.stato === StatoMezzo.SulPosto && p.mezzo.codice !== mezzoAction.mezzo.codice) {
-                    mezziSulPostoCount += 1;
-                } else if (p.mezzo.stato === StatoMezzo.InViaggio && p.mezzo.codice !== mezzoAction.mezzo.codice) {
-                    mezziInViaggioCount += 1;
+    checkNumeroPartenze(partenze: Partenza[]) {
+        let count = 0;
+        if (partenze && partenze.length > 0) {
+            partenze.forEach((p: Partenza) => {
+                if (!p.sganciata) {
+                    count++;
                 }
             });
+        }
+        return count;
+    }
 
-            switch (mezzoAction.action) {
-                case StatoMezzoActions.InRientro:
-                    if (mezziSulPostoCount === 0 && mezziInViaggioCount === 0) {
-                        console.log('La richiesta diventa Chiusa');
-                    } else if (mezziSulPostoCount === 0 && mezziInViaggioCount > 0) {
-                        console.log('La richiesta diventa Assegnata');
-                    } else {
-                        this.actionMezzo.emit(mezzoAction);
-                    }
-                    break;
-
-                default:
-                    this.actionMezzo.emit(mezzoAction);
-                    break;
-            }
-        } */
+    onActionMezzo(mezzoAction: MezzoActionInterface) {
         this.actionMezzo.emit(mezzoAction);
     }
 }
