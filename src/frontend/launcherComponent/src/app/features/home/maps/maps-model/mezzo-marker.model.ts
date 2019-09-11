814cbdbbd7e39e8d314c6e8eb74a3f70323e36cd
Davide Cappa <cappadavide@gmail.com>
2019-09-11 17:12:36 +0200

    Add: aggiunto indirizzo e idRichiesta nel mezzo Marker

--- src/frontend/launcherComponent/src/app/features/home/maps/maps-model/mezzo-marker.model.ts
+++ src/frontend/launcherComponent/src/app/features/home/maps/maps-model/mezzo-marker.model.ts
@@ -1,7 +1,9 @@
 import { Coordinate } from '../../../../shared/model/coordinate.model';
 import { Mezzo } from '../../../../shared/model/mezzo.model';
+import { InfoRichiesta } from '../../../../shared/interface/info-richiesta.interface';
 
 export class MezzoMarker {
+
     constructor(
         /**
          * La stringa dell'indirizzo e le relative coordinate
@@ -11,6 +13,10 @@ export class MezzoMarker {
          * Contiene le proprietà del mezzo
          */
         public mezzo: Mezzo,
+        /**
+         * contiene le info della richiesta sulla quale è assegnato il mezzo
+         */
+        public infoRichiesta?: InfoRichiesta
     ) {
     }
 
