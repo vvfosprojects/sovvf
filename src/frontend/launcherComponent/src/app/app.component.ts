c511d5d04d9d79fc562ef67c18c0dde4432bfdc1
Davide Cappa <cappadavide@gmail.com>
2019-09-18 17:18:37 +0200

    Add: aggiunto metodo che ritorna l'indirizzo locale del browser

--- src/frontend/launcherComponent/src/app/app.component.ts
+++ src/frontend/launcherComponent/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { Component, NgZone, OnDestroy, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { AuthenticationService } from './core/auth/_services';
 import { RoutesPath } from './shared/enum/routes-path.enum';
@@ -11,15 +11,27 @@ import { Utente } from './shared/model/utente.model';
 import { UtenteState } from './features/navbar/store/states/operatore/utente.state';
 import { ClearListaSediNavbar, PatchListaSediNavbar } from './shared/store/actions/sedi-treeview/sedi-treeview.actions';
 import { SediTreeviewState } from './shared/store/states/sedi-treeview/sedi-treeview.state';
-import { NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+declare global {
+    interface Window {
+        RTCPeerConnection: RTCPeerConnection;
+        mozRTCPeerConnection: RTCPeerConnection;
+        webkitRTCPeerConnection: RTCPeerConnection;
+    }
+}
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
+    name = 'Angular';
+    localIp = sessionStorage.getItem('LOCAL_IP');
+
+    private ipRegex = new RegExp(/([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/);
+
 
     subscription = new Subscription();
 
@@ -47,7 +59,8 @@ export class AppComponent implements OnDestroy {
     constructor(private router: Router,
                 private authService: AuthenticationService,
                 private store: Store,
-                private modals: NgbModal) {
+                private modals: NgbModal,
+                private zone: NgZone) {
         router.events.subscribe((val) => {
             if (val instanceof NavigationEnd) {
                 !this.deniedPath.includes(val.urlAfterRedirects.slice(1)) && authService._isLogged() ? this._toggle = true : this._toggle = false;
@@ -80,6 +93,14 @@ export class AppComponent implements OnDestroy {
         }));
     }
 
+    ngOnInit(): void {
+        this.determineLocalIp();
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
+
     _isReady() {
         this.isReady = !!this._signalR && !!this._isLoaded;
         if (!this.isReady) {
@@ -91,7 +112,36 @@ export class AppComponent implements OnDestroy {
         this._opened = !this._opened;
     }
 
-    ngOnDestroy(): void {
-        this.subscription.unsubscribe();
+    private determineLocalIp() {
+        window.RTCPeerConnection = this.getRTCPeerConnection();
+
+        const pc = new RTCPeerConnection({ iceServers: [] });
+        pc.createDataChannel('');
+        pc.createOffer().then(pc.setLocalDescription.bind(pc));
+
+        pc.onicecandidate = (ice) => {
+            this.zone.run(() => {
+                if (!ice || !ice.candidate || !ice.candidate.candidate) {
+                    return;
+                }
+
+                const candidate = this.ipRegex.exec(ice.candidate.candidate);
+                if (candidate && candidate.length > 0) {
+                    this.localIp = this.ipRegex.exec(ice.candidate.candidate)[1];
+                }
+                console.log(ice.candidate['address']);
+                sessionStorage.setItem('LOCAL_IP', this.localIp);
+
+                pc.onicecandidate = () => {
+                };
+                pc.close();
+            });
+        };
+    }
+
+    private getRTCPeerConnection() {
+        return window.RTCPeerConnection ||
+            window.mozRTCPeerConnection ||
+            window.webkitRTCPeerConnection;
     }
 }
