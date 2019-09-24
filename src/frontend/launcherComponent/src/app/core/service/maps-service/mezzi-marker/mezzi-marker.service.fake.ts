8c5564beee5e9671c1ed577be1a15618d7ef0cbd
mike <micheledragonetti99@gmail.com>
2019-09-24 11:48:08 +0200

    Fix: Mezzi Marker Fake

--- src/frontend/launcherComponent/src/app/core/service/maps-service/mezzi-marker/mezzi-marker.service.fake.ts
+++ src/frontend/launcherComponent/src/app/core/service/maps-service/mezzi-marker/mezzi-marker.service.fake.ts
@@ -20,28 +20,28 @@ export class MezziMarkerServiceFake {
 
         this.mezziMarkers = [
             new MezzoMarker(
-                new Coordinate(41.8311007, 12.4686518),
-                new Mezzo('1', 'Autobotte', 'ABP', 'In Viaggio', 1,
-                    new Sede('1', '', null, null, null, null, null), null, 'appartenenza', 'stato', 2, 'efficienza',
+                new Mezzo('1', 'Autobotte', 'ABP', StatoMezzo.InViaggio, 1,
+                    new Sede('1', '',
+                        new Coordinate(41.8311007, 12.4686518), null, null, null, null), null, 'appartenenza', 'stato', 2, 'efficienza',
                     3, 'carburante', 2, 'estinguente', null, 'RM-12842')
             ),
             new MezzoMarker(
-                new Coordinate(41.82699, 12.4879854),
-                new Mezzo('2', 'Autobotte2', 'ABP', 'In Rientro', 1,
-                    new Sede('1', '', null, null, null, null, null),  null, 'appartenenza', 'stato', 2, 'efficienza',
+                new Mezzo('2', 'Autobotte2', 'ABP', StatoMezzo.InRientro, 1,
+                    new Sede('1', '',
+                        new Coordinate(41.82699, 12.4879854), null, null, null, null), null, 'appartenenza', 'stato', 2, 'efficienza',
                     3, 'carburante', 2, 'estinguente', null, 'RM-12840')
             ),
             new MezzoMarker(
-                new Coordinate(41.8531486, 12.5418702),
-                new Mezzo('3', 'Autobotte3', 'ABP', 'In Viaggio'
+                new Mezzo('3', 'Autobotte3', 'ABP', StatoMezzo.InViaggio
                     , 5,
-                    new Sede('1', '', null, null, null, null, null), null, 'appartenenza', 'stato', 2, 'efficienza',
+                    new Sede('1', '',
+                        new Coordinate(41.8531486, 12.5418702), null, null, null, null), null, 'appartenenza', 'stato', 2, 'efficienza',
                     3, 'carburante', 2, 'estinguente', null, 'RM-12841')
             ),
             new MezzoMarker(
-                new Coordinate(41.8935662, 12.5417044),
-                new Mezzo('4', 'Autobotte3', 'ABP', 'Sul Posto', 5,
-                    new Sede('1', '', null, null, null, null, null), null, 'appartenenza', 'stato', 2, 'efficienza',
+                new Mezzo('4', 'Autobotte3', 'ABP', StatoMezzo.SulPosto, 5,
+                    new Sede('1', '',
+                        new Coordinate(41.8935662, 12.5417044), null, null, null, null), null, 'appartenenza', 'stato', 2, 'efficienza',
                     3, 'carburante', 2, 'estinguente', null, 'RM-12839')
             )
         ];
