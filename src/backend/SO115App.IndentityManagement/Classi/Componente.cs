4d9f3f8b3a401fb7a12ded366f4c2910438f10d1
Riccardo Trionfera <riccardo.trionfera@sistinf.it>
2019-09-24 11:46:58 +0200

    ADD - INTEGRAZIONI - Servizio Fake di Identity Management

---
+++ src/backend/SO115App.IndentityManagement/Classi/Componente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SO115App.ApiIndentityManagement
+{
+    public class Componente
+    {
+        /// <summary>
+        ///   Descrizione codice fiscale Componente
+        /// </summary>
+        public string CodiceFiscale { get; set; }
+
+        /// <summary>
+        ///   Descrizione breve qualifica Componente
+        /// </summary>
+        public string DescrizioneQualifica { get; set; }
+
+        /// <summary>
+        ///   Descrizione lunga qualifica Componente
+        /// </summary>
+        public string DescrizioneQualificaLunga { get; set; }
+
+        public string CodiceTurno { get; set; }
+
+        /// <summary>
+        ///   Nominativo Componente
+        /// </summary>
+        public string Nominativo { get; set; }
+    }
+}
---
+++ src/backend/SO115App.IndentityManagement/Controllers/PersonaleController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SO115App.ApiIndentityManagement.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaleController : ControllerBase
+    {
+        [HttpGet("{codiciFiscali}")]
+        public ActionResult<List<Componente>> Get(string codiceSede, string[] ListaCodiciFiscali)
+        {
+            List<Componente> ListaComponenti = new List<Componente>();
+            try
+            {
+                ListaComponenti = ComponentiSquadreService.GetListaComponentiSquadra(codiceSede, ListaCodiciFiscali);
+                return ListaComponenti;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
