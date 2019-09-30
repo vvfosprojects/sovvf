7b887c2157d700ffa9284426e77c364c0187447c
mike <micheledragonetti99@gmail.com>
2019-09-30 15:53:02 +0200

    Add: aggiunta la possibilità di visualizzare gli eventi in modo testuale Chg: modificato template

--- src/frontend/launcherComponent/src/app/features/home/eventi/filtri-eventi-richiesta/filtri-eventi-richiesta.component.ts
+++ src/frontend/launcherComponent/src/app/features/home/eventi/filtri-eventi-richiesta/filtri-eventi-richiesta.component.ts
@@ -3,55 +3,68 @@ import { FiltroTargaMezzo } from '../filtro-targa-mezzo.interface';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-    selector: 'app-filtri-eventi-richiesta',
-    templateUrl: './filtri-eventi-richiesta.component.html',
-    styleUrls: ['./filtri-eventi-richiesta.component.css']
+  selector: 'app-filtri-eventi-richiesta',
+  templateUrl: './filtri-eventi-richiesta.component.html',
+  styleUrls: ['./filtri-eventi-richiesta.component.css']
 })
 export class FiltriEventiRichiestaComponent implements OnChanges {
 
-    @Input() idRichiesta: string;
-    @Input() listaTargaMezzo: FiltroTargaMezzo[];
-    @Input() initValue: string[];
-    @Output() targheSelezionate = new EventEmitter<string[]>();
+  @Input() idRichiesta: string;
+  @Input() listaTargaMezzo: FiltroTargaMezzo[];
+  @Input() initValue: string[];
+  @Input() iconeNomeClasseEvento: boolean;
 
-    form: FormGroup;
+  @Output() targheSelezionate = new EventEmitter<string[]>();
+  @Output() toggleIconeNomeClasseEvento = new EventEmitter<boolean>();
 
-    constructor(private formBuilder: FormBuilder) {
-        this.form = this.formBuilder.group({
-            targheSelezionate: [{ value: '', disabled: true }]
-        });
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      targheSelezionate: [{ value: '', disabled: true }]
+    });
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes && changes.listaTargaMezzo && changes.listaTargaMezzo.currentValue) {
+      const listaTargaMezzo: FiltroTargaMezzo[] = changes.listaTargaMezzo.currentValue;
+      if (listaTargaMezzo.length > 0) {
+        this.targaControl.enable();
+      }
     }
-
-    ngOnChanges(changes: SimpleChanges): void {
-        if (changes && changes.listaTargaMezzo && changes.listaTargaMezzo.currentValue) {
-            const listaTargaMezzo: FiltroTargaMezzo[] = changes.listaTargaMezzo.currentValue;
-            if (listaTargaMezzo.length > 0) {
-                this.targaControl.enable();
-            }
-        }
-        if (changes && changes.initValue && changes.initValue.currentValue) {
-            const initValue: string = changes.initValue.currentValue;
-            if (initValue) {
-                this.targaControl.setValue(initValue);
-            }
-        }
+    if (changes && changes.initValue && changes.initValue.currentValue) {
+      const initValue: string = changes.initValue.currentValue;
+      if (initValue) {
+        this.targaControl.setValue(initValue);
+      }
     }
+  }
 
 
-    get targaControl() {
-        return this.form.controls.targheSelezionate;
+  get targaControl() {
+    return this.form.controls.targheSelezionate;
+  }
+
+  onChange() {
+    this.targheSelezionate.emit(this.targaControl.value);
+  }
+
+  mostraTesto(): string {
+    if (this.listaTargaMezzo && this.listaTargaMezzo.length > 0) {
+      return 'Filtra per mezzo';
+    } else {
+      return 'Non ci sono mezzi da filtrare';
     }
+  }
 
-    onChange() {
-        this.targheSelezionate.emit(this.targaControl.value);
+  getIconaBottone() {
+    let returnIcon = '';
+    if (this.iconeNomeClasseEvento) {
+      returnIcon = 'fa-align-left';
+    } else {
+      returnIcon = 'fa-eye';
     }
-
-    mostraTesto(): string {
-        if (this.listaTargaMezzo && this.listaTargaMezzo.length > 0) {
-            return 'Filtra per mezzo';
-        } else {
-            return 'Non ci sono mezzi da filtrare';
-        }
-    }
+    return returnIcon;
+  }
 
 }
