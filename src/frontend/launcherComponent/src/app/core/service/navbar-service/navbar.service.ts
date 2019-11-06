b51880b13cd88e229c191ad196c55a3a8e011703
Davide Cappa <cappadavide@gmail.com>
2019-11-06 12:08:37 +0100

    Fix: tipicizzato il mapping di AppSettings

--- src/frontend/launcherComponent/src/app/core/service/navbar-service/navbar.service.ts
+++ src/frontend/launcherComponent/src/app/core/service/navbar-service/navbar.service.ts
@@ -3,8 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { catchError, map, retry, tap } from 'rxjs/operators';
 import { handleError } from '../../../shared/helper/handleError';
-import { AppSettings } from '../../../shared/interface/app-settings.interface';
+import { AppSettings, AppSettingsAPI } from '../../../shared/interface/app-settings.interface';
 import { Observable } from 'rxjs';
+import { Tipologia } from '../../../shared/model/tipologia.model';
 
 const API_URL_NAVBAR = environment.apiUrl.navbar;
 
@@ -16,27 +17,33 @@ export class NavbarService {
     }
 
     getNavbar(): Observable<AppSettings> {
-        return this.http.get<AppSettings>(API_URL_NAVBAR).pipe(
-            map((data: any) => {
-                const result = data;
-                result.tipologie.map(tipologia => {
-                    tipologia.codiceDescrizione = `${tipologia.descrizione} (${tipologia.codice})`;
-                });
-                const mapper = e => ({
-                    text: e.nome,
-                    value: e.codice,
-                    children: e.figli.map(mapper)
-                });
-                result.listaSedi = {
-                    text: data.listaSedi.nome,
-                    value: data.listaSedi.codice,
-                    children: data.listaSedi.figli.map(mapper)
-                };
-                return result;
-            }),
+        return this.http.get<AppSettingsAPI>(API_URL_NAVBAR).pipe(
+            map((data: AppSettingsAPI) => mapAppSettings(data)),
             tap(data => console.log('AppSettings from Api:', data)),
             retry(3),
             catchError(handleError)
         );
+
+        function mapAppSettings(data: AppSettingsAPI): AppSettings {
+            const result = {} as AppSettings;
+            result.tipologie = data.tipologie.map((tipologia: Tipologia) => {
+                return {
+                    ...tipologia,
+                    codiceDescrizione: `${tipologia.descrizione} (${tipologia.codice})`
+                };
+            });
+            const mapper = e => ({
+                text: e.nome,
+                value: e.codice,
+                children: e.figli.map(mapper)
+            });
+            result.listaSedi = {
+                text: data.listaSedi.nome,
+                value: data.listaSedi.codice,
+                children: data.listaSedi.figli.map(mapper)
+            };
+            return result;
+        }
     }
 }
+
--- src/frontend/launcherComponent/src/app/shared/interface/app-settings.interface.ts
+++ src/frontend/launcherComponent/src/app/shared/interface/app-settings.interface.ts
@@ -1,7 +1,12 @@
-import { ListaSedi } from './lista-sedi';
+import { ListaSedi, ListaSediAPI } from './lista-sedi';
 import { Tipologia } from '../model/tipologia.model';
 
 export interface AppSettings {
     tipologie: Tipologia[];
     listaSedi: ListaSedi;
+}
+
+export interface AppSettingsAPI {
+    tipologie: Tipologia[];
+    listaSedi: ListaSediAPI;
 }
