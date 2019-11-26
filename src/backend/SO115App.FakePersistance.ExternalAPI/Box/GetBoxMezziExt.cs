469eef5f30338996da4416f1e3271616530b7f5c
Furgoncino <davide.durante@sistinf.it>
2019-11-26 12:28:57 +0100

    Add: create le classi della logica di reperimento dai servizi esterni per i Box

---
+++ src/backend/SO115App.FakePersistance.ExternalAPI/Box/GetBoxMezziExt.cs
@@ -0,0 +1,79 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GetBoxMezzi.cs" company="CNVVF">
+// Copyright (C) 2017 - CNVVF
+//
+// This file is part of SOVVF.
+// SOVVF is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// SOVVF is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using SO115App.API.Models.Classi.Boxes;
+using SO115App.Models.Classi.Utility;
+using SO115App.Models.Servizi.Infrastruttura.Box;
+using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Gac;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SO115App.ExternalAPI.Fake.Box
+{
+    /// <summary>
+    ///   La classe GetBoxMezzi recupera i mezzi utilizzabili dal servizio esterno Gac e aggiorna il
+    ///   box dei mezzi
+    /// </summary>
+    public class GetBoxMezziExt : IGetBoxMezzi
+    {
+        private readonly IGetMezziUtilizzabili _getMezziUtilizzabili;
+
+        public GetBoxMezziExt(IGetMezziUtilizzabili getMezziUtilizzabili)
+        {
+            _getMezziUtilizzabili = getMezziUtilizzabili;
+        }
+
+        /// <summary>
+        ///   Il metodo accetta in firma il codice sede e recupera i mezzi utilizzabili dal servizio
+        ///   esterno Gac e aggiorna i contatori dei mezzi
+        /// </summary>
+        /// <param name="codiceSede">il codice sede</param>
+        /// <returns>BoxMezzi</returns>
+        public BoxMezzi Get(string codiceSede)
+        {
+            var mezzi = new BoxMezzi();
+            var listaCodici = new List<string>
+            {
+                codiceSede
+            };
+
+            var listaMezzi = _getMezziUtilizzabili.Get(listaCodici, "", "");
+
+            mezzi.InSede = listaMezzi.Where(x => x.Stato == Costanti.MezzoInSede)
+                .Select(x => x.Stato)
+                .Count();
+            mezzi.InViaggio = listaMezzi.Where(x => x.Stato == Costanti.MezzoInViaggio)
+                .Select(x => x.Stato)
+                .Count();
+            mezzi.InRientro = listaMezzi.Where(x => x.Stato == Costanti.MezzoInRientro)
+                .Select(x => x.Stato)
+                .Count();
+            mezzi.SulPosto = listaMezzi.Where(x => x.Stato == Costanti.MezzoSulPosto)
+                .Select(x => x.Stato)
+                .Count();
+            mezzi.Istituto = listaMezzi.Where(x => x.Stato == Costanti.MezzoIstituto)
+                .Select(x => x.Stato)
+                .Count();
+            mezzi.InServizio = mezzi.InSede + mezzi.InRientro + mezzi.SulPosto + mezzi.Istituto + mezzi.InViaggio;
+
+            return mezzi;
+        }
+    }
+}
---
+++ src/backend/SO115App.FakePersistance.ExternalAPI/Box/GetBoxPersonaleExt.cs
@@ -0,0 +1,73 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="GetPersonale.cs" company="CNVVF">
+// Copyright (C) 2017 - CNVVF
+//
+// This file is part of SOVVF.
+// SOVVF is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// SOVVF is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using SO115App.API.Models.Classi.Boxes;
+using SO115App.API.Models.Classi.Condivise;
+using SO115App.Models.Servizi.Infrastruttura.Box;
+using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Personale;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SO115App.ExternalAPI.Fake.Box
+{
+    public class GetBoxPersonaleExt : IGetBoxPersonale
+    {
+        private readonly IGetSquadreBySede _getSquadreBySede;
+
+        public GetBoxPersonaleExt(IGetSquadreBySede getSquadreBySede)
+        {
+            _getSquadreBySede = getSquadreBySede;
+        }
+
+        public BoxPersonale Get(string codiceSede)
+        {
+            var personale = new BoxPersonale();
+            var numeroComponenti = 0;
+            var listaFunzionari = new List<Componente>();
+
+            foreach (var turno in _getSquadreBySede.SquadreBySede(codiceSede))
+            {
+                personale.SquadreAssegnate +=
+                turno.ListaSquadre.Count(x => x.Stato == Squadra.StatoSquadra.InViaggio) +
+                turno.ListaSquadre.Count(x => x.Stato == Squadra.StatoSquadra.SulPosto) +
+                turno.ListaSquadre.Count(x => x.Stato == Squadra.StatoSquadra.InRientro);
+                personale.SquadreServizio += turno.ListaSquadre.Count;
+
+                foreach (var squadra in turno.ListaSquadre)
+                {
+                    numeroComponenti += squadra.Componenti.Count;
+                    foreach (var componente in squadra.Componenti)
+                    {
+                        if (componente.TecnicoGuardia1 || componente.TecnicoGuardia2 || componente.CapoTurno ||
+                            componente.FunGuardia)
+                        {
+                            listaFunzionari.Add(componente);
+                        }
+                    }
+                }
+            }
+
+            personale.PersonaleTotale = numeroComponenti;
+            personale.Funzionari = listaFunzionari;
+
+            return personale;
+        }
+    }
+}
