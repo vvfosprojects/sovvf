9c3411b37a6dfd0414c8fc72e568f97cef6b0f50
Furgoncino <davide.durante@sistinf.it>
2019-11-29 12:08:48 +0100

    Add: aggiunta la query per il reperimento delle schede contatto relativa a quella sede.

---
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneSchedeNue/GetSchedeContatto/GetSchedeContattoAuthorizationQueryHandlerDecorator.cs
@@ -0,0 +1,35 @@
+﻿using CQRS.Authorization;
+using CQRS.Queries.Authorizers;
+using SO115App.API.Models.Classi.Autenticazione;
+using SO115App.Models.Classi.Utility;
+using System;
+using System.Collections.Generic;
+using System.Security.Principal;
+using System.Text;
+
+namespace SO115App.Models.Servizi.CQRS.Queries.GestioneSchedeNue.GetSchedeContatto
+{
+    internal class GetSchedeContattoAuthorizationQueryHandlerDecorator : IQueryAuthorizer<GetSchedeContattoQuery, GetSchedeContattoResult>
+    {
+        private readonly IPrincipal _currentUser;
+
+        public GetSchedeContattoAuthorizationQueryHandlerDecorator(IPrincipal currentUser)
+        {
+            _currentUser = currentUser;
+        }
+
+        public IEnumerable<AuthorizationResult> Authorize(GetSchedeContattoQuery query)
+        {
+            var username = _currentUser.Identity.Name;
+
+            if (_currentUser.Identity.IsAuthenticated)
+            {
+                var user = Utente.FindUserByUsername(username);
+                if (user == null)
+                    yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
+            }
+            else
+                yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
+        }
+    }
+}
---
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneSchedeNue/GetSchedeContatto/GetSchedeContattoQuery.cs
@@ -0,0 +1,12 @@
+﻿using CQRS.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO115App.Models.Servizi.CQRS.Queries.GestioneSchedeNue.GetSchedeContatto
+{
+    public class GetSchedeContattoQuery : IQuery<GetSchedeContattoResult>
+    {
+        public string CodiceSede { get; set; }
+    }
+}
