3e5385ba0f021991d66293ba038e2605df641562
Furgoncino <davide.durante@sistinf.it>
2019-11-29 12:07:34 +0100

    Fix: la notification aggiorna la scheda contatto con le modifiche di gestione della scheda

--- src/backend/SO115App.SignalR/Sender/GestioneSchedeContatto/NotificationSetSchedaGestita.cs
+++ src/backend/SO115App.SignalR/Sender/GestioneSchedeContatto/NotificationSetSchedaGestita.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.SignalR;
 using SO115App.Models.Servizi.CQRS.Commands.GestioneSchedeNue.SetSchedaGestita;
 using SO115App.Models.Servizi.CQRS.Queries.GestioneSchedeNue.GetContatoreSchede;
+using SO115App.Models.Servizi.CQRS.Queries.GestioneSchedeNue.GetSchedeContatto;
 using SO115App.Models.Servizi.Infrastruttura.Notification.GestioneSchedeContatto;
 using System.Threading.Tasks;
 
@@ -11,13 +12,15 @@ namespace SO115App.SignalR.Sender.GestioneSchedeContatto
     {
         private readonly IHubContext<NotificationHub> _notificationHubContext;
         private readonly IQueryHandler<GetConteggioSchedeQuery, GetConteggioSchedeResult> _getConteggioSchedeHandler;
+        private readonly IQueryHandler<GetSchedeContattoQuery, GetSchedeContattoResult> _getSchedeContattoHandler;
 
         public NotificationSetSchedaGestita(
             IHubContext<NotificationHub> notificationHubContext,
-            IQueryHandler<GetConteggioSchedeQuery, GetConteggioSchedeResult> getConteggioSchedeHandler)
+            IQueryHandler<GetConteggioSchedeQuery, GetConteggioSchedeResult> getConteggioSchedeHandler, IQueryHandler<GetSchedeContattoQuery, GetSchedeContattoResult> getSchedeContatto)
         {
             _notificationHubContext = notificationHubContext;
             _getConteggioSchedeHandler = getConteggioSchedeHandler;
+            _getSchedeContattoHandler = getSchedeContatto;
         }
 
         public async Task SendNotification(SetSchedaGestitaCommand command)
@@ -26,8 +29,17 @@ namespace SO115App.SignalR.Sender.GestioneSchedeContatto
             {
                 CodiceSede = command.CodiceSede
             };
+
+            var getSchedeContatto = new GetSchedeContattoQuery
+            {
+                CodiceSede = command.CodiceSede
+            };
+
+            var schedaContattoUpdated = _getSchedeContattoHandler.Handle(getSchedeContatto).SchedeContatto.Find(x => x.CodiceScheda.Equals(command.CodiceScheda));
+
             var infoNue = _getConteggioSchedeHandler.Handle(getConteggioSchedeQuery).InfoNue;
             await _notificationHubContext.Clients.Group(command.CodiceSede).SendAsync("NotifyGetContatoriSchedeContatto", infoNue);
+            await _notificationHubContext.Clients.Groups(command.CodiceSede).SendAsync("NotifyGetUpdateSchedaContatto", schedaContattoUpdated);
         }
     }
 }
