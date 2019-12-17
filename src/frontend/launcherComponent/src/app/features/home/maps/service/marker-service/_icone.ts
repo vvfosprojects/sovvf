a9dfaf76f4f678a8a76e51ec7d5cf94dbb202f24
mike <micheledragonetti99@gmail.com>
2019-12-17 17:30:12 +0100

    Aggiunti i marker delle "SchedeContatto" Aggiunta apertura "info-window" all'hover del marker

--- src/frontend/launcherComponent/src/app/features/home/maps/service/marker-service/_icone.ts
+++ src/frontend/launcherComponent/src/app/features/home/maps/service/marker-service/_icone.ts
@@ -100,7 +100,8 @@ export class IconMappe {
 
         this.iconeSpeciali = [
             ['meteo', 'speciali/marker-meteo-32.png'],
-            ['chiamata', 'speciali/chiamata-marker-rosso.png']
+            ['chiamata', 'speciali/chiamata-marker-rosso.png'],
+            ['schedaContatto', 'speciali/scheda-contatto-maker.png']
         ];
         this.mapIconeSpeciali = new Map(this.iconeSpeciali);
     }
