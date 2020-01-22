2235a724194fbc20d0c60dbc21563c742dc51748
Riccardo Trionfera <riccardo.trionfera@sistinf.it>
2020-01-22 17:49:27 +0100

    ADD - Generazione Codice Chiamata e Richiesta su MongoDB

---
+++ src/backend/SO115App.Persistence.MongoDB/GestioneInterventi/GeneraCodiceRichiesta.cs
@@ -0,0 +1,36 @@
+﻿using SO115App.Models.Servizi.Infrastruttura.GestioneSoccorso.GenerazioneCodiciRichiesta;
+using SO115App.Persistence.MongoDB.GestioneInterventi.Utility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO115App.Persistence.MongoDB.GestioneInterventi
+{
+    public class GeneraCodiceRichiesta : IGeneraCodiceRichiesta
+    {
+        private readonly GetMaxCodice _getMaxCodice;
+
+        public GeneraCodiceRichiesta(GetMaxCodice getMaxCodice)
+        {
+            _getMaxCodice = getMaxCodice;
+        }
+
+        public string Genera(string codiceProvincia, int anno)
+        {
+            int ultimeDueCifreAnno = anno % 100;
+            int nuovoNumero = _getMaxCodice.GetMax();
+            string returnFormatString = string.Format("{0}{1}{2:D5}", codiceProvincia.Split('.')[0], ultimeDueCifreAnno, nuovoNumero);
+            return returnFormatString;
+        }
+
+        public string GeneraCodiceChiamata(string codiceProvincia, int anno)
+        {
+            int ultimeDueCifreAnno = anno % 100;
+            int giorno = DateTime.UtcNow.Day;
+            int mese = DateTime.UtcNow.Month;
+            int nuovoNumero = _getMaxCodice.GetMaxCodiceChiamata();
+            string returnString = string.Format("{0}{1}{2}{3}{4:D5}", codiceProvincia.Split('.')[0], giorno, mese, ultimeDueCifreAnno, nuovoNumero);
+            return returnString;
+        }
+    }
+}
---
+++ src/backend/SO115App.Persistence.MongoDB/GestioneInterventi/Utility/GetMaxCodice.cs
@@ -0,0 +1,55 @@
+﻿using MongoDB.Driver;
+using Persistence.MongoDB;
+using SO115App.API.Models.Classi.Soccorso;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SO115App.Persistence.MongoDB.GestioneInterventi.Utility
+{
+    public class GetMaxCodice
+    {
+        private readonly DbContext _dbContext;
+
+        public GetMaxCodice(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public int GetMax()
+        {
+            int MaxIdSintesi;
+
+            var ListaRichieste = _dbContext.RichiestaAssistenzaCollection.Find(Builders<RichiestaAssistenza>.Filter.Empty).ToList();
+
+            if (ListaRichieste.FindAll(x => x.CodRichiesta != null).Count > 0)
+            {
+                var idPRov = ListaRichieste.FindAll(x => x.CodRichiesta != null).OrderByDescending(x => x.CodRichiesta).FirstOrDefault().CodRichiesta;
+
+                MaxIdSintesi = Convert.ToInt16(idPRov.Substring(idPRov.Length - 5)) + 1;
+            }
+            else
+                MaxIdSintesi = 0;
+
+            return MaxIdSintesi;
+        }
+
+        public int GetMaxCodiceChiamata()
+        {
+            int MaxIdSintesi;
+
+            var ListaRichieste = _dbContext.RichiestaAssistenzaCollection.Find(Builders<RichiestaAssistenza>.Filter.Empty).ToList();
+
+            if (ListaRichieste != null)
+            {
+                string codice = ListaRichieste.OrderByDescending(x => x.Codice).FirstOrDefault().Codice;
+                MaxIdSintesi = Convert.ToInt16(codice.Substring(codice.Length - 5)) + 1;
+            }
+            else
+                MaxIdSintesi = 0;
+
+            return MaxIdSintesi;
+        }
+    }
+}
