b2a763d14bc27ddf8447ea7a3d782f00dfd059a1
Furgoncino <davide.durante@sistinf.it>
2020-01-28 09:57:52 +0100

    Fix: corretta dipendenza su GetCentroMappa

--- src/backend/SO115App.Persistence.MongoDB/Marker/GetCentroMappa.cs
+++ src/backend/SO115App.Persistence.MongoDB/Marker/GetCentroMappa.cs
@@ -1,9 +1,6 @@
 ﻿using SO115App.API.Models.Classi.Geo;
-using SO115App.ExternalAPI.Fake.Uos;
 using SO115App.Models.Servizi.Infrastruttura.Marker;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace SO115App.Persistence.MongoDB.Marker
 {
