a8a85bcc9aae2ffde2a71ad38340602c782b1ba6
Davide Cappa <cappadavide@gmail.com>
2020-02-04 16:10:43 +0100

    Chg: refactoring del metodo AreaMappaFiltrata e aggiunto filtroSchedeContatto

--- src/frontend/launcherComponent/src/app/core/service/maps-service/schede-contatto-marker/schede-contatto-marker.service.ts
+++ src/frontend/launcherComponent/src/app/core/service/maps-service/schede-contatto-marker/schede-contatto-marker.service.ts
@@ -5,6 +5,8 @@ import { Observable } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import { handleError } from '../../../../shared/helper/handleError';
 import { environment } from '../../../../../environments/environment';
+import { AreaMappaFiltrata } from '../../../../shared/helper/query-helper';
+import { FiltroSchedeContatto } from '../../../../features/home/maps/maps-model/filtro-schede-contatto';
 
 const API_MARKER = environment.apiUrl.markers;
 
@@ -14,8 +16,8 @@ export class SchedeContattoMarkerService {
     constructor(private http: HttpClient) {
     }
 
-    getSchedeContattoMarkers(areaMappa: AreaMappa): Observable<any> {
-        return this.http.post(`${API_MARKER}/GetSchedeContatto`, areaMappa).pipe(
+    getSchedeContattoMarkers(areaMappa: AreaMappa, filtroSchedeContatto?: FiltroSchedeContatto): Observable<any> {
+        return this.http.post(`${API_MARKER}/GetSchedeContatto`, AreaMappaFiltrata(areaMappa, { filtroSchedeContatto })).pipe(
             retry(3),
             catchError(handleError));
     }
