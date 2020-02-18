5bf3b48fe71786e63834c498a0826afc0e51c4b8
Riccardo Trionfera <riccardo.trionfera@sistinf.it>
2020-02-18 18:01:17 +0100

    Add - Aggiunta automatica delle 3 competenze nell'inserimento di una chiamata

--- src/backend/SO115App.Models/Servizi/Infrastruttura/SistemiEsterni/Competenze/IGetCompetenzeRichiesta.cs
+++ src/backend/SO115App.Models/Servizi/Infrastruttura/SistemiEsterni/Competenze/IGetCompetenzeRichiesta.cs
@@ -7,6 +7,6 @@ namespace SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Competenze
 {
     public interface IGetCompetenzeRichiesta
     {
-        List<CompetenzeRichiesta> GetCompetenzeRichiesta(string CodSede, string NomeVia, string Civico);
+        CompetenzeRichiesta GetCompetenzeRichiesta(string CodSede, string NomeVia, string Civico);
     }
 }
