a5c8b9379edbf0c1a9473ef64bd82f112d71c7fe
Davide Cappa <cappadavide@gmail.com>
2020-03-10 16:31:06 +0100

    Fix: modificata interface AppSetting di navbar controller, in aggiunta alla listaSedi, arriva la lista ruoli per l'utente loggato

--- src/frontend/launcherComponent/src/app/shared/interface/app-settings.interface.ts
+++ src/frontend/launcherComponent/src/app/shared/interface/app-settings.interface.ts
@@ -1,7 +1,9 @@
 import { ListaSedi, ListaSediAPI } from './lista-sedi';
+import { Ruolo } from '../model/utente.model';
 
 export interface AppSettings {
     listaSedi: ListaSedi;
+    listaRuoli: Ruolo[];
 }
 
 export interface AppSettingsAPI {
