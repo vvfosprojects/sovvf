4446ba304c1a05aa5439c931a925ce66a7c75fb6
Davide <cappadavide@gmail.com>
2020-03-11 19:30:16 +0100

    Fix: aggiunto patch listaSediNavbar anche al cambio di login

--- src/frontend/launcherComponent/src/app/app.component.ts
+++ src/frontend/launcherComponent/src/app/app.component.ts
@@ -70,6 +70,7 @@ export class AppComponent implements OnDestroy {
         this.subscription.add(this.offsetTime$.subscribe((serverTime: number) => OFFSET_SYNC_TIME.unshift(serverTime)));
         this.subscription.add(this.user$.subscribe((user: Utente) => {
             this.user = user;
+            this.listeSediLoaded && this.store.dispatch(new PatchListaSediNavbar([ user.sede.codice ]));
         }));
         this.subscription.add(this.listeSediLoaded$.subscribe((r: boolean) => {
             this.listeSediLoaded = r;
