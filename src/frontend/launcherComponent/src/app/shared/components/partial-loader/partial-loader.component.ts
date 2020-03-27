8154e71b1d5112581517e87261c8cb8e2bfc7a4a
notmikenot <micheledragonetti99@gmail.com>
2020-03-27 09:59:01 +0100

    ListaRichieste: migliorato loader rif. issue #82 Creato componente shared "PartialLoader"

---
+++ src/frontend/launcherComponent/src/app/shared/components/partial-loader/partial-loader.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-partial-loader',
+  templateUrl: './partial-loader.component.html',
+  styleUrls: ['./partial-loader.component.css']
+})
+export class PartialLoaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
