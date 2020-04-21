aab3be2e49ee9a3294c8082c942c9d294700f623
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2020-04-21 15:28:31 +0200

    Fix - Url Origin per Cors

--- src/backend/SO115App.API/Startup.cs
+++ src/backend/SO115App.API/Startup.cs
@@ -46,7 +46,7 @@ namespace SO115App.API
     public class Startup
     {
         private readonly Container container = new Container();
-        private readonly string MyAllowSpecificOrigins = "https://sovvf-test.dipvvf.it/";
+        private readonly string MyAllowSpecificOrigins = "https://sovvf-test.dipvvf.it";
 
         public Startup(IConfiguration configuration)
         {
