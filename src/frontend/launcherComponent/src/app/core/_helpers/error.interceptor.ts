eafacbedfb5d56b177e052bca851fb4fa586e4c0
notmikenot <micheledragonetti99@gmail.com>
2020-05-13 16:51:13 +0200

    Gestione errore 404 su "ErrorInterceptor"

--- src/frontend/launcherComponent/src/app/core/_helpers/error.interceptor.ts
+++ src/frontend/launcherComponent/src/app/core/_helpers/error.interceptor.ts
@@ -37,7 +37,7 @@ export class ErrorInterceptor implements HttpInterceptor {
                 this.store.dispatch(new ShowToastr(ToastrType.Error, 'Errore', err.error));
             }
 
-            if ([503].indexOf(err.status) !== -1) {
+            if ([404].indexOf(err.status) !== -1) {
                 this.store.dispatch(new ShowToastr(ToastrType.Error, 'Servizio non disponibile', err.error));
             }
 
