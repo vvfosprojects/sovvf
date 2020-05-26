0eb77da7f7857421d39d9e390f792f2ff94aef67
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2020-05-26 17:19:03 +0200

    Fix - Find Server IP

--- src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneUtente/Login/LogInQueryHandler.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneUtente/Login/LogInQueryHandler.cs
@@ -19,7 +19,10 @@
 //-----------------------------------------------------------------------
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
 using System.Security.Claims;
 using System.Text;
 using CQRS.Queries;
@@ -79,12 +82,55 @@ namespace SO115App.API.Models.Servizi.CQRS.Queries.GestioneUtente.LogIn
             var token = tokenHandler.CreateToken(tokenDescriptor);
 
             utente.Token = tokenHandler.WriteToken(token);
-            utente.Machine = Server.HostName;
+            utente.Machine = GetLocalIpAddress();
 
             return new LogInResult()
             {
                 User = utente
             };
+        }
+
+        public static string GetLocalIpAddress()
+        {
+            UnicastIPAddressInformation mostSuitableIp = null;
+            var networkInterfaces = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (var network in networkInterfaces)
+            {
+                if (network.OperationalStatus != OperationalStatus.Up)
+                    continue;
+
+                var properties = network.GetIPProperties();
+                if (properties.GatewayAddresses.Count == 0)
+                    continue;
+
+                foreach (var address in properties.UnicastAddresses)
+                {
+                    if (address.Address.AddressFamily != AddressFamily.InterNetwork)
+                        continue;
+
+                    if (IPAddress.IsLoopback(address.Address))
+                        continue;
+
+                    if (!address.IsDnsEligible)
+                    {
+                        if (mostSuitableIp == null)
+                            mostSuitableIp = address;
+                        continue;
+                    }
+
+                    // The best IP is the IP got from DHCP server
+                    if (address.PrefixOrigin != PrefixOrigin.Dhcp)
+                    {
+                        if (mostSuitableIp == null || !mostSuitableIp.IsDnsEligible)
+                            mostSuitableIp = address;
+                        continue;
+                    }
+                    return address.Address.ToString();
+                }
+            }
+            return mostSuitableIp != null
+                ? mostSuitableIp.Address.ToString()
+                : "";
         }
     }
 }
