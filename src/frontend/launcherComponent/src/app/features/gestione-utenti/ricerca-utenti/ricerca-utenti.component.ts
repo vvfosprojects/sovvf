e7f1d58c29881ee4ac408f3f6e58fe02d5bb494c
notmikenot <micheledragonetti99@gmail.com>
2020-05-26 12:54:46 +0200

    Continuo implementazione "Filtro Sede" su "Gestione Utenti"

--- src/frontend/launcherComponent/src/app/features/gestione-utenti/ricerca-utenti/ricerca-utenti.component.ts
+++ src/frontend/launcherComponent/src/app/features/gestione-utenti/ricerca-utenti/ricerca-utenti.component.ts
@@ -3,13 +3,19 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
     selector: 'app-ricerca-utenti',
     templateUrl: './ricerca-utenti.component.html',
-    styleUrls: [ './ricerca-utenti.component.css' ]
+    styleUrls: ['./ricerca-utenti.component.css']
 })
 export class RicercaUtentiComponent {
 
     @Input() loading: boolean;
-    @Input() sediFiltro: string[];
+    @Input() sediFiltro: any[];
+    @Input() sediFiltroSelezionate: string[];
     ricerca: string;
 
     @Output() ricercaChange = new EventEmitter<any>();
+    @Output() filtroChange = new EventEmitter<string>();
+
+    _isSelected(codSede: string) {
+        return this.sediFiltroSelezionate.filter(s => s === codSede).length > 0;
+    }
 }
--- src/frontend/launcherComponent/src/app/features/gestione-utenti/store/actions/ricerca-utenti/ricerca-utenti.actons.ts
+++ src/frontend/launcherComponent/src/app/features/gestione-utenti/store/actions/ricerca-utenti/ricerca-utenti.actons.ts
@@ -1,7 +1,10 @@
+import { Sede } from '../../../../../shared/model/sede.model';
+
 export class SetRicercaUtenti {
     static readonly type = '[RicercaUtenti] Set Ricerca';
 
-    constructor(public ricerca: any) {}
+    constructor(public ricerca: any) {
+    }
 }
 
 export class ClearRicercaUtenti {
@@ -11,9 +14,30 @@ export class ClearRicercaUtenti {
 export class SetSediFiltro {
     static readonly type = '[RicercaUtenti] Set Sedi Filtro';
 
-    constructor(public sedi: string[]) {}
+    constructor(public sedi: any[]) {
+    }
 }
 
 export class ClearSediFiltro {
     static readonly type = '[RicercaUtenti] Clear Sedi Filtro';
+}
+
+export class ReducerSelezioneFiltroSede {
+    static readonly type = '[RicercaUtenti] Reducer Selezione Filtro Sede';
+
+    constructor(public sedeFiltro: string) {
+    }
+}
+export class SetSedeFiltroSelezionato {
+    static readonly type = '[RicercaUtenti] Set Sede Filtro Selezionato';
+
+    constructor(public sedeFiltro: string) {
+    }
+}
+
+export class SetSedeFiltroDeselezionato {
+    static readonly type = '[RicercaUtenti] Clear Sede Filtro Selezionato';
+
+    constructor(public sedeFiltro: string) {
+    }
 }
