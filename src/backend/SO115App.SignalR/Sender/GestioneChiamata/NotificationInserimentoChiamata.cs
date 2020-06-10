26b4049f3373ff94812b42f1dc7744e778a62752
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2020-06-10 14:36:22 +0200

    Fix - LogMatrix

--- src/backend/SO115App.SignalR/Sender/GestioneChiamata/NotificationInserimentoChiamata.cs
+++ src/backend/SO115App.SignalR/Sender/GestioneChiamata/NotificationInserimentoChiamata.cs
@@ -89,6 +89,7 @@ namespace SO115App.SignalR.Sender.GestioneChiamata
             }
 
             var CodSedePerMatrix = sintesi.CodSOCompetente.Split('.')[0];
+            await _notificationHubContext.Clients.Group("RM.1000").SendAsync("NotifyDoppioneChiamataInCorso", $"Il CodSede è {CodSedePerMatrix}");
 
             var GetRoomId = _callMatrix.GetChatRoomID(CodSedePerMatrix).Result;
             if (GetRoomId.Error.Trim().Length == 0)
