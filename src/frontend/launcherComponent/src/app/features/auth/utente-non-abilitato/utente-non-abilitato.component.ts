d48cf792864707a26875f60aa07cd80febafd379
notmikenot <micheledragonetti99@gmail.com>
2020-06-18 12:09:39 +0200

    Chg: creato template per "UtenteNonAbilitato"

--- src/frontend/launcherComponent/src/app/features/auth/utente-non-abilitato/utente-non-abilitato.component.ts
+++ src/frontend/launcherComponent/src/app/features/auth/utente-non-abilitato/utente-non-abilitato.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { CasLogout } from '../store/auth.actions';
 
 @Component({
-  selector: 'app-utente-non-abilitato',
-  templateUrl: './utente-non-abilitato.component.html',
-  styleUrls: ['./utente-non-abilitato.component.css']
+    selector: 'app-utente-non-abilitato',
+    templateUrl: './utente-non-abilitato.component.html',
+    styleUrls: ['./utente-non-abilitato.component.css']
 })
 export class UtenteNonAbilitatoComponent implements OnInit {
 
-  constructor() { }
+    constructor(private store: Store) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    clearCas() {
+        this.store.dispatch(new CasLogout());
+    }
 
 }
