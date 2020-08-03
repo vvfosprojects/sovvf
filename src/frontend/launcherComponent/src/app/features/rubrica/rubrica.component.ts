b32a78c8f75efe066545b9cbe6d825e9b8506739
bek2495 <bekimhoxha2495@gmail.com>
2020-08-03 16:56:22 +0200

    fix: rubrica

--- src/frontend/launcherComponent/src/app/features/rubrica/rubrica.component.ts
+++ src/frontend/launcherComponent/src/app/features/rubrica/rubrica.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Store } from '@ngxs/store';
+import { Store, Select } from '@ngxs/store';
 import { GetEnti, UpdateEnte, DeleteEnte, AddEnte } from 'src/app/shared/store/actions/enti/enti.actions';
+import { PaginationState } from 'src/app/shared/store/states/pagination/pagination.state';
+import { Observable } from 'rxjs';
+import { LoadingState } from 'src/app/shared/store/states/loading/loading.state';
 
 @Component({
   selector: 'app-rubrica',
@@ -8,6 +11,13 @@ import { GetEnti, UpdateEnte, DeleteEnte, AddEnte } from 'src/app/shared/store/a
   styleUrls: ['./rubrica.component.css']
 })
 export class RubricaComponent implements OnInit {
+
+  @Select(PaginationState.pageSize) pageSize$: Observable<number>;
+  pageSize: number;
+  @Select(PaginationState.pageSizes) pageSizes$: Observable<number[]>;
+  @Select(PaginationState.totalItems) totalItems$: Observable<number>;
+  @Select(PaginationState.page) page$: Observable<number>;
+  @Select(LoadingState.loading) loading$: Observable<boolean>;
 
   constructor(private store: Store) {
     this.getEnti();
