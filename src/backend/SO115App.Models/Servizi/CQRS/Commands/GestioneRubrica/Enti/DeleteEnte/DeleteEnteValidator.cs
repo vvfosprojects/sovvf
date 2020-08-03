5e668e5198422104ddeb87258f26c22b261f5457
Francesco d'Angelis <francescodangelis@gmail.com>
2020-08-03 15:19:04 +0200

    Ok validazione delete ente

--- src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneRubrica/Enti/DeleteEnte/DeleteEnteValidator.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneRubrica/Enti/DeleteEnte/DeleteEnteValidator.cs
@@ -1,6 +1,8 @@
 ﻿using CQRS.Commands.Validators;
 using CQRS.Validation;
+using SO115App.Models.Classi.Utility;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SO115App.Models.Servizi.CQRS.Commands.GestioneRubrica.Enti.DeleteEnte
 {
@@ -8,7 +10,10 @@ namespace SO115App.Models.Servizi.CQRS.Commands.GestioneRubrica.Enti.DeleteEnte
     {
         public IEnumerable<ValidationResult> Validate(DeleteEnteCommand command)
         {
-            return new List<ValidationResult>();
+            var lstNumeri = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            if (command.CodiceEnte == "0" || !command.CodiceEnte.All(c => lstNumeri.Contains(c)))
+                yield return new ValidationResult(Costanti.IdRichiestaNonValida);
         }
     }
 }
