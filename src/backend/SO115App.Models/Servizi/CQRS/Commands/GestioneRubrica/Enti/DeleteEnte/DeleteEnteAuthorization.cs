9bbd4aeb8872fb923a57349cecc3b38c37ffeffe
Francesco d'Angelis <francescodangelis@gmail.com>
2020-08-03 15:39:23 +0200

    appoggiata logica autorizzazione delete ente

--- src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneRubrica/Enti/DeleteEnte/DeleteEnteAuthorization.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneRubrica/Enti/DeleteEnte/DeleteEnteAuthorization.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using CQRS.Authorization;
+using SO115App.Models.Classi.Utility;
+using SO115App.Models.Servizi.CQRS.Commands.GestioneRubrica.Enti.UpdateEnte;
+using SO115App.Models.Servizi.Infrastruttura.Autenticazione;
+using SO115App.Models.Servizi.Infrastruttura.GestioneUtenti.VerificaUtente;
 using System.Collections.Generic;
-using System.Text;
+using System.Security.Principal;
 
 namespace SO115App.Models.Servizi.CQRS.Commands.GestioneRubrica.Enti.DeleteEnte
 {
     public class DeleteEnteAuthorization
     {
+        private readonly IPrincipal _currentUser;
+        private readonly IFindUserByUsername _findUserByUsername;
+        private readonly IGetAutorizzazioni _getAutorizzazioni;
+        public DeleteEnteAuthorization(IPrincipal currentUser, IFindUserByUsername findUserByUsername, IGetAutorizzazioni getAutorizzazioni)
+        {
+            _currentUser = currentUser;
+            _findUserByUsername = findUserByUsername;
+            _getAutorizzazioni = getAutorizzazioni;
+        }
+
+        public IEnumerable<AuthorizationResult> Authorize(UpdateEnteCommand command)
+        {
+            var username = _currentUser.Identity.Name;
+            var user = _findUserByUsername.FindUserByUs(username);
+
+            if (_currentUser.Identity.IsAuthenticated)
+            {
+                if (user == null)
+                    yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
+                else
+                {
+                    //TODO autorizzazione
+
+                    //if (!_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.CodiceSede, Costanti.GestoreRichieste))
+                    //    yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
+                }
+            }
+            else
+                yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
+        }
     }
 }
