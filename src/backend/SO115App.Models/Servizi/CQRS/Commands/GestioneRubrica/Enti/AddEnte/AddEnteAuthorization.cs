107be48312bc09321a3e29ccedd547f1c0ed1949
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2020-08-03 17:34:45 +0200

    Fix - Autorizzazioni Enti

--- src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneRubrica/Enti/AddEnte/AddEnteAuthorization.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneRubrica/Enti/AddEnte/AddEnteAuthorization.cs
@@ -3,6 +3,7 @@ using CQRS.Commands.Authorizers;
 using SO115App.Models.Classi.Utility;
 using SO115App.Models.Servizi.Infrastruttura.Autenticazione;
 using SO115App.Models.Servizi.Infrastruttura.GestioneUtenti.VerificaUtente;
+using System;
 using System.Collections.Generic;
 using System.Security.Principal;
 
@@ -13,6 +14,7 @@ namespace SO115App.Models.Servizi.CQRS.Commands.GestioneRubrica.Enti.AddEnte
         private readonly IPrincipal _currentUser;
         private readonly IFindUserByUsername _findUserByUsername;
         private readonly IGetAutorizzazioni _getAutorizzazioni;
+
         public AddEnteAuthorization(IPrincipal currentUser, IFindUserByUsername findUserByUsername, IGetAutorizzazioni getAutorizzazioni)
         {
             _currentUser = currentUser;
@@ -31,10 +33,17 @@ namespace SO115App.Models.Servizi.CQRS.Commands.GestioneRubrica.Enti.AddEnte
                     yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
                 else
                 {
-                    //TODO autorizzazione
+                    Boolean abilitato = false;
+                    foreach (var ruolo in user.Ruoli)
+                    {
+                        if (_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.Ente.CodSede, Costanti.GestoreChiamate))
+                            abilitato = true;
+                        if (_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.Ente.CodSede, Costanti.GestoreRichieste))
+                            abilitato = true;
+                    }
 
-                    //if (!_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.CodiceSede, Costanti.GestoreRichieste))
-                    //    yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
+                    if (!abilitato)
+                        yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
                 }
             }
             else
