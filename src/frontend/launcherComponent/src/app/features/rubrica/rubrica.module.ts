aa0bb02db318d7bdce5b3c65e8fa7b52e26a8349
bek2495 <bekimhoxha2495@gmail.com>
2020-08-03 17:28:10 +0200

    fix: rubrica

--- src/frontend/launcherComponent/src/app/features/rubrica/rubrica.module.ts
+++ src/frontend/launcherComponent/src/app/features/rubrica/rubrica.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
  * Component
  */
 import { RubricaComponent } from './rubrica.component';
-import { AddVoceRubricaModalComponent } from './add-voce-rubrica-modal/add-voce-rubrica-modal.component';
 import { TabellaVociRubricaComponent } from './tabella-voci-rubrica/tabella-voci-rubrica.component';
 import { VociRubricaPerPaginaComponent } from './tabella-voci-rubrica/voci-rubrica-per-pagina/voci-rubrica-per-pagina.component';
 import { RisultatiPaginazioneComponent } from './tabella-voci-rubrica/risultati-paginazione/risultati-paginazione.component';
@@ -16,12 +15,12 @@ import { RubricaRouting } from './rubrica.routing';
 
 
 @NgModule({
-  declarations: [RubricaComponent, AddVoceRubricaModalComponent, TabellaVociRubricaComponent, VociRubricaPerPaginaComponent, RisultatiPaginazioneComponent],
+  declarations: [RubricaComponent, TabellaVociRubricaComponent, VociRubricaPerPaginaComponent, RisultatiPaginazioneComponent],
   imports: [
     CommonModule,
     RubricaRouting
   ],
-  entryComponents: [AddVoceRubricaModalComponent],
+  entryComponents: [],
   providers: [
   ]
 })
