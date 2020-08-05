41bcb7949023614955f0fcc650885d2185a0a07c
Francesco d'Angelis <francescodangelis@gmail.com>
2020-08-05 16:28:53 +0200

    Add - aggiunta paginazione nella notifica di delete enti

--- src/backend/SO115App.Models/Servizi/Infrastruttura/GestioneRubrica/Enti/IGetRubrica.cs
+++ src/backend/SO115App.Models/Servizi/Infrastruttura/GestioneRubrica/Enti/IGetRubrica.cs
@@ -10,12 +10,14 @@ namespace SO115App.Models.Servizi.Infrastruttura.GestioneRubrica.Enti
     public interface IGetRubrica
     {
         /// <param name="CodSede">I codici della sede per cui vanno ricercati gli enti</param>
-        /// <param name="Ricorsivo"></param>
+        /// <param name="search">Campo ricerca per cui filtrare gli enti</param>
         /// <returns>Elenco della rubrica secondo più codici sede e la ricorsività</returns>
         List<EnteDTO> Get(string[] CodSede, string search = null);
 
         List<EnteDTO> GetBylstCodici(int[] lstCodici);
 
         EnteDTO Get(string Id);
+
+        int GetTotalItems();
     }
 }
