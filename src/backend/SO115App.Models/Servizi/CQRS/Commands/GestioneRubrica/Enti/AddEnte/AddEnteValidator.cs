5f7b2c9a027458fc44c3a256470de72e46b52e99
Francesco d'Angelis <francescodangelis@gmail.com>
2020-08-06 15:43:55 +0200

    Add - aggiunte validazioni telefono.numero in add e update enti

--- src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneRubrica/Enti/AddEnte/AddEnteValidator.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneRubrica/Enti/AddEnte/AddEnteValidator.cs
@@ -2,6 +2,7 @@
 using CQRS.Validation;
 using SO115App.Models.Classi.Utility;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SO115App.Models.Servizi.CQRS.Commands.GestioneRubrica.Enti.AddEnte
 {
@@ -17,8 +18,9 @@ namespace SO115App.Models.Servizi.CQRS.Commands.GestioneRubrica.Enti.AddEnte
                 if (command.Ente.CodCategoria == 0)
                     yield return new ValidationResult("Nessuna categoria selezionata");
 
-                if(command.Ente.Telefoni == null || command.Ente.Telefoni.Count == 0)
-                    yield return new ValidationResult("Nessun telefono selezionato");
+                if(command.Ente.Telefoni == null || command.Ente.Telefoni.Count == 0
+                    || command.Ente.Telefoni.Any(c => c.Numero.ToList().Any(x => !new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', ' ' }.Contains(x))))
+                    yield return new ValidationResult("Telefoni non validi");
 
                 if (command.Ente.Descrizione == "" || command.Ente.Descrizione == null)
                     yield return new ValidationResult("Nessuna descrizione selezionata");
