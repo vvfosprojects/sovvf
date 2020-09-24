3824bda833f47597cc6f85ca40aa6058de9fe277
notmikenot <micheledragonetti99@gmail.com>
2020-09-24 17:02:22 +0200

    Add: aggiunto indice nella pagina di "Informazioni sugli Aggiornamenti"

--- src/frontend/launcherComponent/src/app/features/changelog/changelog-item/changelog-item.component.ts
+++ src/frontend/launcherComponent/src/app/features/changelog/changelog-item/changelog-item.component.ts
@@ -10,14 +10,21 @@ import { LSNAME } from '../../../core/settings/config';
 export class ChangelogItemComponent implements OnInit {
 
     @Input() changelog: ChangelogInterface;
+    @Input() changelogId: string;
     @Input() ultimoChangelog: boolean;
+
+    indiceVisible = true;
 
     emailProblemi = LSNAME.emailError;
 
     constructor() {
     }
 
-    ngOnInit() {
+    ngOnInit(): void {
+    }
+
+    toggleIndiceVisible(): void {
+        this.indiceVisible = !this.indiceVisible;
     }
 
 }
