88cc02b57f7b5f6a60e5b0d84be2834b8c0dc3e4
Francesco d'Angelis <francescodangelis@gmail.com>
2020-10-02 13:17:32 +0200

    aggiunta cache ad api esterna "PersonaleApiUtenteComuni"

--- src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/Personale/GetPersonaleByCF.cs
+++ src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/Personale/GetPersonaleByCF.cs
@@ -5,6 +5,7 @@ using SO115App.ExternalAPI.Fake.Classi.PersonaleUtentiComuni;
 using SO115App.ExternalAPI.Fake.Classi.Utility;
 using SO115App.Models.Classi.Utenti.Autenticazione;
 using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Personale;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -56,32 +57,32 @@ namespace SO115App.ExternalAPI.Fake.Servizi.Personale
 
         private async Task<List<PersonaleVVF>> GetPersonaleVVFExternalAPI(string[] codSede)
         {
-            List<PersonaleVVF> listaPersonale = new List<PersonaleVVF>();
+            var listaPersonale = new List<PersonaleVVF>();
 
-            #region API ESTERNA
+            Parallel.ForEach(codSede, async codice =>
+            {
+                if (!_memoryCache.TryGetValue($"Personale_{codice.Split('.')[0]}", out listaPersonale))
+                {
 
-            _client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("test");
-            var sede = string.Concat(codSede.Select(c => c.Split(".")[0]));
-            var response = await _client.GetAsync($"{_configuration.GetSection("UrlExternalApi").GetSection("PersonaleApiUtenteComuni").Value}?codiciSede={sede}").ConfigureAwait(false);
-            response.EnsureSuccessStatusCode();
-            using HttpContent content = response.Content;
-            string data = await content.ReadAsStringAsync().ConfigureAwait(false);
-            var personaleUC = JsonConvert.DeserializeObject<List<PersonaleUC>>(data);
+                    #region API ESTERNA
 
-            #endregion
+                    var client = new HttpClient();
+                    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("test");
+                    var sede = string.Concat(codSede.Select(c => c.Split(".")[0]));
+                    var response = await client.GetAsync($"{_configuration.GetSection("UrlExternalApi").GetSection("PersonaleApiUtenteComuni").Value}?codiciSede={sede}").ConfigureAwait(false);
+                    response.EnsureSuccessStatusCode();
+                    using HttpContent content = response.Content;
+                    string data = await content.ReadAsStringAsync().ConfigureAwait(false);
+                    var personaleUC = JsonConvert.DeserializeObject<List<PersonaleUC>>(data);
 
-            listaPersonale = MapPersonaleVVFsuPersonaleUC.Map(personaleUC);
+                    listaPersonale = MapPersonaleVVFsuPersonaleUC.Map(personaleUC);
 
-            //TODO GESTIONE CACHE
+                    #endregion
 
-            //foreach (var codice in codSede)
-            //{
-            //    if (!_memoryCache.TryGetValue($"Personale_{codice.Split('.')[0]}", out listaPersonale))
-            //    {
-            //        var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromHours(8));
-            //        _memoryCache.Set($"Personale_{codice.Split('.')[0]}", listaPersonale, cacheEntryOptions);
-            //    }
-            //}
+                    var cacheEntryOptions = new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromHours(8));
+                    _memoryCache.Set($"Personale_{codice.Split('.')[0]}", listaPersonale, cacheEntryOptions);
+                }
+            });
 
             return listaPersonale;
         }
