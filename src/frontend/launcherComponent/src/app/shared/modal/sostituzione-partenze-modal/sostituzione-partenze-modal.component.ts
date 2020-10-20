beb41ddf4432700fba1149977c3a46ee06d4593e
notmikenot <micheledragonetti99@gmail.com>
2020-10-20 16:46:04 +0200

    Chg: inizio implementazione nuovo componente per "SostituzionePerFineTurno"

---
+++ src/frontend/launcherComponent/src/app/shared/modal/sostituzione-partenze-modal/sostituzione-partenze-modal.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-sostituzione-partenze-modal',
+    templateUrl: './sostituzione-partenze-modal.component.html',
+    styleUrls: ['./sostituzione-partenze-modal.component.scss']
+})
+export class SostituzionePartenzeModalComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+}
