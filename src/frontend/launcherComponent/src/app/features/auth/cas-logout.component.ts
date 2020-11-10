12fa75381e2778c14864fd35e68a2aab05667d31
notmikenot <micheledragonetti99@gmail.com>
2020-11-10 14:29:18 +0100

    Fix: corretta logica di "CAS test" e del logout per utente CAS non abilitato

--- src/frontend/launcherComponent/src/app/features/auth/cas-logout.component.ts
+++ src/frontend/launcherComponent/src/app/features/auth/cas-logout.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngxs/store';
 import { ClearCurrentUser } from './store/auth.actions';
+import { Navigate } from '@ngxs/router-plugin';
 
 @Component({ template: `` })
 export class CasLogoutComponent {
 
     constructor(private store: Store) {
-        this.store.dispatch([ new ClearCurrentUser() ]);
+        this.store.dispatch([
+            new ClearCurrentUser(),
+            new Navigate(['/login'])
+        ]);
     }
 
 }
