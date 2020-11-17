4e0175ed2410ebea08abfa8c885632d683752960
Francesco d'Angelis <francescodangelis@gmail.com>
2020-11-17 11:38:50 +0100

    Add: implementata classe che gestisce genericamente le chiamate ai servizi esterni

---
+++ src/backend/SO115App.FakePersistance.ExternalAPI/HttpManager/HttpRequestManager.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using Polly;
+using Polly.Caching;
+using Polly.Caching.Memory;
+using Polly.Wrap;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Prova_Polly2
+{
+    public class HttpRequestManager<OutputData> : IHttpRequestManager<OutputData>, IDisposable
+    {
+        private readonly HttpClient _client;
+        private AsyncPolicyWrap<HttpResponseMessage> policies;
+        private OutputData Result = default;
+
+        private HttpRequestManager() { }
+        public HttpRequestManager(HttpClient client, string cacheString = null)
+        {
+            _client = client;
+
+            var timeoutPolicy = Policy
+                .TimeoutAsync<HttpResponseMessage>(120);
+
+            var retryPolicy = Policy
+                .Handle<AggregateException>(e => throw new Exception("Servizio non raggiungibile"))
+                .OrResult<HttpResponseMessage>(c => c.StatusCode == HttpStatusCode.NotFound)
+                .RetryAsync(3);
+
+            if (!string.IsNullOrEmpty(cacheString))
+            {
+                var memoryCacheProvider = new MemoryCacheProvider(new MemoryCache(new MemoryCacheOptions()));
+                var cachePolicy = Policy.CacheAsync(memoryCacheProvider.AsyncFor<HttpResponseMessage>(), TimeSpan.FromHours(6), c =>
+                {
+                    var msgCache = "Risultato salvato in cache";
+                    Console.WriteLine(msgCache);
+                    return msgCache;
+                });
+
+                policies = Policy.WrapAsync(cachePolicy, retryPolicy, timeoutPolicy);
+            }
+            else
+                policies = Policy.WrapAsync(retryPolicy, timeoutPolicy);
+        }
+
+        public async Task<OutputData> ExecuteGet(Uri url)
+        {
+            await policies.ExecuteAsync(() => Task.Run(() =>
+            {
+                var response = _client.GetAsync(url).Result;
+                var data = response.Content.ReadAsStringAsync().Result;
+                Result = JsonConvert.DeserializeObject<OutputData>(data);
+
+                return response;
+            }));
+
+            return Result;
+        }
+
+        public async Task<OutputData> ExecutePost(Uri url, HttpContent content)
+        {
+            await policies.ExecuteAsync(() => Task.Run(() =>
+            {
+                var response = _client.PostAsync(url, content).Result;
+                var data = response.Content.ReadAsStringAsync().Result;
+                Result = JsonConvert.DeserializeObject<OutputData>(data);
+
+                return response;
+            }));
+
+            return Result;
+        }
+
+        public async Task<OutputData> ExecutePut(Uri url, HttpContent content)
+        {
+            await policies.ExecuteAsync(() => Task.Run(() =>
+            {
+                var response = _client.PutAsync(url, content).Result;
+                var data = response.Content.ReadAsStringAsync().Result;
+                Result = JsonConvert.DeserializeObject<OutputData>(data);
+
+                return response;
+            }));
+
+            return Result;
+        }
+
+        public void Dispose()
+        {
+            _client.Dispose();
+        }
+    }
+}
---
+++ src/backend/SO115App.FakePersistance.ExternalAPI/HttpManager/IHttpRequestManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Prova_Polly2
+{
+    public interface IHttpRequestManager<OutputData>
+    {
+        Task<OutputData> ExecuteGet(Uri url);
+
+        Task<OutputData> ExecutePost(Uri url, HttpContent content);
+
+        Task<OutputData> ExecutePut(Uri url, HttpContent content);
+    }
+}
