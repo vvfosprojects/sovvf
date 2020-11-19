badb1f29aa1f8dfa404db6eb20b724f4e6bce500
Francesco d'Angelis <francescodangelis@gmail.com>
2020-11-19 15:54:08 +0100

    cancellata implementazione non utilizzata

--- src/backend/SO115App.CompositionRoot/ExternalAPIFakeServicesConfigurator.cs
+++ src/backend/SO115App.CompositionRoot/ExternalAPIFakeServicesConfigurator.cs
@@ -172,10 +172,10 @@ namespace SO115App.CompositionRoot
             container.Register<
                 SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Mezzi.IGetMezzoById,
                 SO115App.ExternalAPI.Fake.ImportOracle.MezziMapper.GetMezzoById>();
-            container.Register<IGetMezziUtilizzabili, SO115App.ExternalAPI.Fake.GestioneMezzi.GetMezziUtilizzabili>();
+            container.Register<IGetMezziUtilizzabili, GetMezziUtilizzabili>();
 
             container.Register<
-                SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Gac.IGetMezziUtilizzabiliByAreaMappa,
+                IGetMezziUtilizzabiliByAreaMappa,
                 ExternalAPI.Fake.GestioneMezzi.GetMezziUtilizzabiliByAreaMappa>();
 
             #endregion Mezzi
