12308412fa9b0ec3edc0dbed5784a04f3ef31964
notmikenot <micheledragonetti99@gmail.com>
2020-11-24 15:23:06 +0100

    Fix valore viewport per utilizzo su monitor esteso

--- src/frontend/launcherComponent/src/app/shared/store/states/viewport/viewport.state.ts
+++ src/frontend/launcherComponent/src/app/shared/store/states/viewport/viewport.state.ts
@@ -56,7 +56,7 @@ export class ViewportState {
 
     @Selector()
     static doubleMonitor(state: ViewportStateModel): boolean {
-        return state.innerWidth > 3800;
+        return state.innerWidth > 3400;
     }
 
     @Action(SetAvailHeight)
