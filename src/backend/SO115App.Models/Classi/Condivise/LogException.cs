1d0c1bacd6d2a6fc847ef664bafcada25a95bfa4
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2020-11-25 11:21:22 +0100

    Add gestione eccezioni servizi esterni

---
+++ src/backend/SO115App.Models/Classi/Condivise/LogException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO115App.Models.Classi.Condivise
+{
+    public class LogException
+    {
+        public DateTime DataOraEsecuzione { get; set; }
+        public string Servizio { get; set; }
+        public string Content { get; set; }
+        public string Response { get; set; }
+        public string CodComando { get; set; }
+        public string IdOperatore { get; set; }
+    }
+}
