8f45d8522b307d764f3390eba71e5a6fafc335e2
bek2495 <bekimhoxha2495@gmail.com>
2021-01-08 11:34:41 +0100

    add: AddSoccorsoAereo (per test BE)

--- src/frontend/launcherComponent/src/app/shared/modal/soccorso-aereo-modal/soccorso-aereo-modal.component.ts
+++ src/frontend/launcherComponent/src/app/shared/modal/soccorso-aereo-modal/soccorso-aereo-modal.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnDestroy } from '@angular/core';
-import {Subscription} from 'rxjs';
+import {Observable, Subscription} from 'rxjs';
 import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import {Store} from '@ngxs/store';
+import {Select, Store} from '@ngxs/store';
 import {ComposizioneSoccorsoAereoState} from '../../../features/home/store/states/composizione-partenza/composizione-soccorso-aereo.state';
 import {makeCopy} from '../../helper/function';
+import {AddSoccorsoAereo} from '../../../features/home/store/actions/composizione-partenza/composizione-soccorso-aereo.actions';
+import {AuthState} from '../../../features/auth/store/auth.state';
+import {Utente} from '../../model/utente.model';
 
 
 @Component({
@@ -14,10 +17,14 @@ import {makeCopy} from '../../helper/function';
 
 export class SoccorsoAereoModalComponent implements OnDestroy {
 
+  @Select(AuthState.currentUser) user$: Observable<Utente>;
+  utente: Utente;
+
   subscription: Subscription = new Subscription();
   azioniRichiesta: any[];
 
   constructor(private modal: NgbActiveModal, private store: Store) {
+    this.getUtente();
     this.azioniRichiesta = makeCopy(store.selectSnapshot(ComposizioneSoccorsoAereoState.azioniRichieste));
     this.azioniRichiesta.forEach(x => x.checked = false);
   }
@@ -32,13 +39,28 @@ export class SoccorsoAereoModalComponent implements OnDestroy {
 
   chiudiModalSoccorsoAereo(closeRes: string): void {
     if (closeRes === 'ok') {
+      const obj = {
+        motivazione: '',
+        tipologiaSelezionata: '',
+        nome: this.utente.nome,
+        cognome: this.utente.cognome,
+        codiceFiscale: this.utente.codiceFiscale,
+      };
       this.modal.close({
         status: 'ok',
         result: {},
       });
+      this.store.dispatch(new AddSoccorsoAereo(obj));
     } else {
       this.modal.close({ status: 'ko'});
     }
   }
 
+  getUtente(): void {
+    this.subscription.add(
+      this.user$.subscribe((user: Utente) => {
+        this.utente = user;
+      })
+    );
+  }
 }
