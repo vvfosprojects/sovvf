a2596d391b62ccf4e12eabca8e892b1288218429
bek2495 <bekimhoxha2495@gmail.com>
2021-01-15 10:38:28 +0100

    fix: dettaglio soccorso aereo

--- src/frontend/launcherComponent/src/app/shared/modal/modifica-soccorso-aereo-modal/dettaglio-soccorso-aereo-modal.component.ts
+++ src/frontend/launcherComponent/src/app/shared/modal/modifica-soccorso-aereo-modal/dettaglio-soccorso-aereo-modal.component.ts
@@ -14,12 +14,45 @@ export class DettaglioSoccorsoAereoModalComponent implements OnDestroy {
 
   subscription: Subscription = new Subscription();
   richiesta: SintesiRichiesta;
+  showAttivita = true;
+  attivita: any[] = [
+     {
+      stato: 'test1',
+      aggiornamento: 'test2',
+      nucleo: 'test3',
+      velivolo: 'test4',
+      categorie: 'test5',
+      tempoStimato: 'test6',
+      accettazione: 'test7',
+      decollo: 'test8',
+      arrivo: 'test9',
+      rientro: 'test10',
+      sede: 'test11'
+    },
+    {
+      stato: 'aaaaa',
+      aggiornamento: 'bbbb',
+      nucleo: 'cccc',
+      velivolo: 'ddddd',
+      categorie: 'eeeeee',
+      tempoStimato: 'fffff',
+      accettazione: 'ggggg',
+      decollo: 'hhhhhh',
+      arrivo: 'iiiii',
+      rientro: 'lllll',
+      sede: 'mmmmm'
+    },
+  ];
 
   constructor(private modal: NgbActiveModal) {
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  onShowAttivita(): void {
+    this.showAttivita = !this.showAttivita;
   }
 
   chiudiModalSoccorsoAereo(closeRes: string): void {
