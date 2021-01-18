bd4c8247580b1bfd4c68bd72d693511781bbaa0a
notmikenot <micheledragonetti99@gmail.com>
2021-01-18 17:04:06 +0100

    Creato "Fake Triage" con componente Treeview

--- src/frontend/launcherComponent/src/app/features/impostazioni-sede/triage/triage.component.ts
+++ src/frontend/launcherComponent/src/app/features/impostazioni-sede/triage/triage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TreeviewConfig, TreeviewItem } from 'ngx-treeview';
 
 @Component({
     selector: 'app-triage',
@@ -6,6 +7,111 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./triage.component.scss']
 })
 export class TriageComponent implements OnInit {
+
+    tConfig = {
+        hasAllCheckBox: false,
+        hasFilter: false,
+        hasCollapseExpand: false,
+        decoupleChildFromParent: false,
+        maxHeight: 1000
+    } as TreeviewConfig;
+
+    tItems = [
+        new TreeviewItem({
+            text: 'Ci sono persone in casa?',
+            value: 'Ci sono persone in casa?',
+            disabled: true,
+            children: [
+                {
+                    text: 'Si',
+                    value: 'si',
+                    children: [{
+                        text: 'Ci sono persone in pericolo?',
+                        value: 'Ci sono persone in pericolo?',
+                        children: [
+                            { text: 'Si', value: 'si', disabled: true },
+                            { text: 'No', value: 'no', disabled: true },
+                            { text: 'Non lo so', value: 'non lo so', disabled: true }
+                        ],
+                        disabled: true
+                    }],
+                    disabled: true
+                },
+                {
+                    text: 'No',
+                    value: 'no',
+                    children: [{
+                        text: 'Si riesce ad accedere all\'abitazione?',
+                        value: 'Si riesce ad accedere all\'abitazione?',
+                        children: [
+                            { text: 'Si', value: 'si', disabled: true },
+                            { text: 'No', value: 'no', disabled: true },
+                            { text: 'Non lo so', value: 'non lo so', disabled: true }
+                        ],
+                        disabled: true
+                    }],
+                    disabled: true
+                },
+                {
+                    text: 'Non lo so',
+                    value: 'non lo so',
+                    children: [{
+                        text: 'Ci sono persone in pericolo?',
+                        value: 'Ci sono persone in pericolo?',
+                        children: [
+                            {
+                                text: 'Si',
+                                value: 'si',
+                                children: [{
+                                    text: 'Si riesce ad accedere all\'abitazione?',
+                                    value: 'Si riesce ad accedere all\'abitazione?',
+                                    children: [
+                                        { text: 'Si', value: 'si', disabled: true },
+                                        { text: 'No', value: 'no', disabled: true },
+                                        { text: 'Non lo so', value: 'non lo so', disabled: true }
+                                    ],
+                                    disabled: true
+                                }],
+                                disabled: true
+                            },
+                            {
+                                text: 'No',
+                                value: 'no',
+                                children: [{
+                                    text: 'Si riesce ad accedere all\'abitazione?',
+                                    value: 'Si riesce ad accedere all\'abitazione?',
+                                    children: [
+                                        { text: 'Si', value: 'si', disabled: true },
+                                        { text: 'No', value: 'no', disabled: true },
+                                        { text: 'Non lo so', value: 'non lo so', disabled: true }
+                                    ],
+                                    disabled: true
+                                }],
+                                disabled: true
+                            },
+                            {
+                                text: 'Non lo so',
+                                value: 'non lo so',
+                                children: [{
+                                    text: 'Si riesce ad accedere all\'abitazione?',
+                                    value: 'Si riesce ad accedere all\'abitazione?',
+                                    children: [
+                                        { text: 'Si', value: 'si', disabled: true },
+                                        { text: 'No', value: 'no', disabled: true },
+                                        { text: 'Non lo so', value: 'non lo so', disabled: true }
+                                    ],
+                                    disabled: true
+                                }],
+                                disabled: true
+                            }
+                        ],
+                        disabled: true
+                    }],
+                    disabled: true
+                }
+            ]
+        })
+    ];
 
     constructor() {
     }
