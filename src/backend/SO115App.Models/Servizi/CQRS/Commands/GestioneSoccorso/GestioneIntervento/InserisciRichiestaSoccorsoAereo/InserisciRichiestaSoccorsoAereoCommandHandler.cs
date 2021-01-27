77f9b9ca90c0f10d294a778c29e132fd42370728
Francesco d'Angelis <francescodangelis@gmail.com>
2021-01-27 15:25:18 +0100

    fix: popolati campi per servizio esterno inserimento AFM

--- src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneSoccorso/GestioneIntervento/InserisciRichiestaSoccorsoAereo/InserisciRichiestaSoccorsoAereoCommandHandler.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneSoccorso/GestioneIntervento/InserisciRichiestaSoccorsoAereo/InserisciRichiestaSoccorsoAereoCommandHandler.cs
@@ -1,8 +1,11 @@
 ﻿using CQRS.Commands;
+using SO115App.API.Models.Classi.Condivise;
 using SO115App.API.Models.Servizi.Infrastruttura.GestioneSoccorso;
 using SO115App.Models.Classi.ServiziEsterni.Utility;
 using SO115App.Models.Classi.Soccorso.Eventi;
 using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.AFM;
+using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Competenze;
+using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Distaccamenti;
 using System;
 using System.Linq;
 
@@ -12,13 +15,18 @@ namespace SO115App.Models.Servizi.CQRS.Commands.GestioneSoccorso.GestioneInterve
     {
         private readonly IAggiornaRichiestaSoccorsoAereo _aggiorna;
         private readonly IUpDateRichiestaAssistenza _updateRichiesta;
+        private readonly IGetCompetenzeByCoordinateIntervento _getComeptenze;
+        private readonly IGetDistaccamentoByCodiceSedeUC _getDistaccamento;
 
-        //IGetCompetenzeByCoordinateIntervento
-
-        public InserisciRichiestaSoccorsoAereoCommandHandler(IAggiornaRichiestaSoccorsoAereo aggiorna, IUpDateRichiestaAssistenza updateRichiesta)
+        public InserisciRichiestaSoccorsoAereoCommandHandler(IAggiornaRichiestaSoccorsoAereo aggiorna,
+            IUpDateRichiestaAssistenza updateRichiesta,
+            IGetCompetenzeByCoordinateIntervento getComeptenze,
+            IGetDistaccamentoByCodiceSedeUC getDistaccamento)
         {
             _aggiorna = aggiorna;
             _updateRichiesta = updateRichiesta;
+            _getComeptenze = getComeptenze;
+            _getDistaccamento = getDistaccamento;
         }
 
         public void Handle(InserisciRichiestaSoccorsoAereoCommand command)
@@ -35,11 +43,15 @@ namespace SO115App.Models.Servizi.CQRS.Commands.GestioneSoccorso.GestioneInterve
             #region AFM Servizio
 
             //COMPONGO IL MODELLO PER AFM
+            var codiceDistaccamentoCompetenza = _getComeptenze.GetCompetenzeByCoordinateIntervento(new Coordinate((double)command.RichiestaSoccorsoAereo.lat, (double)command.RichiestaSoccorsoAereo.lng))[0];
+            var distaccamentoCompetenza = _getDistaccamento.Get(codiceDistaccamentoCompetenza);
             command.RichiestaSoccorsoAereo.datetime = date;
             command.RichiestaSoccorsoAereo.requestKey = MapRequestKeyAFM.MapForAFM(command.RichiestaSoccorsoAereo.requestKey);
-            command.RichiestaSoccorsoAereo.progressiveNumber = command.Richiesta.CodRichiesta?.Split('-')[2] ?? "";
-            //command.RichiestaSoccorsoAereo.;
-
+            command.RichiestaSoccorsoAereo.progressiveNumber = command.Richiesta.CodRichiesta?.Split('-')[2].TrimStart() ?? "";
+            command.RichiestaSoccorsoAereo.locality = command.Richiesta.Localita.Indirizzo;
+            command.RichiestaSoccorsoAereo.venueInCharge = distaccamentoCompetenza.Result.DescDistaccamento;
+            command.RichiestaSoccorsoAereo.onSiteContact = command.Richiesta.Richiedente.Nominativo;
+            command.RichiestaSoccorsoAereo.onSiteContactPhoneNumber = command.Richiesta.Richiedente.Telefono;
 
             //Comunico al servizio esterno
             var responseAFM = _aggiorna.Aggiorna(command.RichiestaSoccorsoAereo);
