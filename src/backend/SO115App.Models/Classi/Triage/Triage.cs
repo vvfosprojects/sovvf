2cc94136d9aaf002d999a0f24998f81f0407053b
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2021-02-03 17:38:19 +0100

    Modifica modello Triage

--- src/backend/SO115App.Models/Classi/Triage/Triage.cs
+++ src/backend/SO115App.Models/Classi/Triage/Triage.cs
@@ -10,8 +10,6 @@ namespace SO115App.Models.Classi.Triage
         public string CodiceSede { get; set; }
         public int CodTipologia { get; set; }
         public int CodDettaglioTipologia { get; set; }
-        public string Text { get; set; }
-        public string Value { get; set; }
-        public List<Triage> InternalChildren { get; set; }
+        public TreeTriage data { get; set; }
     }
 }
