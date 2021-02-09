a4d4aec9e987a42da9670117e72d0ec83aa74b60
Francesco d'Angelis <francescodangelis@gmail.com>
2021-02-09 17:06:23 +0100

    cng: sostituito gestore richieste http su tutti i servizi esterni

--- src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/AFM/GetHistoryRichiestaSoccorsoAereo.cs
+++ src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/AFM/GetHistoryRichiestaSoccorsoAereo.cs
@@ -1,28 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Configuration;
+﻿using SO115App.ExternalAPI.Client;
 using SO115App.ExternalAPI.Fake.Classi;
-using SO115App.ExternalAPI.Fake.HttpManager;
 using SO115App.Models.Classi.ServiziEsterni.AFM;
-using SO115App.Models.Servizi.Infrastruttura.GestioneLog;
 using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.AFM;
 using System;
-using System.Net.Http;
 
 namespace SO115App.ExternalAPI.Fake.Servizi.AFM
 {
-    public class GetHistoryRichiestaSoccorsoAereo : BaseService, IGetHistoryRichiestaSoccorsoAereo
+    public class GetHistoryRichiestaSoccorsoAereo : IGetHistoryRichiestaSoccorsoAereo
     {
-        public GetHistoryRichiestaSoccorsoAereo(HttpClient client, IConfiguration configuration, IMemoryCache cache, IWriteLog log, IHttpContextAccessor accessor)
-        : base(client, configuration, cache, log, accessor) { }
+        private readonly IHttpRequestManager<StoricoAFM> _client;
+
+        public GetHistoryRichiestaSoccorsoAereo(IHttpRequestManager<StoricoAFM> client) => _client = client;
 
         public StoricoAFM Get(string requestKey)
         {
-            var APImanager = new HttpRequestManager<StoricoAFM>(_client, _memoryCache, _writeLog, _httpContext, _configuration);
+            _client.Configure();
 
-            APImanager.Configure();
-
-            var result = APImanager.GetAsync(new Uri(Costanti.AFM + "rescueRequest/" + requestKey + "/history"), "francesco.dangelis@dipvvf.it", "DNGFNC98R17D662Q").Result;
+            var result = _client.GetAsync(new Uri(Costanti.AFM + "rescueRequest/" + requestKey + "/history"), "francesco.dangelis@dipvvf.it", "DNGFNC98R17D662Q").Result;
 
             return result;
         }
--- src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/Gac/SetUscitaMezzo.cs
+++ src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/Gac/SetUscitaMezzo.cs
@@ -1,36 +1,38 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using SO115App.ExternalAPI.Client;
 using SO115App.ExternalAPI.Fake.Classi;
-using SO115App.ExternalAPI.Fake.HttpManager;
 using SO115App.Models.Classi.ServiziEsterni.Gac;
 using SO115App.Models.Servizi.Infrastruttura.Composizione;
-using SO115App.Models.Servizi.Infrastruttura.GestioneLog;
 using System;
 using System.Collections.Generic;
 using System.Net.Http;
 
 namespace SO115App.ExternalAPI.Fake.Servizi.Gac
 {
-    public class SetUscitaMezzo : BaseService, ISetUscitaMezzo
+    public class SetUscitaMezzo : ISetUscitaMezzo
     {
-        public SetUscitaMezzo(HttpClient client, IConfiguration configuration, IMemoryCache memoryCache, IWriteLog writeLog, IHttpContextAccessor httpContext)
-            : base(client, configuration, memoryCache, writeLog, httpContext) { }
+        private readonly IGetToken _getToken;
+        private readonly IHttpRequestManager<List<Response>> _client;
+        private readonly IConfiguration _configuration;
+        public SetUscitaMezzo(IHttpRequestManager<List<Response>> client, IGetToken getToken, IConfiguration configuration)
+        {
+            _getToken = getToken;
+            _configuration = configuration;
+            _client = client;
+        }
 
         public void Set(UscitaGAC uscita)
         {
-            GetToken getToken = new GetToken(_client, _configuration, _memoryCache, _writeLog, _httpContext);
             var lstUscite = new List<UscitaGAC>() { uscita };
 
-            var requestManager = new HttpRequestManager<List<Response>>(_client, _memoryCache, _writeLog, _httpContext, _configuration);
-            requestManager.Configure();
+            _client.Configure();
 
             var jsonString = JsonConvert.SerializeObject(lstUscite);
             var content = new StringContent(jsonString);
             var uri = new Uri(_configuration.GetSection("UrlExternalApi").GetSection("GacApi").Value + Costanti.GacUscitaMezzo);
 
-            var result = requestManager.PutAsync(uri, content, getToken.GeneraToken()).Result;
+            var result = _client.PutAsync(uri, content, _getToken.GeneraToken()).Result;
         }
     }
 }
