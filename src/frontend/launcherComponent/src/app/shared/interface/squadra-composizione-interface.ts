48976a9e071c28faa78d12d08f58982bf3a287d6
notmikenot <micheledragonetti99@gmail.com>
2021-03-03 18:05:48 +0100

    Miglioramenti in "Composizione Partenza"

--- src/frontend/launcherComponent/src/app/shared/interface/squadra-composizione-interface.ts
+++ src/frontend/launcherComponent/src/app/shared/interface/squadra-composizione-interface.ts
@@ -1,8 +1,9 @@
 import { Squadra } from 'src/app/shared/model/squadra.model';
-import {MezzoComposizione} from './mezzo-composizione-interface';
+import { MezzoComposizione } from './mezzo-composizione-interface';
 
 export interface SquadraComposizione {
     id: string;
     squadra: Squadra;
     mezzoPreaccoppiato?: MezzoComposizione;
+    listaMezzi?: MezzoComposizione[];
 }
