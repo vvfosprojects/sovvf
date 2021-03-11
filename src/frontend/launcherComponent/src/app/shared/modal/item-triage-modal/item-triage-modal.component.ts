c86d46025833881c6c9309108e6966b375484fc7
notmikenot <micheledragonetti99@gmail.com>
2021-03-11 11:47:52 +0100

    Migliorata logica "Triage"

--- src/frontend/launcherComponent/src/app/shared/modal/item-triage-modal/item-triage-modal.component.ts
+++ src/frontend/launcherComponent/src/app/shared/modal/item-triage-modal/item-triage-modal.component.ts
@@ -111,17 +111,7 @@ export class ItemTriageModalComponent implements OnInit, OnDestroy {
     }
 
     onConferma(): void {
-        if (this.item && this.parentItemData && this.getParentItemDataDiffs()) {
-            const item = {
-                value: this.item.value,
-                domandaSeguente: this.f.domandaSeguente.value,
-                soccorsoAereo: this.getItemDataDiffs().soccorsoAereo,
-                generiMezzo: this.getItemDataDiffs().generiMezzo && this.getItemDataDiffs().generiMezzo.length > 0 ? this.getItemDataDiffs().generiMezzo : null,
-                noteOperatore: this.getItemDataDiffs().noteOperatore,
-                prioritaConsigliata: this.getItemDataDiffs().prioritaConsigliata
-            };
-            this.modal.close({ success: true, data: item });
-        } else if (this.item && !this.parentItemData) {
+        if (this.item && !this.parentItemData || (this.parentItemData && this.getParentItemDataDiffs())) {
             const item = {
                 value: this.item.value,
                 domandaSeguente: this.f.domandaSeguente.value,
@@ -163,33 +153,6 @@ export class ItemTriageModalComponent implements OnInit, OnDestroy {
         }
 
         return diffs;
-    }
-
-    getItemDataDiffs(): ItemTriageData {
-        const itemDiffs = {
-            soccorsoAereo: null,
-            generiMezzo: null,
-            noteOperatore: null,
-            prioritaConsigliata: null
-        } as ItemTriageData;
-
-        if (this.parentItemData?.soccorsoAereo !== this.f.soccorsoAereo.value) {
-            itemDiffs.soccorsoAereo = this.f.soccorsoAereo.value;
-        }
-
-        if (this.parentItemData?.generiMezzo !== this.f.generiMezzo.value) {
-            itemDiffs.generiMezzo = this.f.generiMezzo.value;
-        }
-
-        if (this.parentItemData?.prioritaConsigliata !== this.f.prioritaConsigliata.value) {
-            itemDiffs.prioritaConsigliata = this.f.prioritaConsigliata.value;
-        }
-
-        if (this.parentItemData?.noteOperatore !== this.f.noteOperatore.value) {
-            itemDiffs.noteOperatore = this.f.noteOperatore.value;
-        }
-
-        return itemDiffs;
     }
 
     getTitle(): string {
