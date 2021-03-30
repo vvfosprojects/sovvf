c28689b9977f92b65514c57de55a5d0887659933
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2021-03-30 12:29:26 +0200

    Controller per la richiesta dell'istogramma di Coda Chiamate

---
+++ src/backend/SO115App.API/Controllers/GestioneCodaChiamateController.cs
@@ -0,0 +1,48 @@
+﻿using CQRS.Queries;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SO115App.API.Models.Classi.Condivise;
+using SO115App.API.Models.Servizi.CQRS.Queries.GestioneSoccorso.CodaChiamate;
+using SO115App.Models.Classi.Utility;
+using SO115App.Models.Servizi.CQRS.Queries.GestioneSoccorso.GetCompetenze;
+using System;
+using System.Threading.Tasks;
+
+namespace SO115App.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GestioneCodaChiamateController : ControllerBase
+    {
+        private readonly IQueryHandler<CodaChiamateQuery, CodaChiamateResult> _getCodaChiamate;
+
+        public GestioneCodaChiamateController(IQueryHandler<CodaChiamateQuery, CodaChiamateResult> getCodaChiamate)
+        {
+            _getCodaChiamate = getCodaChiamate;
+        }
+
+        [HttpGet("GetInfoIstogramma")]
+        public async Task<IActionResult> GetInfoIstogramma()
+        {
+            try
+            {
+                var query = new CodaChiamateQuery()
+                {
+                    CodiciSede = Request.Headers["codicesede"].ToString().Split(',', StringSplitOptions.RemoveEmptyEntries)
+                };
+
+                return Ok(_getCodaChiamate.Handle(query));
+            }
+            catch (Exception ex)
+            {
+                ex = ex.GetBaseException();
+
+                if (ex.Message.Contains(Costanti.UtenteNonAutorizzato))
+                    return StatusCode(403, new { message = Costanti.UtenteNonAutorizzato });
+                else
+                    return BadRequest(ex);
+            }
+        }
+    }
+}
---
+++ src/backend/SO115App.Models/Classi/CodaChiamate/InfoIstogramma.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO115App.Models.Classi.CodaChiamate
+{
+    public class InfoIstogramma
+    {
+        public List<Istogramma> ListaCodaChiamate { get; set; }
+    }
+}
