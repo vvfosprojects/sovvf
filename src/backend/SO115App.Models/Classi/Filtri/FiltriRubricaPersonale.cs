cd6e8994f4d2fd2469c36d1d69b95cb3d5a0ee7e
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2021-03-31 16:13:31 +0200

    add: filtro ricerca fulltext in rubrica personale

---
+++ src/backend/SO115App.Models/Classi/Filtri/FiltriRubricaPersonale.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SO115App.Models.Classi.Filtri
+{
+    public class FiltriRubricaPersonale
+    {
+        public string Search { get; set; }
+        public string Stato { get; set; }
+    }
+}
