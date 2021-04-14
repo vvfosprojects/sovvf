f695b49f475a11225bd266c52fcb4b0841ff055b
notmikenot <micheledragonetti99@gmail.com>
2021-04-14 12:36:50 +0200

    Completata implementazione store "Distaccamenti" Completata implementazione service "Distaccamenti"

--- src/frontend/launcherComponent/src/app/core/service/distaccamenti-service/distaccamenti-service.ts
+++ src/frontend/launcherComponent/src/app/core/service/distaccamenti-service/distaccamenti-service.ts
@@ -2,6 +2,7 @@ import { environment } from '../../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Sede } from '../../../shared/model/sede.model';
 
 const BASE_URL = environment.baseUrl;
 const API_DISTACCAMENTI = BASE_URL + environment.apiUrl.distaccamenti;
@@ -14,9 +15,8 @@ export class DistaccamentiService {
     constructor(private http: HttpClient) {
     }
 
-    // TODO: tipizzare
-    getDistaccamenti(): Observable<any> {
-        return this.http.get<any>(API_DISTACCAMENTI + '/Get');
+    getDistaccamenti(): Observable<Sede[]> {
+        return this.http.get<Sede[]>(API_DISTACCAMENTI + '/Get');
     }
 
 }
