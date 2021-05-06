b5793403f79e5a333a78e4b251bf229ef7da0fed
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2021-05-06 17:57:24 +0200

    stampa funzionante

---
+++ src/backend/SO115App.API/Controllers/GestioneFileController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SO115App.Persistence.File.PDFManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SO115App.API.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GestioneFileController : ControllerBase
+    {
+        //TODO: fare query e commands
+
+        [HttpGet("Get")]
+        public async Task<IActionResult> Get(string path)
+        {
+            var manager = new PDFManager(path);
+
+            manager.prova();
+
+            manager.salva(/*"C:\\Users\\francescodangelis\\Desktop\\PDFResults\\"*/);
+
+            return Ok();
+        }
+    }
+}
---
+++ src/backend/SO115App.PDFManagement/PDFManagement/PDFManager.cs
@@ -0,0 +1,35 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using System.Text;
+
+namespace SO115App.Persistence.File.PDFManagement
+{
+    public class PDFManager
+    {
+        private PdfDocument _document;
+
+        private PDFManager() { }
+        public PDFManager(string filePath)
+        {
+            _document = PdfReader.Open(filePath, PdfDocumentOpenMode.Modify);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+        }
+
+        public void prova()
+        {
+            PdfPage page = _document.Pages[0];
+
+            XGraphics gfx = XGraphics.FromPdfPage(page);
+
+            XFont font = new XFont("Verdana", 50);
+
+            gfx.DrawString("Hello, World!", font, XBrushes.Black, new XRect(0, 0, page.Width, page.Height), XStringFormats.Center);
+        }
+
+        public void salva()
+        {
+            _document.Save("C:\\users\\francescodangelis\\desktop\\PDFResults\\prova.pdf");
+        }
+    }
+}
