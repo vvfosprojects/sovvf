25d08196baa55ebe18cea10cef31569aabc71e57
bek2495 <bekimhoxha2495@gmail.com>
2021-05-17 16:52:18 +0200

    fix: stampa riepilogo interventi; enti intervenuti.

--- src/frontend/launcherComponent/src/app/core/service/stampa-richieste/stampa-richiesta.service.ts
+++ src/frontend/launcherComponent/src/app/core/service/stampa-richieste/stampa-richiesta.service.ts
@@ -16,10 +16,10 @@ export class StampaRichiestaService {
     }
 
     getStampaRichiesta(obj: any): Observable<any> {
-        return this.http.get<any>(API_STAMPA_RICHIESTA + '?codice=' + obj.idRichiesta);
+        return this.http.get<any>(API_STAMPA_RICHIESTA + '/Get?codice=' + obj.idRichiesta);
     }
 
     stampaRiepilogoInterventi(obj: any): Observable<any> {
-        return this.http.post<any>(API_STAMPA_RICHIESTA + '/RiepilogoInterventi', obj);
+        return this.http.post<any>(API_STAMPA_RICHIESTA, obj);
     }
 }
--- src/frontend/launcherComponent/src/environments/environment.demo.ts
+++ src/frontend/launcherComponent/src/environments/environment.demo.ts
@@ -41,7 +41,7 @@ export const environment = {
         enti: '/api/GestioneEnti',
         rubricaPersonale: '/api/GestioneRubricaPersonale',
         trasferimentoChiamata: '/api/GestioneTrasferimentiChiamate',
-        stampaRichiesta: '/api/GestioneFile/DettaglioRichiesta/Get',
+        stampaRichiesta: '/api/GestioneFile/DettaglioRichiesta',
         modificaPartenza: '/api/GestionePartenza/ModificaPartenza',
         sostituzionePartenza: '/api/GestionePartenza/SostituzionePartenza',
         distaccamenti: '/api/Distaccamenti',
