0b6ebd60cc5b8eaefd207ca80b7ad94881bb2210
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2021-05-17 17:57:04 +0200

    fix vari, init stampa riepilogo interventi pdf

---
+++ src/backend/SO115App.Persistence.File/PDFManagement/TemplateModelForms/FiltriRiepilogoInterventi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SO115App.Persistence.File.PDFManagement.TemplateModelForms
+{
+    public class FiltriRiepilogoInterventi
+    {
+        public DateTime Da { get; set; }
+        public DateTime A { get; set; }
+        public string Turno { get; set; }
+        public string Squadra { get; set; }
+        public string Distaccamento { get; set; }
+
+        public AltriFiltri AltriFiltri { get; set; }
+    }
+
+    public class AltriFiltri
+    {
+        public bool SoloInterventi { get; set; } = false;
+        public bool TipologiaIntervento { get; set; } = false;
+        public bool Trasmessi { get; set; } = false;
+    }
+}
