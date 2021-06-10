1d2a8032cdbae00564f20712d4664ad3e088ca47
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2021-06-10 18:21:41 +0200

    Add - Aggiunto elenco di tutte le squadre di uno o più distaccamenti per filtraggio Riepilogo Stampe

---
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneSquadre/GetAllSquadre/GetAllSquadreResult.cs
@@ -0,0 +1,10 @@
+﻿using SO115App.Models.Classi.Composizione;
+using System.Collections.Generic;
+
+namespace SO115App.Models.Servizi.CQRS.Queries.GestioneSquadre.GetAllSquadre
+{
+    public class GetAllSquadreResult
+    {
+        public List<SquadraComposizione> DataArray { get; set; }
+    }
+}
