d914546da36caa6894eff6068b13dfa2ad29b4e7
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2021-06-10 16:38:41 +0200

    Add - Gestione sedi by codice sede. torna l'elenco delle sottosedi di un codice sede inviato nella Header

---
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneSedi/GetDistaccamentiByCodSede/GetDistaccamentiByCodSedeQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using CQRS.Queries;
+using SO115App.API.Models.Classi.Condivise;
+using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Distaccamenti;
+using System.Linq;
+
+namespace SO115App.Models.Servizi.CQRS.Queries.GestioneSedi.GetDistaccamentiByCodSede
+{
+    public class GetDistaccamentiByCodSedeQueryHandler : IQueryHandler<GetDistaccamentiByCodSedeQuery, GetDistaccamentiByCodSedeResult>
+    {
+        private readonly IGetSedi _service;
+
+        public GetDistaccamentiByCodSedeQueryHandler(IGetSedi service)
+        {
+            _service = service;
+        }
+
+        public GetDistaccamentiByCodSedeResult Handle(GetDistaccamentiByCodSedeQuery query)
+        {
+            var lstSedi = _service.GetAll();
+
+            var result = lstSedi.Where(s => s.attiva == 1 && s.codFiglio_TC >= 1000)
+                .Select(s => new Sede($"{s.codProv}.{s.codFiglio_TC}", s.sede, "", new Coordinate(s.latitudine, s.longitudine), "", "", "", "", ""))
+                .OrderBy(s => s.Codice)
+                .ToList();
+
+            return new GetDistaccamentiByCodSedeResult() { DataArray = result };
+        }
+    }
+}
