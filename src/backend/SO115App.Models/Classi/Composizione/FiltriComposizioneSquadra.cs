c8cfb77eb6a7b3561433a136b1597edc3ecc2f48
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2021-06-15 11:30:28 +0200

    fix nuovi filtri composizione squadra

--- src/backend/SO115App.Models/Classi/Composizione/FiltriComposizioneSquadra.cs
+++ src/backend/SO115App.Models/Classi/Composizione/FiltriComposizioneSquadra.cs
@@ -8,11 +8,11 @@ namespace SO115App.Models.Classi.Composizione
         public Paginazione Pagination { get; set; }
         public string[] CodiciDistaccamenti { get; set; }
         public string Ricerca { get; set; }
-        public TurnoRelativo? Turno { get; set; }
         public bool DiEmergenza { get; set; }
         public string CodiceChiamata { get; set; }
         public string[] CodiciCompetenze { get; set; }
-        public string Stato { get; set; }
+        public TurnoRelativo? Turno { get; set; }
+        public StatoSquadraComposizione? Stato { get; set; }
 
         [JsonPropertyName("codDistaccamentoSelezionato")]
         public string DistaccamentoSquadra { get; set; }
