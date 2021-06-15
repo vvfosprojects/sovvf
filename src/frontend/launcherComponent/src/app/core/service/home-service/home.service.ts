d667a3dbba40b072263b03c5c3ef2a80e8eac45f
bek2495 <bekimhoxha2495@gmail.com>
2021-06-15 17:21:00 +0200

    fix: headers welcome

--- src/frontend/launcherComponent/src/app/core/service/home-service/home.service.ts
+++ src/frontend/launcherComponent/src/app/core/service/home-service/home.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
 import { Welcome } from '../../../shared/interface/welcome.interface';
+import { LSNAME } from '../../settings/config';
 
 const BASE_URL = environment.baseUrl;
 const API_WELCOME = BASE_URL + environment.apiUrl.welcome;
@@ -10,7 +11,15 @@ const API_WELCOME = BASE_URL + environment.apiUrl.welcome;
 @Injectable()
 export class HomeService {
 
+    jwt = sessionStorage.getItem(LSNAME.token);
+    idUtente = JSON.parse(sessionStorage.getItem(LSNAME.currentUser)).id;
+    codiceSede = JSON.parse(sessionStorage.getItem(LSNAME.cacheSedi));
+    a = 'Bearer ' + this.jwt;
+    headers: any;
+
     constructor(private http: HttpClient) {
+        this.a = this.a.replace('"', '').replace('"', '');
+        this.headers = new HttpHeaders({ 'Content-Type': 'application/json', Authorization: this.a, CodiceSede: this.codiceSede, IdUtente: this.idUtente });
     }
 
     getHome(): Observable<Welcome> {
