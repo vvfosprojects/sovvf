4eebd0742d866b9e09466ee4a40f3304ea645a11
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2021-07-26 16:02:41 +0200

    fix vari

--- src/backend/SO115App.Models/Classi/Composizione/FiltriComposizioneMezzo.cs
+++ src/backend/SO115App.Models/Classi/Composizione/FiltriComposizioneMezzo.cs
@@ -4,9 +4,9 @@
     {
         public string Ricerca { get; set; }
         public string CodDistaccamentoSelezionato { get; set; }
+        public string CodMezzoSelezionato { get; set; }
         public string[]? Stato { get; set; }
         public string[]? CodiciDistaccamenti { get; set; }
         public string[]? Tipo { get; set; }
-        public string CodMezzoSelezionato { get; set; }
     }
 }
