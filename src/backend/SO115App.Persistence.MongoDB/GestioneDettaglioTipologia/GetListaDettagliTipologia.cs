a35ffd69743c41774153c201c605748da4658960
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2021-10-07 18:10:48 +0200

    Fix - Lista dettaglio tipologie

--- src/backend/SO115App.Persistence.MongoDB/GestioneDettaglioTipologia/GetListaDettagliTipologia.cs
+++ src/backend/SO115App.Persistence.MongoDB/GestioneDettaglioTipologia/GetListaDettagliTipologia.cs
@@ -34,7 +34,7 @@ namespace SO115App.Persistence.MongoDB.GestioneDettaglioTipologia
             var lstCodiciPin = listaPin.Select(c => c.Codice).ToList();
             var lstEnti = new List<TipologiaDettaglio>();
 
-            if (CodTipologia != 0)
+            if (CodTipologia != 0 && CodTipologia != null)
                 lstEnti = _dbContext.TipologiaDettaglioCollection.Find(c => lstCodiciPin.Contains(c.CodSede)
                 && (c.Descrizione.ToLower().Contains(text)) && (CodTipologia == c.CodiceTipologia)).ToList();
             else
