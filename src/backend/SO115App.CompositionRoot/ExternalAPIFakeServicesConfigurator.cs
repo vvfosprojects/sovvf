d53241680b3c92c48454b67a899cfd85202c9d25
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2021-10-11 15:34:50 +0200

    fix 3

--- src/backend/SO115App.CompositionRoot/ExternalAPIFakeServicesConfigurator.cs
+++ src/backend/SO115App.CompositionRoot/ExternalAPIFakeServicesConfigurator.cs
@@ -72,7 +72,7 @@ namespace SO115App.CompositionRoot
             container.Register(typeof(ExternalAPI.Client.IHttpRequestManager<>), typeof(ExternalAPI.Client.IHttpRequestManager<>).Assembly.DefinedTypes.First(n => n.Name.Contains("HttpRequestManager")));
 
             container.Register<IGetToken, ExternalAPI.Fake.Servizi.Gac.GetToken>();
-            container.Register<IGetToken_ESRI, ExternalAPI.Fake.Servizi.ESRI.GetToken>();
+            container.Register<IGetToken_ESRI, ExternalAPI.Fake.Servizi.ESRI.GetToken_ESRI>();
 
             #region IdentityManagement
 
