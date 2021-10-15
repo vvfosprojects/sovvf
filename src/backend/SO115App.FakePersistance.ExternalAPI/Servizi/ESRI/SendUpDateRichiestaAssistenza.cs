626db16456dfe6e7a6868b4dfcaef6dce9d659a7
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2021-10-15 16:36:31 +0200

    comentate chiamate esri

--- src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/ESRI/SendUpDateRichiestaAssistenza.cs
+++ src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/ESRI/SendUpDateRichiestaAssistenza.cs
@@ -27,29 +27,29 @@ namespace SO115App.ExternalAPI.Fake.Servizi.ESRI
 
         public void UpDate(ESRI_RichiestaMessage messaggio)
         {
-            List<ESRI_RichiestaMessage> listaMsg = new List<ESRI_RichiestaMessage>();
-            listaMsg.Add(messaggio);
-            var jsonString = JsonConvert.SerializeObject(listaMsg);
+            //List<ESRI_RichiestaMessage> listaMsg = new List<ESRI_RichiestaMessage>();
+            //listaMsg.Add(messaggio);
+            //var jsonString = JsonConvert.SerializeObject(listaMsg);
 
-            Dictionary<string, string> postData = new Dictionary<string, string>();
-            postData.Add("features", jsonString);
-            postData.Add("f", "json");
-            postData.Add("token", _getToken_ESRI.Get());
+            //Dictionary<string, string> postData = new Dictionary<string, string>();
+            //postData.Add("features", jsonString);
+            //postData.Add("f", "json");
+            //postData.Add("token", _getToken_ESRI.Get());
 
-            var multipartFormDataContent = new MultipartFormDataContent();
+            //var multipartFormDataContent = new MultipartFormDataContent();
 
-            foreach (var keyValuePair in postData)
-            {
-                multipartFormDataContent.Add(new StringContent(keyValuePair.Value),
-                    String.Format("\"{0}\"", keyValuePair.Key));
-            }
+            //foreach (var keyValuePair in postData)
+            //{
+            //    multipartFormDataContent.Add(new StringContent(keyValuePair.Value),
+            //        String.Format("\"{0}\"", keyValuePair.Key));
+            //}
 
-            var uri = new Uri(_configuration.GetSection("ESRI").GetSection("URLRichieste").Value + "/updateFeatures");
+            //var uri = new Uri(_configuration.GetSection("ESRI").GetSection("URLRichieste").Value + "/updateFeatures");
 
-            var result = _client.PostAsyncFormData(uri, multipartFormDataContent).Result;
+            //var result = _client.PostAsyncFormData(uri, multipartFormDataContent).Result;
 
-            if (result != null && result.updateResults[0].success == false)
-                throw new Exception($"Errore servizio ESRI");
+            //if (result != null && result.updateResults[0].success == false)
+            //    throw new Exception($"Errore servizio ESRI");
         }
     }
 }
