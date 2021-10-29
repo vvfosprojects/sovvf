7a70d7338a3f3920ad8105bec49540e2d75b71da
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2021-10-29 12:26:58 +0200

    fix temporaneo per coordinate mezzo

--- src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/Gac/GetMezziUtilizzabili.cs
+++ src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/Gac/GetMezziUtilizzabili.cs
@@ -195,7 +195,7 @@ namespace SO115App.ExternalAPI.Fake.Servizi.Gac
             try
             {
                 return new Mezzo(mezzoDto.CodiceMezzo, mezzoDto.Descrizione, mezzoDto.Genere, Costanti.MezzoInSede,
-                    mezzoDto.CodiceDistaccamento, sede, new Coordinate(0.0, 0.0))
+                    mezzoDto.CodiceDistaccamento, sede, new Coordinate(distaccamento.Coordinate?.Latitudine ?? 0.0, distaccamento.Coordinate?.Longitudine ?? 0.0))
                 {
                     DescrizioneAppartenenza = mezzoDto.DescrizioneAppartenenza,
                 };
