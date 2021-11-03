c6f7fa55e5dc77dacb114ac2d77b9625464eee80
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2021-11-03 15:43:50 +0100

    Moduli di Colonna Mobile - Creato il metodo GetEmergenzaById

---
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneEmergenza/GetEmergenzaById/GetEmergenzaByIdQueryHandler.cs
@@ -0,0 +1,26 @@
+﻿using CQRS.Queries;
+using SO115App.Models.Classi.Emergenza;
+using SO115App.Models.Servizi.Infrastruttura.GestioneEmergenza;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SO115App.Models.Servizi.CQRS.Queries.GestioneEmergenza.GetEmergenzaById
+{
+    public class GetEmergenzaByIdQueryHandler : IQueryHandler<GetEmergenzaByIdQuery, GetEmergenzaByIdResult>
+    {
+        private readonly IGetEmergenzaById _getEmergenza;
+
+        public GetEmergenzaByIdQueryHandler(IGetEmergenzaById getEmergenza)
+        {
+            _getEmergenza = getEmergenza;
+        }
+
+        public GetEmergenzaByIdResult Handle(GetEmergenzaByIdQuery query)
+        {
+            return new GetEmergenzaByIdResult()
+            {
+                emergenza = _getEmergenza.Get(query.IdEmergenza)
+            };
+        }
+    }
+}
---
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneEmergenza/GetEmergenzaById/GetEmergenzaByIdResult.cs
@@ -0,0 +1,11 @@
+﻿using SO115App.Models.Classi.Condivise;
+using SO115App.Models.Classi.Emergenza;
+using System.Collections.Generic;
+
+namespace SO115App.Models.Servizi.CQRS.Queries.GestioneEmergenza.GetEmergenzaById
+{
+    public class GetEmergenzaByIdResult
+    {
+        public Emergenza emergenza { get; set; }
+    }
+}
