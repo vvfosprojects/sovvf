a2c79869f035b39f3f6e9035e5f09ab036688308
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2021-11-04 17:21:08 +0100

    Pulizia codice

--- src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneEmergenza/GetListaEmergenzeByCodComando/GetListaEmergenzeByCodComandoQuery.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneEmergenza/GetListaEmergenzeByCodComando/GetListaEmergenzeByCodComandoQuery.cs
@@ -21,11 +21,5 @@ namespace SO115App.Models.Servizi.CQRS.Queries.GestioneEmergenza.GetListaEmergen
         ///   l'oggetto paginazione con i parametri utili per la paginazione
         /// </summary>
         public Paginazione Pagination { get; set; }
-
-        /// <summary>
-        ///   Search è un testo libero che l'utente digita per ricercare un determinato record in
-        ///   dettaglioTipologia Ricerca Full-Text sul campo DESCRIZIONE della basedati
-        /// </summary>
-        //public FiltriDettaglioTipologia? Filters { get; set; }
     }
 }
