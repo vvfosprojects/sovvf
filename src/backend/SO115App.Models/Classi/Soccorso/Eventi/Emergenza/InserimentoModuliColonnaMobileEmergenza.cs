8195e79990772cdeac07dc29e1f9e3fbc36284c5
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2021-11-08 16:20:02 +0100

    Moduli colonna Mobile - Aggiunto parametro Elenco Colonne Mobili

---
+++ src/backend/SO115App.Models/Classi/Soccorso/Eventi/Emergenza/InserimentoModuliColonnaMobileEmergenza.cs
@@ -0,0 +1,18 @@
+﻿using SO115App.API.Models.Classi.Soccorso.Eventi;
+using System;
+
+namespace SO115App.Models.Classi.Soccorso.Eventi.Emergenza
+{
+    public class InserimentoModuliColonnaMobileEmergenza : Evento
+    {
+        public InserimentoModuliColonnaMobileEmergenza(DateTime istante,
+                         string codiceFonte,
+                         string codice,
+                         string codSedePresaInCarico) : base(istante, codiceFonte, codice, "InserimentoModuliColonnaMobileEmergenza")
+        {
+            CodSedePresaInCarico = codSedePresaInCarico;
+        }
+
+        public string CodSedePresaInCarico;
+    }
+}
