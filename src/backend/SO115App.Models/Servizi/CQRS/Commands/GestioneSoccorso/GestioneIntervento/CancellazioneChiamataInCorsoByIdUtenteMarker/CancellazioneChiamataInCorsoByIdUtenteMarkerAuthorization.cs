de040495e4459325d1fc823ade89997f1986a4eb
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2021-11-11 15:16:11 +0100

    Fix logout

--- src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneSoccorso/GestioneIntervento/CancellazioneChiamataInCorsoByIdUtenteMarker/CancellazioneChiamataInCorsoByIdUtenteMarkerAuthorization.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneSoccorso/GestioneIntervento/CancellazioneChiamataInCorsoByIdUtenteMarker/CancellazioneChiamataInCorsoByIdUtenteMarkerAuthorization.cs
@@ -51,14 +51,14 @@ namespace DomainModel.CQRS.Commands.ChiamataInCorsoMarker
                     yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
                 else
                 {
-                    bool abilitato = false;
+                    bool abilitato = true;
 
-                    if (_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.CodiceSede, Costanti.GestoreChiamate))
-                        abilitato = true;
-                    if (_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.CodiceSede, Costanti.Amministratore))
-                        abilitato = true;
-                    if (_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.CodiceSede, Costanti.GestoreRichieste))
-                        abilitato = true;
+                    //if (_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.CodiceSede, Costanti.GestoreChiamate))
+                    //    abilitato = true;
+                    //if (_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.CodiceSede, Costanti.Amministratore))
+                    //    abilitato = true;
+                    //if (_getAutorizzazioni.GetAutorizzazioniUtente(user.Ruoli, command.CodiceSede, Costanti.GestoreRichieste))
+                    //    abilitato = true;
 
                     if (!abilitato)
                         yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
