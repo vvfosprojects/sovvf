35b44c0a42962fc3bca46d3aa407074eb09b86cb
notmikenot <micheledragonetti99@gmail.com>
2021-11-12 16:04:52 +0100

    Fix compilazione Client

--- src/frontend/launcherComponent/src/app/features/zone-emergenza/zone-emergenza.module.ts
+++ src/frontend/launcherComponent/src/app/features/zone-emergenza/zone-emergenza.module.ts
@@ -31,7 +31,7 @@ import { ZoneEmergenzaRouting } from './zone-emergenza.routing';
  */
 import { ZoneEmergenzaState } from './store/states/zone-emergenza/zone-emergenza.state';
 import { ModuliColonnaMobileState } from './store/states/moduli-colonna-mobile/moduli-colonna-mobile.state';
-import { PcaModalComponent } from './sedi-zona-emergenza/pca-modal/pca-modal.component';
+// import { PcaModalComponent } from './sedi-zona-emergenza/pca-modal/pca-modal.component';
 
 const ngWizardConfig: NgWizardConfig = {
     theme: THEME.default
@@ -49,7 +49,7 @@ const ngWizardConfig: NgWizardConfig = {
         ZonaEmergenzaModalComponent,
         SediZonaEmergenzaComponent,
         DoaModalComponent,
-        PcaModalComponent
+        // PcaModalComponent
     ],
     imports: [
         CommonModule,
