3f34abcbab8d7e16163388316c37aa6156c74e2f
notmikenot <micheledragonetti99@gmail.com>
2021-11-30 13:25:37 +0100

    Fix "Cancella Ricerca"

--- src/frontend/launcherComponent/src/app/features/pos/ricerca-pos/ricerca-pos.component.ts
+++ src/frontend/launcherComponent/src/app/features/pos/ricerca-pos/ricerca-pos.component.ts
@@ -1,19 +1,17 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { NgbDropdownConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-ricerca-pos',
     templateUrl: './ricerca-pos.component.html',
-    styleUrls: ['./ricerca-pos.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    styleUrls: ['./ricerca-pos.component.css']
 })
 export class RicercaPosComponent {
 
+    @Input() ricerca: string;
     @Input() loading: boolean;
 
     @Output() ricercaChange = new EventEmitter<any>();
-
-    ricerca: string;
 
     constructor(config: NgbDropdownConfig) {
         config.placement = 'bottom-right';
