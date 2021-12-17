13310321bf6054b6dd61bf4ab7d519613ccabfdb
notmikenot <micheledragonetti99@gmail.com>
2021-12-17 14:57:14 +0100

    Fix visualizzazione percorsi relativi alle partenze dell'intervento selezionato

---
+++ src/frontend/launcherComponent/src/app/core/settings/esri-layers-config.ts
@@ -0,0 +1,13 @@
+export const ESRI_LAYERS_CONFIG = {
+    layers: {
+        mezzi: 'LOCALIZZAZIONE_MEZZI_VVF_0',
+        schedeContatto: {
+            gestite: 'Gestita',
+            nonGestite: 'SchedeContatto - Non Gestita'
+        },
+        interventi: {
+            aperti: 'Interventi - Aperti',
+            chiusi: 'Interventi - Chiusi'
+        }
+    }
+};
