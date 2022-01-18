307c274efb6788325fc904a2ee82564a01348b75
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2022-01-18 15:28:08 +0100

    Init - Progetto esterno di topo SOAP: CAP

---
+++ src/backend/SO115App.CAP/SO115App.CAP/ISvcCap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SO115App.CAP
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di interfaccia "ISvcCap" nel codice e nel file di configurazione contemporaneamente.
+    [ServiceContract]
+    public interface ISvcCap
+    {
+        [OperationContract]
+        void DoWork();
+    }
+}
---
+++ src/backend/SO115App.CAP/SO115App.CAP/SvcCap.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace SO115App.CAP
+{
+    // NOTA: è possibile utilizzare il comando "Rinomina" del menu "Refactoring" per modificare il nome di classe "SvcCap" nel codice, nel file svc e nel file di configurazione contemporaneamente.
+    // NOTA: per avviare il client di prova WCF per testare il servizio, selezionare SvcCap.svc o SvcCap.svc.cs in Esplora soluzioni e avviare il debug.
+    public class SvcCap : ISvcCap
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
