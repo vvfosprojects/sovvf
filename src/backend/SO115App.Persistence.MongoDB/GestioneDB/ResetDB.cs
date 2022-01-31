a0a322d09125f962cc65bfa0dfe03a64d5e064ba
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-01-31 18:13:39 +0100

    Gestione DB - Reset riabilitato

--- src/backend/SO115App.Persistence.MongoDB/GestioneDB/ResetDB.cs
+++ src/backend/SO115App.Persistence.MongoDB/GestioneDB/ResetDB.cs
@@ -16,35 +16,35 @@ namespace SO115App.Persistence.MongoDB.GestioneDB
         {
             try
             {
-                //if (!_dbContext.DeleteDB("richiesteAssistenza"))
-                //    return false;
-
-                //if (!_dbContext.DeleteDB("chiamateInCorso"))
-                //    return false;
-
-                //if (!_dbContext.DeleteDB("statoMezzo"))
-                //    return false;
-
-                //if (!_dbContext.DeleteDB("statoSquadra"))
-                //    return false;
-
-                //if (!_dbContext.DeleteDB("trasferimentiChiamate"))
-                //    return false;
-
-                //if (!_dbContext.DeleteDB("composizioneMezzi"))
-                //    return false;
-
-                //if (!_dbContext.DeleteDB("composizioneSquadre"))
-                //    return false;
-
-                //if (!_dbContext.DeleteDB("unitaOperative"))
-                //    return false;
-
-                //if (!_dbContext.DeleteDB("emergenza"))
-                //    return false;
-
-                if (!_dbContext.DeleteDB("schedecontatto"))
+                if (!_dbContext.DeleteDB("richiesteAssistenza"))
                     return false;
+
+                if (!_dbContext.DeleteDB("chiamateInCorso"))
+                    return false;
+
+                if (!_dbContext.DeleteDB("statoMezzo"))
+                    return false;
+
+                if (!_dbContext.DeleteDB("statoSquadra"))
+                    return false;
+
+                if (!_dbContext.DeleteDB("trasferimentiChiamate"))
+                    return false;
+
+                if (!_dbContext.DeleteDB("composizioneMezzi"))
+                    return false;
+
+                if (!_dbContext.DeleteDB("composizioneSquadre"))
+                    return false;
+
+                if (!_dbContext.DeleteDB("unitaOperative"))
+                    return false;
+
+                if (!_dbContext.DeleteDB("emergenza"))
+                    return false;
+
+                //if (!_dbContext.DeleteDB("schedecontatto"))
+                //    return false;
 
                 return true;
             }
