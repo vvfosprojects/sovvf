ca1c5c2797908fe9b292c779851d9a71e7bc0927
notmikenot <micheledragonetti99@gmail.com>
2022-02-21 16:02:25 +0100

    Fix glitch "In Attesa" e "Segnala Urgenza e condividi in gestione" nel Form Chiamata

--- src/frontend/launcherComponent/src/app/shared/interface/forms/richiesta-form.model.ts
+++ src/frontend/launcherComponent/src/app/shared/interface/forms/richiesta-form.model.ts
@@ -10,7 +10,7 @@ import { Fonogramma } from '../../model/fonogramma.model';
 import { Partenza } from '../../model/partenza.model';
 import { AttivitaUtente } from '../../model/attivita-utente.model';
 
-export interface RichiestaForm {
+export class RichiestaForm {
     id: string;
     codice: string;
     codiceRichiesta: string;
@@ -69,4 +69,65 @@ export interface RichiestaForm {
     listaUtentiPresaInCarico: AttivitaUtente[];
     codUOCompetenza: string[];
     noteNue: string;
+
+    constructor() {
+        this.id = null;
+        this.codice = null;
+        this.codiceRichiesta = null;
+        this.operatore = null;
+        this.istanteRicezioneRichiesta = null;
+        this.stato = null;
+        this.codTipologia = null;
+        this.dettaglioTipologia = null;
+        this.nominativo = null;
+        this.telefono = null;
+        this.competenze = null;
+        this.codCompetenzaCentrale = null;
+        this.codPrimaCompetenza = null;
+        this.codSecondaCompetenza = null;
+        this.codTerzaCompetenza = null;
+        this.complessita = null;
+        this.istantePresaInCarico = null;
+        this.istantePrimaAssegnazione = null;
+        this.trnInsChiamata = null;
+        this.turnoIntervento = null;
+        this.boschi = null;
+        this.campi = null;
+        this.sterpaglie = null;
+        this.indirizzo = null;
+        this.latitudine = null;
+        this.longitudine = null;
+        this.provincia = null;
+        this.cap = null;
+        this.regione = null;
+        this.civico = null;
+        this.piano = null;
+        this.palazzo = null;
+        this.scala = null;
+        this.interno = null;
+        this.etichette = null;
+        this.noteIndirizzo = null;
+        this.obiettivoSensibile = null;
+        this.rilevanzaGrave = null;
+        this.rilevanzaStArCu = null;
+        this.notePrivate = null;
+        this.notePubbliche = null;
+        this.descrizione = null;
+        this.zoneEmergenza = null;
+        this.prioritaRichiesta = null;
+        this.codSchedaContatto = null;
+        this.listaEntiPresaInCarico = null;
+        this.urgenza = null;
+        this.esercitazione = null;
+        this.codSOCompetente = null;
+        this.sediAllertate = null;
+        this.codSOAllertate = null;
+        this.fonogramma = null;
+        this.partenze = null;
+        this.codEntiIntervenuti = null;
+        this.motivazione = null;
+        this.listaUtentiPresaInCarico = null;
+        this.codUOCompetenza = null;
+        this.noteNue = null;
+    }
 }
