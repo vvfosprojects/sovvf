6e716039d6b940c3e53204240362e59b814b905e
notmikenot <micheledragonetti99@gmail.com>
2022-02-28 17:17:17 +0100

    Inizio implementazione concorrenza su "Cambio stato mezzo"

--- src/frontend/launcherComponent/src/app/shared/components/mezzo/mezzo-actions/mezzo-actions.component.ts
+++ src/frontend/launcherComponent/src/app/shared/components/mezzo/mezzo-actions/mezzo-actions.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { NgbDropdownConfig, NgbTooltipConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownConfig, NgbModal, NgbTooltipConfig } from '@ng-bootstrap/ng-bootstrap';
 import { Mezzo } from '../../../model/mezzo.model';
 import { StatoMezzoActions } from '../../../enum/stato-mezzo-actions.enum';
 import { StatoMezzo } from 'src/app/shared/enum/stato-mezzo.enum';
@@ -7,6 +7,8 @@ import { MezzoActionsModalComponent } from 'src/app/shared/modal/mezzo-actions-m
 import { MezzoActionEmit } from '../../../interface/mezzo-action-emit.interface';
 import { calcolaActionSuggeritaMezzo, statoMezzoActionColor, statoMezzoActionsEnumToStringArray } from '../../../helper/function-mezzo';
 import { EventoMezzo } from '../../../interface/evento-mezzo.interface';
+import { LockedConcorrenzaService } from '../../../../core/service/concorrenza-service/locked-concorrenza.service';
+import { TipoConcorrenzaEnum } from '../../../enum/tipo-concorrenza.enum';
 
 @Component({
     selector: 'app-mezzo-actions',
@@ -16,6 +18,7 @@ import { EventoMezzo } from '../../../interface/evento-mezzo.interface';
 })
 export class MezzoActionsComponent implements OnInit {
 
+    @Input() idRichiesta: string;
     @Input() mezzo: Mezzo;
     @Input() codicePartenza: string;
     @Input() doubleMonitor: Mezzo;
@@ -34,11 +37,10 @@ export class MezzoActionsComponent implements OnInit {
             stato: e,
         }));
 
-    constructor(
-        dropdownConfig: NgbDropdownConfig,
-        tooltipConfig: NgbTooltipConfig,
-        private modalService: NgbModal
-    ) {
+    constructor(dropdownConfig: NgbDropdownConfig,
+                tooltipConfig: NgbTooltipConfig,
+                private modalService: NgbModal,
+                private lockedConcorrenzaService: LockedConcorrenzaService) {
         dropdownConfig.container = 'body';
         dropdownConfig.placement = 'top';
         tooltipConfig.container = 'body';
@@ -51,63 +53,65 @@ export class MezzoActionsComponent implements OnInit {
     }
 
     onClick(action?: string, ora?: string, event?: MouseEvent): void {
-        if (event) {
-            event.stopPropagation();
-        }
-        let modal;
-        const dataInViaggio = {
-            anno: '',
-            mese: '',
-            giorno: '',
-            ora: '',
-            minuti: ''
-        };
-        if (ora) {
-            const indexOra = ora.indexOf('T') + 1;
-            const indexMin = ora.indexOf(':') + 1;
-            const indexFirstCut = ora.indexOf('-') + 1;
-            const indexSecondCut = ora.lastIndexOf('-') + 1;
-            dataInViaggio.ora = ora.slice(indexOra, indexOra + 2);
-            dataInViaggio.minuti = ora.slice(indexMin, indexMin + 2);
-            dataInViaggio.anno = ora.slice(0, 4);
-            dataInViaggio.mese = ora.slice(indexFirstCut, indexSecondCut - 1);
-            dataInViaggio.giorno = ora.slice(indexSecondCut, indexOra - 1);
-        }
-        modal = this.modalService.open(MezzoActionsModalComponent, {
-            windowClass: 'modal-holder',
-            backdropClass: 'light-blue-backdrop',
-            size: 'lg',
-            centered: true
-        });
-        modal.componentInstance.codicePartenza = this.codicePartenza;
-        modal.componentInstance.statoMezzo = this.mezzo.stato;
-        modal.componentInstance.title = !ora ? 'Conferma' : 'Modifica';
-        modal.componentInstance.titleStato = !ora ? '' : ': ' + action;
-        modal.componentInstance.dataInViaggio = dataInViaggio;
-        modal.componentInstance.listaEventi = this.listaEventi;
-        modal.result.then((res: { status: string, result: any }) => {
-            switch (res.status) {
-                case 'ok' :
-                    if (action) {
-                        this.statoMezzoActions = StatoMezzoActions[action.replace(' ', '')];
-                        const orario = res.result.oraEvento;
-                        const data = res.result.dataEvento;
-                        const azioneIntervento = res.result.azioneIntervento;
-                        this.actionMezzo.emit({
-                            mezzoAction: this.statoMezzoActions,
-                            oraEvento: { ora: orario.hour, minuti: orario.minute, secondi: orario.second },
-                            dataEvento: { giorno: data.day, mese: data.month, anno: data.year },
-                            azioneIntervento,
-                            codicePartenza: this.codicePartenza
-                        });
-                    } else {
-                        this.actionMezzo.emit();
-                    }
-                    break;
-                case 'ko':
-                    break;
+        if (!this.lockedConcorrenzaService.getLockedConcorrenza(TipoConcorrenzaEnum.Richiesta, this.idRichiesta)) {
+            if (event) {
+                event.stopPropagation();
             }
-        });
+            let modal;
+            const dataInViaggio = {
+                anno: '',
+                mese: '',
+                giorno: '',
+                ora: '',
+                minuti: ''
+            };
+            if (ora) {
+                const indexOra = ora.indexOf('T') + 1;
+                const indexMin = ora.indexOf(':') + 1;
+                const indexFirstCut = ora.indexOf('-') + 1;
+                const indexSecondCut = ora.lastIndexOf('-') + 1;
+                dataInViaggio.ora = ora.slice(indexOra, indexOra + 2);
+                dataInViaggio.minuti = ora.slice(indexMin, indexMin + 2);
+                dataInViaggio.anno = ora.slice(0, 4);
+                dataInViaggio.mese = ora.slice(indexFirstCut, indexSecondCut - 1);
+                dataInViaggio.giorno = ora.slice(indexSecondCut, indexOra - 1);
+            }
+            modal = this.modalService.open(MezzoActionsModalComponent, {
+                windowClass: 'modal-holder',
+                backdropClass: 'light-blue-backdrop',
+                size: 'lg',
+                centered: true
+            });
+            modal.componentInstance.codicePartenza = this.codicePartenza;
+            modal.componentInstance.statoMezzo = this.mezzo.stato;
+            modal.componentInstance.title = !ora ? 'Conferma' : 'Modifica';
+            modal.componentInstance.titleStato = !ora ? '' : ': ' + action;
+            modal.componentInstance.dataInViaggio = dataInViaggio;
+            modal.componentInstance.listaEventi = this.listaEventi;
+            modal.result.then((res: { status: string, result: any }) => {
+                switch (res.status) {
+                    case 'ok' :
+                        if (action) {
+                            this.statoMezzoActions = StatoMezzoActions[action.replace(' ', '')];
+                            const orario = res.result.oraEvento;
+                            const data = res.result.dataEvento;
+                            const azioneIntervento = res.result.azioneIntervento;
+                            this.actionMezzo.emit({
+                                mezzoAction: this.statoMezzoActions,
+                                oraEvento: { ora: orario.hour, minuti: orario.minute, secondi: orario.second },
+                                dataEvento: { giorno: data.day, mese: data.month, anno: data.year },
+                                azioneIntervento,
+                                codicePartenza: this.codicePartenza
+                            });
+                        } else {
+                            this.actionMezzo.emit();
+                        }
+                        break;
+                    case 'ko':
+                        break;
+                }
+            });
+        }
     }
 
     getListaEventiMezzo(): void {
