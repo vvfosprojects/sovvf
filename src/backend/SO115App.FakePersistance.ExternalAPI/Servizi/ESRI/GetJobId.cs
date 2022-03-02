84a9da60310cf62b6f0146f3dcf33ce5a01efca0
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2022-03-02 17:53:31 +0100

    fix vari - Esri chiamata per distanza e tempo mezzi

--- src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/ESRI/GetJobId.cs
+++ src/backend/SO115App.FakePersistance.ExternalAPI/Servizi/ESRI/GetJobId.cs
@@ -4,6 +4,7 @@ using SO115App.Models.Classi.ESRI;
 using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.ESRI;
 using System;
 using System.Net.Http;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SO115App.ExternalAPI.Fake.Servizi.ESRI
@@ -19,13 +20,14 @@ namespace SO115App.ExternalAPI.Fake.Servizi.ESRI
             _config = config;
         }
 
-        public async Task<string> Get(string token)
+        public async Task<string> Get(string token, ESRI_DistanzaTempoMezzi obj)
         {
             try
             {
                 var url = new Uri(_config.GetSection("ESRI").GetSection("URLJobId").Value.Replace("{token}", token));
+                var json = JsonSerializer.Serialize(obj);
 
-                var result = _client.PostAsync(url, new StringContent("{ }")).Result;
+                var result = _client.PostAsync(url, new StringContent(json)).Result;
 
                 return result.jobId;
             }
