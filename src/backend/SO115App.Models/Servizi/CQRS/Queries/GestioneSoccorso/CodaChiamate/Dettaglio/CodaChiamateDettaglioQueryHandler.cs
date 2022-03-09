09b3534c7d6b58a39223cfa480c4baf73e284d43
notmikenot <micheledragonetti99@gmail.com>
2022-03-09 13:19:36 +0100

    Fix controller Coda Chiamate

--- src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneSoccorso/CodaChiamate/Dettaglio/CodaChiamateDettaglioQueryHandler.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneSoccorso/CodaChiamate/Dettaglio/CodaChiamateDettaglioQueryHandler.cs
@@ -1,4 +1,4 @@
-﻿//-----------------------------------------------------------------------
+//-----------------------------------------------------------------------
 // <copyright file="CodaChiamateDettaglioQueryHandler.cs" company="CNVVF">
 // Copyright (C) 2017 - CNVVF
 //
@@ -101,7 +101,7 @@ namespace SO115App.API.Models.Servizi.CQRS.Queries.GestioneSoccorso.CodaChiamate
             {
                 codDistaccamento = query.CodiceSede,
                 descDistaccamento = listaSedi.ToList().Find(x => x.Codice.Equals(query.CodiceSede)).Codice.Contains("1000") ? 
-                    "Comando VV.F. " + listaSedi.ToList().Find(x => x.Codice.Equals(query.CodiceSede)).Nome 
+                    listaSedi.ToList().Find(x => x.Codice.Equals(query.CodiceSede)).Nome 
                     : listaSedi.ToList().Find(x => x.Codice.Equals(query.CodiceSede)).Nome,
                 listaSintesi = listaSintesi != null ? listaSintesi.FindAll(x => x.CodUOCompetenza[0].Equals(query.CodiceSede) && (x.Stato.Equals("Chiamata") || x.Sospesa)) : null,
                 listaSquadre = listaSquadre.FindAll(x => x.Distaccamento.Codice.Equals(query.CodiceSede) && x.Turno.Equals(char.Parse(turnoCorrente.Substring(0, 1)))).Select(x => new Squadra()
