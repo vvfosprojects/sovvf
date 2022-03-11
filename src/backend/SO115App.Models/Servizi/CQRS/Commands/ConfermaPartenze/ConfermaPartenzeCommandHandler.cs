faa068dfa4e537d2d3d8c835fa2e0d23873cc990
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-03-11 11:05:55 +0100

    Sganciamento - Fix

--- src/backend/SO115App.Models/Servizi/CQRS/Commands/ConfermaPartenze/ConfermaPartenzeCommandHandler.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Commands/ConfermaPartenze/ConfermaPartenzeCommandHandler.cs
@@ -215,7 +215,7 @@ namespace SO115App.API.Models.Servizi.CQRS.Queries.GestioneSoccorso.Composizione
             foreach (var partenza in command.ConfermaPartenze.Partenze.Where(p => p.Codice == "0" || p.Codice == null))
             {
                 codpart++;
-                partenza.Codice = partenza.Mezzo.Appartenenza.Substring(0, 2) + codpart;
+                partenza.Codice = partenza.Mezzo.Distaccamento.Codice.Substring(0, 2) + codpart;
                 partenza.Turno = _getTurno.Get().Codice;
 
                 command.Richiesta.ListaEventi.OfType<ComposizionePartenze>().Last(e => e.CodiceMezzo.Equals(partenza.Mezzo.Codice)).CodicePartenza = partenza.Codice;
