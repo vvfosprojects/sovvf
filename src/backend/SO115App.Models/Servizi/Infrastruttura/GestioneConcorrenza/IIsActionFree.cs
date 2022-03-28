da41b07016520a11a7b2d35183f0039cee5aaded
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-03-28 11:39:16 +0200

    Inizio gestione concorrenza

---
+++ src/backend/SO115App.Models/Servizi/Infrastruttura/GestioneConcorrenza/IIsActionFree.cs
@@ -0,0 +1,10 @@
+﻿using SO115App.Models.Classi.Concorrenza;
+using System.Collections.Generic;
+
+namespace SO115App.Models.Servizi.Infrastruttura.GestioneConcorrenza
+{
+    public interface IIsActionFree
+    {
+        public bool Check(string action);
+    }
+}
---
+++ src/backend/SO115App.Persistence.MongoDB/GestioneConcorrenza/IsActionFree.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Driver;
+using Persistence.MongoDB;
+using SO115App.Models.Servizi.Infrastruttura.GestioneConcorrenza;
+using System.Linq;
+
+namespace SO115App.Persistence.MongoDB.GestioneConcorrenza
+{
+    public class IsActionFree : IIsActionFree
+    {
+        private readonly DbContext _dbContext;
+
+        public IsActionFree(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public bool Check(string action)
+        {
+            var listaAzioniBloccate = _dbContext.ConcorrenzaCollection.Find(c => c.Id != null).ToList();
+
+            return false;
+        }
+    }
+}
