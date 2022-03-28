809b3e5851541e940f419844c9f0cdd9b48fb727
notmikenot <micheledragonetti99@gmail.com>
2022-03-28 12:49:50 +0200

    Aggiornato ENUM "TipoConcorrenza"

--- src/frontend/launcherComponent/src/app/shared/enum/tipo-concorrenza.enum.ts
+++ src/frontend/launcherComponent/src/app/shared/enum/tipo-concorrenza.enum.ts
@@ -1,5 +1,25 @@
 export enum TipoConcorrenzaEnum {
-    'Richiesta',
-    'Mezzo',
-    'Squadra'
+    Richiesta,
+    Mezzo,
+    Squadra,
+    ChiusuraChiamata,
+    ChiusuraIntervento,
+    Modifica,
+    Trasferimento,
+    InvioPartenza,
+    Allerta,
+    CambioStatoPartenza,
+    GestisciPartenza,
+    Sganciamento,
+    RegistrazioneSchedaContatto,
+    AggiungiTrasferimento,
+    ModificaPos,
+    EliminaPos,
+    ModificaPianiDiscendenti,
+    EliminaPianiDiscendenti,
+    ModificaDettaglioTipologia,
+    EliminaDettaglioTipologia,
+    ModificaTriage,
+    EliminaTriage,
+    AggiungiRuoloUtente
 }
