dea1b9f2f978282e4f4a40f04f8569724b7e3515
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-03-29 18:03:11 +0200

    Gestione Concorrenza: - Modifica Triage - Aggiunta ruolo utente

--- src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneSoccorso/GestioneTriage/UpDateTriage/UpDateTriageAuthorization.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Commands/GestioneSoccorso/GestioneTriage/UpDateTriage/UpDateTriageAuthorization.cs
@@ -1,8 +1,11 @@
 ﻿using CQRS.Authorization;
 using CQRS.Commands.Authorizers;
+using SO115App.Models.Classi.Concorrenza;
 using SO115App.Models.Classi.Utility;
 using SO115App.Models.Servizi.Infrastruttura.Autenticazione;
+using SO115App.Models.Servizi.Infrastruttura.GestioneConcorrenza;
 using SO115App.Models.Servizi.Infrastruttura.GestioneUtenti.VerificaUtente;
+using SO115App.Models.Servizi.Infrastruttura.Utility;
 using System;
 using System.Collections.Generic;
 using System.Security.Principal;
@@ -14,12 +17,19 @@ namespace SO115App.Models.Servizi.CQRS.Commands.GestioneSoccorso.GestioneTriage.
         private readonly IPrincipal _currentUser;
         private readonly IFindUserByUsername _findUserByUsername;
         private readonly IGetAutorizzazioni _getAutorizzazioni;
+        private readonly IGetSottoSediByCodSede _getSottoSediByCodSede;
+        private readonly IIsActionFree _isActionFree;
 
-        public UpDateTriageAuthorization(IPrincipal currentUser, IFindUserByUsername findUserByUsername, IGetAutorizzazioni getAutorizzazioni)
+        public UpDateTriageAuthorization(IPrincipal currentUser, IFindUserByUsername findUserByUsername,
+            IGetAutorizzazioni getAutorizzazioni,
+            IGetSottoSediByCodSede getSottoSediByCodSede,
+            IIsActionFree isActionFree)
         {
             _currentUser = currentUser;
             _findUserByUsername = findUserByUsername;
             _getAutorizzazioni = getAutorizzazioni;
+            _getSottoSediByCodSede = getSottoSediByCodSede;
+            _isActionFree = isActionFree;
         }
 
         public IEnumerable<AuthorizationResult> Authorize(UpDateTriageCommand command)
@@ -33,6 +43,16 @@ namespace SO115App.Models.Servizi.CQRS.Commands.GestioneSoccorso.GestioneTriage.
                     yield return new AuthorizationResult(Costanti.UtenteNonAutorizzato);
                 else
                 {
+                    #region Concorrenza
+
+                    //Controllo Concorrenza
+                    var listaSediInteressate = _getSottoSediByCodSede.Get(new string[1] { command.Triage.CodiceSede });
+
+                    if (!_isActionFree.Check(TipoOperazione.ModificaTriage, user.Id, listaSediInteressate.ToArray(), command.Triage.Id))
+                        yield return new AuthorizationResult($"In questo momento l'intervento risulta occupato da un altro operatore. L'operazione non può essere eseguita");
+
+                    #endregion Concorrenza
+
                     Boolean abilitato = false;
                     foreach (var ruolo in user.Ruoli)
                     {
