5f4c494106ef5e906e1993b1b68f68ba68ac6937
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-04-27 16:25:29 +0200

    POS - Aggiunta notifica e Fix

---
+++ src/backend/SO115App.SignalR/Sender/GestionePos/NotificationEditPos.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SO115App.Models.Servizi.CQRS.Commands.GestioneSoccorso.GestionePOS.EditPos;
+using SO115App.Models.Servizi.Infrastruttura.Notification.GestioneSchedeContatto;
+using SO115App.SignalR.Utility;
+using System.Threading.Tasks;
+
+namespace SO115App.SignalR.Sender.GestionePos
+{
+    public class NotificationEditPos : INotificationEditPos
+    {
+        private readonly IHubContext<NotificationHub> _notificationHubContext;
+        private readonly GetGerarchiaToSend _getGerarchiaToSend;
+
+        public NotificationEditPos(IHubContext<NotificationHub> notificationHubContext,
+                                               GetGerarchiaToSend getGerarchiaToSend)
+        {
+            _notificationHubContext = notificationHubContext;
+            _getGerarchiaToSend = getGerarchiaToSend;
+        }
+
+        public async Task SendNotification(EditPosCommand command)
+        {
+            var SediDaNotificare = _getGerarchiaToSend.Get(command.Pos.CodSede);
+
+            await Task.Factory.StartNew(() => Parallel.ForEach(SediDaNotificare, sede =>
+            {
+                foreach (var tipologia in command.Pos.ListaTipologie)
+                {
+                    _notificationHubContext.Clients.Group(sede).SendAsync("NotifyEditPos", $"E' stata inserita la pos {command.Pos.DescrizionePos} ");
+                }
+            }));
+        }
+    }
+}
