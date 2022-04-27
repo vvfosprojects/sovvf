1754e4f2280d8ecf312c73d107a4c5202a1d896e
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-04-27 16:43:06 +0200

    POS - Delete

--- src/backend/SO115App.SignalR/Sender/GestionePos/NotificationDeletePos.cs
+++ src/backend/SO115App.SignalR/Sender/GestionePos/NotificationDeletePos.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Configuration;
 using SO115App.Models.Servizi.CQRS.Commands.GestioneSoccorso.GestionePOS.DeletePos;
 using SO115App.Models.Servizi.Infrastruttura.Notification.GestioneSchedeContatto;
 using SO115App.SignalR.Utility;
@@ -10,21 +12,34 @@ namespace SO115App.SignalR.Sender.GestionePos
     {
         private readonly IHubContext<NotificationHub> _notificationHubContext;
         private readonly GetGerarchiaToSend _getGerarchiaToSend;
+        private readonly IConfiguration _config;
 
         public NotificationDeletePos(IHubContext<NotificationHub> notificationHubContext,
-                                               GetGerarchiaToSend getGerarchiaToSend)
+                                               GetGerarchiaToSend getGerarchiaToSend,
+                                               IConfiguration config)
         {
             _notificationHubContext = notificationHubContext;
             _getGerarchiaToSend = getGerarchiaToSend;
+            _config = config;
         }
 
         public async Task SendNotification(DeletePosCommand command)
         {
+            #region connessione al WSSignalR
+
+            var hubConnection = new HubConnectionBuilder()
+                        .WithUrl(_config.GetSection("UrlExternalApi").GetSection("WSSignalR").Value)
+                        .Build();
+
+            #endregion connessione al WSSignalR
+
             var SediDaNotificare = _getGerarchiaToSend.Get(command.codSede);
 
             await Task.Factory.StartNew(() => Parallel.ForEach(SediDaNotificare, sede =>
             {
-                _notificationHubContext.Clients.Group(sede).SendAsync("NotifyPos", $"E' stata eliminata la pos {command.Id} ");
+                hubConnection.StartAsync();
+                hubConnection.InvokeAsync("NotifyPos", $"E' stata eliminata la pos {command.Id} ", sede);
+                hubConnection.StopAsync();
             }));
         }
     }
