94c23583b6f4d40bb30cd8d45763986e99f227c2
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2022-05-26 15:37:28 +0200

    nuovo oggetto per gestione situazione ricorsivita sedi

---
+++ src/backend/SO115App.FakePersistance.ExternalAPI/Utility/ManagerSedi.cs
@@ -0,0 +1,31 @@
+﻿using SO115App.API.Models.Classi.Condivise;
+using SO115App.Models.Classi.Condivise;
+using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Distaccamenti;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SO115App.ExternalAPI.Fake.Utility
+{
+    public static class ManagerSedi
+    {
+        public static async Task<List<Sede>> GetFigli(this IGetSedi getSedi, Distaccamento distaccamento)
+        {
+            return null;
+        }
+
+        public static async Task<Sede> GetPadre(this IGetSedi getSedi, Distaccamento distaccamento)
+        {
+            return null;
+        }
+
+        public static async Task<List<Sede>> GetRicorsività(this IGetSedi getSedi, Distaccamento distaccamento)
+        {
+            return null;
+        }
+
+        public static async Task<List<Sede>> GetFigliAndRicorsività(this IGetSedi getSedi, Distaccamento distaccamento)
+        {
+            return null;
+        }
+    }
+}
