192b0cebefdd79fbc94280e871ff097173ae1479
Francesco d'Angelis <francesco.dangelis@dipvvf.it>
2022-06-23 10:54:28 +0200

    fix: stampa chiamata pdf

--- src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneFile/DettaglioRichiesta/DettaglioRichiestaPathQueryHandler.cs
+++ src/backend/SO115App.Models/Servizi/CQRS/Queries/GestioneFile/DettaglioRichiesta/DettaglioRichiestaPathQueryHandler.cs
@@ -4,6 +4,7 @@ using SO115App.API.Models.Classi.Soccorso.Eventi.Segnalazioni;
 using SO115App.Models.Servizi.Infrastruttura.GestioneSoccorso;
 using SO115App.Models.Servizi.Infrastruttura.GestioneSoccorso.GestioneTipologie;
 using SO115App.Models.Servizi.Infrastruttura.GestioneUtenti;
+using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Distaccamenti;
 using SO115App.Persistence.File.CSVManagement;
 using SO115App.Persistence.File.PDFManagement;
 using SO115App.Persistence.File.PDFManagement.TemplateModelForms;
@@ -17,6 +18,7 @@ namespace SO115App.Models.Servizi.CQRS.Queries.GestioneFile.DettaglioRichiesta
         private readonly IGetTipologieByCodice _getTipologie;
         private readonly IGetRichiesta _getRichiesta;
         private readonly IGetUtenteById _getUtente;
+        private readonly IGetSedi _getSedi;
         private readonly IPDFTemplateManager<DettaglioChiamataModelForm> _PDFdettChiamataManager;
         private readonly IPDFTemplateManager<DettaglioInterventoModelForm> _PDFdettInterventoManager;
         private readonly ICSVTemplateManager<DettaglioChiamataModelForm> _CSVdettChiamataManager;
@@ -25,6 +27,7 @@ namespace SO115App.Models.Servizi.CQRS.Queries.GestioneFile.DettaglioRichiesta
         public DettaglioRichiestaPathQueryHandler(IGetRichiesta getRichiesta,
             IGetTipologieByCodice getTipologie,
             IGetUtenteById getUtente,
+            IGetSedi getSedi,
             IPDFTemplateManager<DettaglioChiamataModelForm> dettChiamataManager,
             IPDFTemplateManager<DettaglioInterventoModelForm> dettInterventoManager,
             ICSVTemplateManager<DettaglioChiamataModelForm> CSVdettChiamataManager,
@@ -33,6 +36,7 @@ namespace SO115App.Models.Servizi.CQRS.Queries.GestioneFile.DettaglioRichiesta
             _getUtente = getUtente;
             _getTipologie = getTipologie;
             _getRichiesta = getRichiesta;
+            _getSedi = getSedi;
             _PDFdettChiamataManager = dettChiamataManager;
             _PDFdettInterventoManager = dettInterventoManager;
             _CSVdettChiamataManager = CSVdettChiamataManager;
@@ -46,10 +50,11 @@ namespace SO115App.Models.Servizi.CQRS.Queries.GestioneFile.DettaglioRichiesta
             Log.Error($"Dettaglio QH - 1 **************** INIZIO ************************");
             var richiesta = _getRichiesta.GetByCodice(query.CodiceRichiesta) ?? _getRichiesta.GetByCodiceRichiesta(query.CodiceRichiesta);
             var operatore = _getUtente.GetUtenteByCodice(query.IdOperatore);
+            var sede = _getSedi.GetInfoSede(query.IdSede.First());
 
             var chiamata = new DettaglioChiamata()
             {
-                //Civ_Km = richiesta.Localita.Indirizzo.Split(',')[1],
+                Civ_Km = richiesta.Localita.Civico,
                 Comune = richiesta.Localita.Citta,
                 Prov = richiesta.Localita.Provincia,
                 DataOraChiamata = richiesta.ListaEventi.OfType<Telefonata>().First().Istante,
@@ -64,7 +69,7 @@ namespace SO115App.Models.Servizi.CQRS.Queries.GestioneFile.DettaglioRichiesta
                 Operatore = $"{operatore.Nome} {operatore.Cognome}",
                 Piano = richiesta.Localita.Piano,
                 Scala = richiesta.Localita.Scala,
-                TitoloDistaccamento = richiesta.Competenze.First().Descrizione
+                TitoloDistaccamento = sede.Result.Descrizione
             };
 
             Log.Error($"Dettaglio QH - 1 **************** CHIAMATA ************************");
