d9971c3b6d2ed695913cc158c14634350a54b436
notmikenot <micheledragonetti99@gmail.com>
2022-06-24 17:16:12 +0200

    Soccorso Aereo: fix

--- src/frontend/launcherComponent/src/app/shared/components/sintesi-richiesta/sintesi-richiesta.component.ts
+++ src/frontend/launcherComponent/src/app/shared/components/sintesi-richiesta/sintesi-richiesta.component.ts
@@ -147,8 +147,8 @@ export class SintesiRichiestaComponent implements OnInit, OnChanges {
 
     checkDettaglioSoccorsoAereo(): boolean {
         if (this.richiesta.eventi) {
-            const afmAccettato = this.richiesta.eventi.filter(x => x.note?.includes('AFM accettato: Attesa assegnazione SOCAV'));
-            const afmAnnullato = this.richiesta.eventi.filter(x => x.note?.includes('AFM accettato: Annullato'));
+            const afmAccettato = this.richiesta.eventi.filter(x => x.note?.includes('Inserimento soccorso AFM accettato: Attesa assegnazione SOCAV'));
+            const afmAnnullato = this.richiesta.eventi.filter(x => x.note?.includes('Annullamento soccorso AFM accettato: Annullato'));
             return afmAccettato.length > afmAnnullato.length;
         }
         return false;
