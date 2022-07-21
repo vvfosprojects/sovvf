c397d6a1c7b4b4ced2e4be7cd76753f737dea749
notmikenot <micheledragonetti99@gmail.com>
2022-07-21 15:55:26 +0200

    Sedi Allerta con "Centrale" Sedi Trasferimenti con "Centrale"

--- src/frontend/launcherComponent/src/app/shared/store/states/distaccamenti/distaccamenti.state.ts
+++ src/frontend/launcherComponent/src/app/shared/store/states/distaccamenti/distaccamenti.state.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Sede } from '../../../model/sede.model';
 import { GetDistaccamenti, GetSediAllerta, GetSediTrasferimenti, SetDistaccamenti, SetSediAllerta, SetSediTrasferimenti } from '../../actions/distaccamenti/distaccamenti.actions';
 import { DistaccamentiService } from '../../../../core/service/distaccamenti-service/distaccamenti-service';
+import { getProvinciaByCodProvincia } from '../../../helper/function-province';
 
 export interface DistaccamentiStateModel {
     distaccamenti: Sede[];
@@ -74,9 +75,10 @@ export class DistaccamentiState {
     setSediAllerta({ patchState }: StateContext<DistaccamentiStateModel>, action: SetSediAllerta): void {
         let sediAllertaFiltered = action.sediAllerta;
         // Filtro per avere solo i comandi, il BE restituisce tutte le sedi
-        sediAllertaFiltered = sediAllertaFiltered.filter((s: Sede) => s.codice.split('.')?.length === 2 && s.codice.split('.')[1] === '1000');
-        // Rimuovo la centrale
-        sediAllertaFiltered = sediAllertaFiltered.filter((s: Sede) => s.descrizione?.toLowerCase() !== 'centrale');
+        sediAllertaFiltered = sediAllertaFiltered.filter((s: Sede) => (s.codice.split('.')?.length === 2 && s.codice.split('.')[1] === '1000'));
+        sediAllertaFiltered.forEach((sedeCentrale: Sede) => {
+            sedeCentrale.descrizione = `${sedeCentrale.descrizione} ${getProvinciaByCodProvincia(sedeCentrale.codice.split('.')[0])?.toUpperCase()}`;
+        });
         patchState({
             sediAllerta: sediAllertaFiltered
         });
@@ -84,9 +86,11 @@ export class DistaccamentiState {
 
     @Action(SetSediTrasferimenti)
     setSediTrasferimenti({ patchState }: StateContext<DistaccamentiStateModel>, action: SetSediTrasferimenti): void {
-        let sediTrasferimentiFiltered = action.sediTrasferimenti;
+        const sediTrasferimentiFiltered = action.sediTrasferimenti;
         // Rimuovo la centrale
-        sediTrasferimentiFiltered = sediTrasferimentiFiltered.filter((s: Sede) => s.descrizione?.toLowerCase() !== 'centrale');
+        sediTrasferimentiFiltered.forEach((sedeCentrale: Sede) => {
+            sedeCentrale.descrizione = `${sedeCentrale.descrizione} ${getProvinciaByCodProvincia(sedeCentrale.codice.split('.')[0])?.toUpperCase()}`;
+        });
         patchState({
             sediTrasferimenti: sediTrasferimentiFiltered
         });
