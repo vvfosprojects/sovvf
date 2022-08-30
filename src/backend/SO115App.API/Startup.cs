416ece408ae62a04b9480649e6d2b67ec01a35de
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-08-30 17:59:25 +0200

    Test Redis

--- src/backend/SO115App.API/Startup.cs
+++ src/backend/SO115App.API/Startup.cs
@@ -138,7 +138,7 @@ namespace SO115App.API
                 .AddStackExchangeRedis(o =>
                 {
                     o.Configuration.ChannelPrefix = "SO115Web";
-                    //o.Configuration.Password = Configuration.GetSection("RedisPassword").Value;
+                    o.Configuration.Password = Configuration.GetSection("RedisPassword").Value;
                     o.ConnectionFactory = async writer =>
                     {
                         var config = new ConfigurationOptions
