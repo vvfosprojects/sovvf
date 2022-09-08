876566f81eac222833c6e80c284843802294e627
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-09-08 16:46:17 +0200

    Notifica enti intervenuti - aggiunta la sintesi richiesta

--- src/backend/SO115App.SignalR/Sender/GestioneEntiIntervenuti/NotificationAddEntiIntervenuti.cs
+++ src/backend/SO115App.SignalR/Sender/GestioneEntiIntervenuti/NotificationAddEntiIntervenuti.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using SO115App.API.Models.Servizi.Infrastruttura.GestioneSoccorso;
 using SO115App.Models.Servizi.CQRS.Commands.GestioneSoccorso.GestioneIntervento.GestioneEntiIntervenuti;
 using SO115App.Models.Servizi.Infrastruttura.Notification.GestioneEntiIntervenuti;
 using System.Threading.Tasks;
@@ -8,16 +9,21 @@ namespace SO115App.SignalR.Sender.GestioneFonogramma
     public class NotificationAddEntiIntervenuti : INotifyAddEntiIntervenuti
     {
         private readonly IHubContext<NotificationHub> _notificationHubContext;
+        private readonly IGetSintesiRichiestaAssistenzaByCodice _getSintesiById;
 
-        public NotificationAddEntiIntervenuti(IHubContext<NotificationHub> notificationHubContext)
+        public NotificationAddEntiIntervenuti(IHubContext<NotificationHub> notificationHubContext, IGetSintesiRichiestaAssistenzaByCodice getSintesiById)
         {
             _notificationHubContext = notificationHubContext;
+            _getSintesiById = getSintesiById;
         }
 
         public async Task SendNotification(EntiIntervenutiCommand command)
         {
             //Metto a null la richiesta, perchè al FE non serve, sarebbe un parametro inutile
             //command.Richiesta = null;
+
+            command.Chiamata = _getSintesiById.GetSintesi(command.Richiesta.Codice);
+
             await _notificationHubContext.Clients.All.SendAsync("ModifyAndNotifySuccess", command);
         }
     }
