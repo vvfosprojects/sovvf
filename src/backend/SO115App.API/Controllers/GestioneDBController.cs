4efcae93b17a86033aeb96bf102bea73bf127dfa
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-10-03 17:33:45 +0200

    Fix db

--- src/backend/SO115App.API/Controllers/GestioneDBController.cs
+++ src/backend/SO115App.API/Controllers/GestioneDBController.cs
@@ -1,6 +1,8 @@
 ﻿using CQRS.Queries;
 using Microsoft.AspNetCore.Mvc;
 using SO115App.API.Models.Servizi.CQRS.Queries.GestioneDB;
+using SO115App.API.Models.Servizi.CQRS.Queries.GestioneDB.BonificaSchedeContatto;
+using SO115App.API.Models.Servizi.CQRS.Queries.GestioneDB.ResetUtenti;
 using SO115App.API.Models.Servizi.CQRS.Queries.GestioneExternalLog;
 using SO115App.Persistence.MongoDB.GestioneDB;
 using System;
@@ -13,14 +15,20 @@ namespace SO115App.API.Controllers
     public class GestioneDBController : ControllerBase
     {
         private readonly IQueryHandler<ResetDBQuery, ResetDBResult> _resetDBQuery;
+        private readonly IQueryHandler<ResetUtentiQuery, ResetUtentiResult> _resetUtentiQuery;
+        private readonly IQueryHandler<BonificaSchedeContattoQuery, BonificaSchedeContattoResult> _bonificaSchedeContattoQuery;
         private readonly IQueryHandler<ExternalLogQuery, ExternalLogResult> _externalLogQuery;
         private readonly ISetTipologie _setTipologie;
 
         public GestioneDBController(IQueryHandler<ResetDBQuery, ResetDBResult> resetDBQuery,
+                                    IQueryHandler<ResetUtentiQuery, ResetUtentiResult> resetUtentiQuery,
+                                    IQueryHandler<BonificaSchedeContattoQuery, BonificaSchedeContattoResult> bonificaSchedeContattoQuery,
                                     IQueryHandler<ExternalLogQuery, ExternalLogResult> ExternalLogQuery,
                                     ISetTipologie setTipologie)
         {
             _resetDBQuery = resetDBQuery;
+            _resetUtentiQuery = resetUtentiQuery;
+            _bonificaSchedeContattoQuery = bonificaSchedeContattoQuery;
             _externalLogQuery = ExternalLogQuery;
             _setTipologie = setTipologie;
         }
@@ -38,6 +46,50 @@ namespace SO115App.API.Controllers
             try
             {
                 return Ok(_resetDBQuery.Handle(query).risultato);
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Error(ex.Message);
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("ResetUtenti")]
+        [ProducesResponseType(typeof(bool), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public async Task<IActionResult> ResetUtenti()
+        {
+            ResetUtentiQuery query = new ResetUtentiQuery()
+            {
+                esegui = true
+            };
+
+            try
+            {
+                return Ok(_resetUtentiQuery.Handle(query).risultato);
+            }
+            catch (Exception ex)
+            {
+                Serilog.Log.Error(ex.Message);
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet("BonificaSchedeContatto")]
+        [ProducesResponseType(typeof(bool), 200)]
+        [ProducesResponseType(typeof(string), 400)]
+        public async Task<IActionResult> BonificaSchedeContatto()
+        {
+            BonificaSchedeContattoQuery query = new BonificaSchedeContattoQuery()
+            {
+                esegui = true
+            };
+
+            try
+            {
+                return Ok(_bonificaSchedeContattoQuery.Handle(query).risultato);
             }
             catch (Exception ex)
             {
