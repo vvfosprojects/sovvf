38c64db095f5374afd15bc1febffccb878a64bd2
Riccardo Trionfera <riccardo.trionfera@dipvvf.it>
2022-10-03 13:13:55 +0200

    La descrizione delle sedi viene  presa dalla lista sedi non dalla descrizione nel ruolo utente

--- src/backend/SO115App.Persistence.MongoDB/GestioneUtenti/GestioneUtente/VerificaLogIn.cs
+++ src/backend/SO115App.Persistence.MongoDB/GestioneUtenti/GestioneUtente/VerificaLogIn.cs
@@ -21,23 +21,35 @@ using MongoDB.Driver;
 using Persistence.MongoDB;
 using SO115App.API.Models.Classi.Autenticazione;
 using SO115App.Models.Servizi.Infrastruttura.GestioneUtenti.VerificaUtente;
+using SO115App.Models.Servizi.Infrastruttura.SistemiEsterni.Distaccamenti;
+using System.Threading.Tasks;
 
 namespace SO115App.Persistence.MongoDB.GestioneUtenti.GestioneUtente
 {
     public class VerificaLogIn : IVerificaLogIn
     {
         private readonly DbContext _dbContext;
+        private readonly IGetSedi _getSedi;
 
-        public VerificaLogIn(DbContext dbContext)
+        public VerificaLogIn(DbContext dbContext, IGetSedi getSedi)
         {
             _dbContext = dbContext;
+            _getSedi = getSedi;
         }
 
         public Utente Verifica(string username, string password)
         {
             var builder = Builders<Utente>.Filter;
             var filter = builder.And(builder.Eq(x => x.Username, username), builder.Eq(x => x.Password, password));
-            return _dbContext.UtenteCollection.Find(filter).Single();
+
+            var utente = _dbContext.UtenteCollection.Find(filter).Single();
+
+            Parallel.ForEach(utente.Ruoli, ruolo =>
+            {
+                ruolo.DescSede = _getSedi.GetInfoSede(ruolo.CodSede).Result.Descrizione;
+            });
+
+            return utente;
         }
     }
 }
