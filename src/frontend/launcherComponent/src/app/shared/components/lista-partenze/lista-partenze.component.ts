62c2030f361d2a97df7fa44e58d9b61040e4f42a
federicoserafini19 <federico.serafini@digitaliasistemi.it>
2022-10-27 10:11:33 +0200

    fix composizione partenze e partenze rientrate

--- src/frontend/launcherComponent/src/app/shared/components/lista-partenze/lista-partenze.component.ts
+++ src/frontend/launcherComponent/src/app/shared/components/lista-partenze/lista-partenze.component.ts
@@ -16,77 +16,77 @@ import { MezzoRientratoVisibileRichiesta } from '../../interface/mezzo-rientrato
 import * as moment from 'moment';
 
 @Component({
-    selector: 'app-lista-partenze',
-    templateUrl: './lista-partenze.component.html',
-    styleUrls: ['./lista-partenze.component.css']
+  selector: 'app-lista-partenze',
+  templateUrl: './lista-partenze.component.html',
+  styleUrls: ['./lista-partenze.component.css']
 })
 export class ListaPartenzeComponent {
 
-    @Input() richiesta: SintesiRichiesta;
-    @Input() idDaSganciare: string;
-    @Input() inGestione: boolean;
-    @Input() sostituzioneFineTurnoActive: boolean;
-    @Input() loadingActionMezzo: string[];
-    @Input() annullaStatoMezzi: InfoMezzo[];
-    @Input() disabledModificaStatoMezzo: boolean;
-    @Input() hideSostituzioneFineTurno: boolean;
-    @Input() hideGestisciPartenza: boolean;
-    @Input() onlyPartenzeAttive: boolean;
-    @Input() dateSync: Date;
-    @Input() codMezziRientratiVisibili: MezzoRientratoVisibileRichiesta;
+  @Input() richiesta: SintesiRichiesta;
+  @Input() idDaSganciare: string;
+  @Input() inGestione: boolean;
+  @Input() sostituzioneFineTurnoActive: boolean;
+  @Input() loadingActionMezzo: string[];
+  @Input() annullaStatoMezzi: InfoMezzo[];
+  @Input() disabledModificaStatoMezzo: boolean;
+  @Input() hideSostituzioneFineTurno: boolean;
+  @Input() hideGestisciPartenza: boolean;
+  @Input() onlyPartenzeAttive: boolean;
+  @Input() dateSync: Date;
+  @Input() codMezziRientratiVisibili: MezzoRientratoVisibileRichiesta;
 
-    @Output() actionMezzo: EventEmitter<MezzoActionInterface> = new EventEmitter<MezzoActionInterface>();
-    @Output() modificaPartenza: EventEmitter<string> = new EventEmitter<string>();
-    @Output() selezioneMezzo: EventEmitter<Mezzo> = new EventEmitter<Mezzo>();
-    @Output() sostituzioneFineTurno: EventEmitter<any> = new EventEmitter<any>();
+  @Output() actionMezzo: EventEmitter<MezzoActionInterface> = new EventEmitter<MezzoActionInterface>();
+  @Output() modificaPartenza: EventEmitter<string> = new EventEmitter<string>();
+  @Output() selezioneMezzo: EventEmitter<Mezzo> = new EventEmitter<Mezzo>();
+  @Output() sostituzioneFineTurno: EventEmitter<any> = new EventEmitter<any>();
 
-    tipoConcorrenzaEnum = TipoConcorrenzaEnum;
+  tipoConcorrenzaEnum = TipoConcorrenzaEnum;
+  statoMezzoEnum = StatoMezzo;
+  constructor(private store: Store) {
+  }
 
-    constructor(private store: Store) {
+  onListaSquadrePartenza(codiceMezzo: string, listaSquadre: ListaSquadre, siglaMezzo: string, descMezzo: string): void {
+    this.store.dispatch(new VisualizzaListaSquadrePartenza(codiceMezzo, listaSquadre, siglaMezzo, descMezzo));
+  }
+
+  checkNumeroPartenze(partenze: Partenza[]): number {
+    const annullaStatoMezzoRientrato = this.annullaStatoMezzi?.filter((x: InfoMezzo) => x.stato === StatoMezzo.Rientrato)[0];
+    return this.onlyPartenzeAttive ? checkNumeroPartenzeAttive(partenze) : checkNumeroPartenzeAttive(partenze, this.codMezziRientratiVisibili, annullaStatoMezzoRientrato);
+  }
+
+  getPartenze(partenze: Partenza[]): Partenza[] {
+    const annullaStatoMezzoRientrato = this.annullaStatoMezzi?.filter((x: InfoMezzo) => x.stato === StatoMezzo.Rientrato)[0];
+    return this.onlyPartenzeAttive ? getPartenzeAttive(partenze) : getPartenzeAttive(partenze, this.codMezziRientratiVisibili, annullaStatoMezzoRientrato);
+  }
+
+  getUltimoStatoMezzo(codMezzo: string): StatoMezzo {
+    const eventoCodMezzo = this.richiesta.eventi.filter((evento: EventoMezzo) => evento.codiceMezzo === codMezzo);
+    if (eventoCodMezzo) {
+      const eventoUltimoStatoMezzo = eventoCodMezzo[eventoCodMezzo?.length - 1];
+      return eventoUltimoStatoMezzo.stato;
     }
+  }
 
-    onListaSquadrePartenza(codiceMezzo: string, listaSquadre: ListaSquadre, siglaMezzo: string, descMezzo: string): void {
-        this.store.dispatch(new VisualizzaListaSquadrePartenza(codiceMezzo, listaSquadre, siglaMezzo, descMezzo));
+  checkAnnullaStatoMezzo(codMezzo: string, statoMezzo: StatoMezzo, codPartenza: string): boolean {
+    const annullaStatoMezzo = this.annullaStatoMezzi?.filter((iM: InfoMezzo) => iM.codMezzo === codMezzo && iM.stato === statoMezzo && iM.codicePartenza === codPartenza)[0];
+    if (annullaStatoMezzo) {
+      const unMinutoFa = new Date();
+      unMinutoFa.setMinutes(unMinutoFa.getMinutes() - 1);
+      if (!moment(annullaStatoMezzo.istante).isAfter(unMinutoFa)) {
+        this.store.dispatch(new RemoveAnnullaStatoMezzi([codMezzo], statoMezzo));
+      }
+      return moment(annullaStatoMezzo.istante).isAfter(unMinutoFa);
     }
+    return false;
+  }
 
-    checkNumeroPartenze(partenze: Partenza[]): number {
-        const annullaStatoMezzoRientrato = this.annullaStatoMezzi?.filter((x: InfoMezzo) => x.stato === StatoMezzo.Rientrato)[0];
-        return this.onlyPartenzeAttive ? checkNumeroPartenzeAttive(partenze) : checkNumeroPartenzeAttive(partenze, this.codMezziRientratiVisibili, annullaStatoMezzoRientrato);
-    }
+  getSostituzioneFineTurnoVisible(): boolean {
+    // tslint:disable-next-line:max-line-length
+    return !this.hideSostituzioneFineTurno && this.inGestione && this.sostituzioneFineTurnoActive && !this.richiesta.partenze?.some((p: Partenza) => !p.partenza.partenzaAnnullata && !p.partenza.terminata && !p.partenza.sganciata && this.loadingActionMezzo?.includes(p.partenza.mezzo.codice));
+  }
 
-    getPartenze(partenze: Partenza[]): Partenza[] {
-        const annullaStatoMezzoRientrato = this.annullaStatoMezzi?.filter((x: InfoMezzo) => x.stato === StatoMezzo.Rientrato)[0];
-        return this.onlyPartenzeAttive ? getPartenzeAttive(partenze) : getPartenzeAttive(partenze, this.codMezziRientratiVisibili, annullaStatoMezzoRientrato);
-    }
-
-    getUltimoStatoMezzo(codMezzo: string): StatoMezzo {
-        const eventoCodMezzo = this.richiesta.eventi.filter((evento: EventoMezzo) => evento.codiceMezzo === codMezzo);
-        if (eventoCodMezzo) {
-            const eventoUltimoStatoMezzo = eventoCodMezzo[eventoCodMezzo?.length - 1];
-            return eventoUltimoStatoMezzo.stato;
-        }
-    }
-
-    checkAnnullaStatoMezzo(codMezzo: string, statoMezzo: StatoMezzo, codPartenza: string): boolean {
-        const annullaStatoMezzo = this.annullaStatoMezzi?.filter((iM: InfoMezzo) => iM.codMezzo === codMezzo && iM.stato === statoMezzo && iM.codicePartenza === codPartenza)[0];
-        if (annullaStatoMezzo) {
-            const unMinutoFa = new Date();
-            unMinutoFa.setMinutes(unMinutoFa.getMinutes() - 1);
-            if (!moment(annullaStatoMezzo.istante).isAfter(unMinutoFa)) {
-                this.store.dispatch(new RemoveAnnullaStatoMezzi([codMezzo], statoMezzo));
-            }
-            return moment(annullaStatoMezzo.istante).isAfter(unMinutoFa);
-        }
-        return false;
-    }
-
-    getSostituzioneFineTurnoVisible(): boolean {
-        // tslint:disable-next-line:max-line-length
-        return !this.hideSostituzioneFineTurno && this.inGestione && this.sostituzioneFineTurnoActive && !this.richiesta.partenze?.some((p: Partenza) => !p.partenza.partenzaAnnullata && !p.partenza.terminata && !p.partenza.sganciata && this.loadingActionMezzo?.includes(p.partenza.mezzo.codice));
-    }
-
-    onActionMezzo(mezzoAction: MezzoActionInterface): void {
-        this.actionMezzo.emit(mezzoAction);
-    }
+  onActionMezzo(mezzoAction: MezzoActionInterface): void {
+    this.actionMezzo.emit(mezzoAction);
+  }
 
 }
